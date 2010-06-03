98fcccfe9751894ace9693a39ba0609fe5ea904d
Jeff Muizelaar <jmuizelaar@mozilla.com>
2010-06-03 10:16:07 -0400

    Change the x86 idct functions to do reconstruction at the same time
    Change-Id: I896fe6f9664e6849c7cee2cc6bb4e045eb42540f

--- vp8/decoder/x86/dequantize_x86.h
+++ vp8/decoder/x86/dequantize_x86.h
@@ -21,11 +21,19 @@
  */
 #if HAVE_MMX
 extern prototype_dequant_block(vp8_dequantize_b_mmx);
+extern prototype_dequant_idct_add(vp8_dequant_idct_add_mmx);
+extern prototype_dequant_idct_dc_add(vp8_dequant_dc_idct_add_mmx);
 
 
 #if !CONFIG_RUNTIME_CPU_DETECT
 #undef  vp8_dequant_block
 #define vp8_dequant_block vp8_dequantize_b_mmx
+
+#undef  vp8_dequant_idct_add
+#define vp8_dequant_idct_add vp8_dequant_idct_add_mmx
+
+#undef  vp8_dequant_idct_dc
+#define vp8_dequant_idct_add_dc vp8_dequant_dc_idct_add_mmx
 
 #endif
 #endif
