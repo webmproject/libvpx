fc94ffcea4225428335093c36278e5757b3de934
Yunqing Wang <yunqingwang@google.com>
2010-10-21 10:26:50 -0400

    Rewrite vp8_short_walsh4x4_sse2()
    This rewriting reflects changes made in commit "Improve the
    accuracy of forward walsh-hadamard transform". Since this function
    is not called much, only a small encoder performance gain (~0.5% )
    is seen.
    
    Change-Id: Ie9df58a43028a11fd5b115c4bbe3141f7596578b

--- vp8/encoder/x86/x86_csystemdependent.c
+++ vp8/encoder/x86/x86_csystemdependent.c
@@ -289,7 +289,7 @@ void vp8_arch_x86_encoder_init(VP8_COMP *cpi)
         cpi->rtcd.fdct.fast4x4                   = vp8_short_fdct4x4_sse2;
         cpi->rtcd.fdct.fast8x4                   = vp8_short_fdct8x4_sse2;
 
-        cpi->rtcd.fdct.walsh_short4x4            = vp8_short_walsh4x4_c ;
+        cpi->rtcd.fdct.walsh_short4x4            = vp8_short_walsh4x4_sse2 ;
 
         cpi->rtcd.encodemb.berr                  = vp8_block_error_xmm;
         cpi->rtcd.encodemb.mberr                 = vp8_mbblock_error_xmm;
