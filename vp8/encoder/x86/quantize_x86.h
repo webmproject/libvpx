c32e0ecc592d12573199c992f0fb710b7785c5eb
Johann <johannkoenig@google.com>
2011-03-24 13:31:10 -0400

    use asm_offsets with vp8_fast_quantize_b_sse2
    on the same order as the regular quantize change: ~2%
    
    Change-Id: I5c9eec18e89ae7345dd96945cb740e6f349cee86

--- vp8/encoder/x86/quantize_x86.h
+++ vp8/encoder/x86/quantize_x86.h
@@ -24,11 +24,15 @@
 
 #if HAVE_SSE2
 extern prototype_quantize_block(vp8_regular_quantize_b_sse2);
+extern prototype_quantize_block(vp8_fast_quantize_b_sse2);
 
 #if !CONFIG_RUNTIME_CPU_DETECT
 
 #undef vp8_quantize_quantb
 #define vp8_quantize_quantb vp8_regular_quantize_b_sse2
+
+#undef vp8_quantize_fastquantb
+#define vp8_quantize_fastquantb vp8_fast_quantize_b_sse2
 
 #endif
 
