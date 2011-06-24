deca8cfc4461187a0dcc2fe588c983897b87aa7e
Adrian Grange <agrange@google.com>
2011-06-24 08:43:40 -0700

    Fixed initialization of frame buffer ref counters
    Only the first frame buffer ref counter was being initialized
    because the index was fixed at 0 rather than using i.
    
    Change-Id: Ib842298be4a5e3607f9e21c2cd4bfbee4054ffc4

--- vp8/common/alloccommon.c
+++ vp8/common/alloccommon.c
@@ -70,7 +70,7 @@ int vp8_alloc_frame_buffers(VP8_COMMON *oci, int width, int height)
 
     for (i = 0; i < NUM_YV12_BUFFERS; i++)
     {
-        oci->fb_idx_ref_cnt[0] = 0;
+        oci->fb_idx_ref_cnt[i] = 0;
         oci->yv12_fb[i].flags = 0;
         if (vp8_yv12_alloc_frame_buffer(&oci->yv12_fb[i], width, height, VP8BORDERINPIXELS) < 0)
         {
