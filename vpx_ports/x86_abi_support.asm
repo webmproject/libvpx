c8a23ad3f44fdd70fbe759d3d6e27f1e03fb37b9
Ronald S. Bultje <rbultje@google.com>
2011-06-30 14:04:27 -0700

    Properly use GET_GOT/RESTORE_GOT when using GLOBAL().
    This should fix binaries using PIC on x86-32. Also should
    fix issue 343.
    
    Change-Id: I591de3ad68c8a8bb16054bd8f987a75b4e2bad02

--- vpx_ports/x86_abi_support.asm
+++ vpx_ports/x86_abi_support.asm
@@ -147,6 +147,7 @@
 %if ABI_IS_32BIT
   %if CONFIG_PIC=1
   %ifidn __OUTPUT_FORMAT__,elf32
+    %define GET_GOT_SAVE_ARG 1
     %define WRT_PLT wrt ..plt
     %macro GET_GOT 1
       extern _GLOBAL_OFFSET_TABLE_
@@ -165,6 +166,7 @@
       %define RESTORE_GOT pop %1
     %endmacro
   %elifidn __OUTPUT_FORMAT__,macho32
+    %define GET_GOT_SAVE_ARG 1
     %macro GET_GOT 1
       push %1
       call %%get_got
