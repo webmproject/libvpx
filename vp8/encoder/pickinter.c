2b450a460fa847c7460f51ab38ecb9ab10f18218
Adrian Grange <agrange@google.com>
2011-11-01 08:41:43 -0700

    Deleted repeated code block
    The block of code skipped testing the current mode if the
    reference frame is AltRef, the mv is not (0,0) and
    ARNR filtering is disabled.
    
    This block of code has already been tested above if the
    macro CONFIG_SEGFEATURES is set to 0.
    
    Change-Id: I3f5710bb8270caad06c9a0eee59fa0daf1f70776

--- vp8/encoder/pickinter.c
+++ vp8/encoder/pickinter.c
@@ -600,17 +600,6 @@ void vp8_pick_inter_mode(VP8_COMP *cpi, MACROBLOCK *x, int recon_yoffset,
             memcpy(mdcounts, MDCounts[x->e_mbd.mode_info_context->mbmi.ref_frame], sizeof(mdcounts));
         }
 
-#if !CONFIG_SEGFEATURES
-        // Only consider ZEROMV/ALTREF_FRAME for alt ref frame,
-        // unless ARNR filtering is enabled in which case we want
-        // an unfiltered alternative
-        if (cpi->is_src_frame_alt_ref && (cpi->oxcf.arnr_max_frames == 0))
-        {
-            if (this_mode != ZEROMV || x->e_mbd.mode_info_context->mbmi.ref_frame != ALTREF_FRAME)
-                continue;
-        }
-#endif
-
         switch (this_mode)
         {
         case B_PRED:
