d497ec688d796e06a2611396215f8ecade5aec95
Tero Rintaluoma <teror@google.com>
2011-11-04 09:36:49 +0200

    Fix issue 374: eob read incorrectly
    Updated eob changes to check_reset_2nd_coeffs function.
    
    Change-Id: Id1b21c91c7f0fd286640b487ffe47867009b717d

--- vp8/encoder/encodemb.c
+++ vp8/encoder/encodemb.c
@@ -479,7 +479,7 @@ static void check_reset_2nd_coeffs(MACROBLOCKD *x, int type,
     if(bd->dequant[0]>=35 && bd->dequant[1]>=35)
         return;
 
-    for(i=0;i<bd->eob;i++)
+    for(i=0;i<(*bd->eob);i++)
     {
         int coef = bd->dqcoeff[vp8_default_zig_zag1d[i]];
         sum+= (coef>=0)?coef:-coef;
@@ -496,14 +496,14 @@ static void check_reset_2nd_coeffs(MACROBLOCKD *x, int type,
     **************************************************************************/
     if(sum < 35)
     {
-        for(i=0;i<bd->eob;i++)
+        for(i=0;i<(*bd->eob);i++)
         {
             int rc = vp8_default_zig_zag1d[i];
             bd->qcoeff[rc]=0;
             bd->dqcoeff[rc]=0;
         }
-        bd->eob = 0;
-        *a = *l = (bd->eob != !type);
+        *bd->eob = 0;
+        *a = *l = (*bd->eob != !type);
     }
 }
 
