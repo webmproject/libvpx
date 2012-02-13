ef8ade138de7550d1e39d721807a59a152c21f82
Deb Mukherjee <debargha@google.com>
2012-02-13 14:38:58 -0800

    Adjusting 8-tap filter + prelim edge pixel filter code.
    Results with the new filter coefficients compared with the previous
    versions are here:
    http://www.corp.google.com/~debargha/vp8_results/enhinterp.html (derf)
    http://www.corp.google.com/~debargha/vp8_results/enhinterp_hd.html (HD)
    Overall, the derf set improves by 0.94% with 8-tap filters while
    the HD set improves by 0.58%.
    
    Patch 1: resolving merge conflict
    
    Change-Id: I09db0abdab7b08bb19f86d911de23d2123309748

--- vp8/common/filter.h
+++ vp8/common/filter.h
@@ -18,7 +18,15 @@
 #define VP8_FILTER_WEIGHT 128
 #define VP8_FILTER_SHIFT  7
 
+/* whether to use a special filter for edge pixels */
+#define EDGE_PIXEL_FILTER 0
+
 extern const short vp8_bilinear_filters[8][2];
 extern const short vp8_sub_pel_filters[8][INTERP_EXTEND*2];
+
+#if EDGE_PIXEL_FILTER > 0
+#define EDGE_PIXEL_FILTER_EXTEND 2
+extern const short vp8_sub_pel_filters_ns[64][4*EDGE_PIXEL_FILTER_EXTEND*EDGE_PIXEL_FILTER_EXTEND];
+#endif
 
 #endif //FILTER_H
