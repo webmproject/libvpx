56e8485c8423072e552db537bd55dbdaebde877b
Johann <johannkoenig@google.com>
2012-03-16 14:54:07 -0400

    darwin universal builds need BUILD_PFX
    Universal builds create subdirectories for each target. Without
    BUILD_PFX we only generated one vpx_rtcd.h instead of one for each.
    
    Change-Id: I1caed4e018c8865ffc8da15e434cae2b96154fb4

--- libs.mk
+++ libs.mk
@@ -329,8 +329,8 @@ CLEAN-OBJS += $(BUILD_PFX)vpx_version.h
 #
 # Rule to generate runtime cpu detection files
 #
-$(OBJS-yes:.o=.d): vpx_rtcd.h
-vpx_rtcd.h: $(SRC_PATH_BARE)/$(sort $(filter %rtcd_defs.sh,$(CODEC_SRCS)))
+$(OBJS-yes:.o=.d): $(BUILD_PFX)vpx_rtcd.h
+$(BUILD_PFX)vpx_rtcd.h: $(SRC_PATH_BARE)/$(sort $(filter %rtcd_defs.sh,$(CODEC_SRCS)))
 	@echo "    [CREATE] $@"
 	$(qexec)$(SRC_PATH_BARE)/build/make/rtcd.sh --arch=$(TGT_ISA) \
           --sym=vpx_rtcd \
