38ddb426d097349c0926977068f97655d2d77f26
Christian Duvivier <cduvivier@google.com>
2012-05-21 07:54:20 -0700

    Inline Intrinsic optimized Denoiser
    Faster version of denoiser, cut cost by 1.7x for C path, by 3.3x for
    SSE2 path.
    
    Change-Id: I154786308550763bc0e3497e5fa5bfd1ce651beb

--- vp8/encoder/denoising.h
+++ vp8/encoder/denoising.h
@@ -13,6 +13,8 @@
 
 #include "block.h"
 
+#define NOISE_DIFF2_THRESHOLD (75)
+
 typedef struct vp8_denoiser
 {
   YV12_BUFFER_CONFIG yv12_running_avg;
@@ -29,5 +31,13 @@ void vp8_denoiser_denoise_mb(VP8_DENOISER *denoiser,
                              unsigned int zero_mv_sse,
                              int recon_yoffset,
                              int recon_uvoffset);
+
+union coeff_pair
+{
+    uint32_t as_int;
+    uint16_t as_short[2];
+};
+
+union coeff_pair *vp8_get_filter_coeff_LUT(unsigned int motion_magnitude);
 
 #endif  // VP8_ENCODER_DENOISING_H_
