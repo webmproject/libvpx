8df79e9d427fe2e334f110a8abfe3e43449d87c4
John Koleszar <jkoleszar@google.com>
2012-06-15 15:40:13 -0700

    Remove threading dependencies with --disable-multithread
    Avoid a pthreads dependency via pthread_once() when compiled with
    --disable-multithread.
    
    In addition, this synchronization is disabled for Win32 as well, even
    though we can be sure that the required primatives exist, so that the
    requirements on the application when built with --disable-multithread
    are consistent across platforms.
    
    Users using libvpx built with --disable-multithread in a multithreaded
    context should provide their own synchronization. Updated the
    documentation to vpx_codec_enc_init_ver() and vpx_codec_dec_init_ver()
    to note this requirement. Moved the RTCD initialization call to match
    this description, as previously it didn't happen until the first
    frame.
    
    Change-Id: Id576f6bce2758362188278d3085051c218a56d4a

--- vp8/vp8_dx_iface.c
+++ vp8/vp8_dx_iface.c
@@ -11,6 +11,7 @@
 
 #include <stdlib.h>
 #include <string.h>
+#include "vpx_rtcd.h"
 #include "vpx/vpx_decoder.h"
 #include "vpx/vp8dx.h"
 #include "vpx/internal/vpx_codec_internal.h"
@@ -186,6 +187,8 @@ static vpx_codec_err_t vp8_init(vpx_codec_ctx_t *ctx,
 {
     vpx_codec_err_t        res = VPX_CODEC_OK;
     (void) data;
+
+    vpx_rtcd();
 
     /* This function only allocates space for the vpx_codec_alg_priv_t
      * structure. More memory may be required at the time the stream
