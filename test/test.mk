532faf0f1c1f3000a7271f05db279519f7a62e91
Yaowu Xu <yaowu@google.com>
2012-06-18 13:41:17 -0700

    Added unit test for 4x4 forward dct
    Change-Id: I1607676879c29adc0173a3c0355a0e5d8a84fc3b

--- test/test.mk
+++ test/test.mk
@@ -6,6 +6,7 @@ LIBVPX_TEST_SRCS-yes += encode_test_driver.h
 LIBVPX_TEST_SRCS-yes += i420_video_source.h
 LIBVPX_TEST_SRCS-yes += idctllm_test.cc
 LIBVPX_TEST_SRCS-yes += intrapred_test.cc
+LIBVPX_TEST_SRCS-yes += fdct4x4_test.cc
 LIBVPX_TEST_SRCS-yes += keyframe_test.cc
 LIBVPX_TEST_SRCS-yes += pp_filter_test.cc
 LIBVPX_TEST_SRCS-yes += resize_test.cc
