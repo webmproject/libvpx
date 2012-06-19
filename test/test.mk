c7acd6db5e81998dced1177c430546fa5666ab5c
Adrian Grange <agrange@google.com>
2012-06-19 12:03:36 -0700

    Added unit test for vp8_post_proc_down_and_across
    This is a unit test for the post-processing functions:
    - vp8_post_proc_down_and_across_c
    - vp8_post_proc_down_and_across_mmx
    - vp8_post_proc_down_and_across_xmm
    
    Change-Id: Iec3e690327b17470209c00417835473f6d9a35d6

--- test/test.mk
+++ test/test.mk
@@ -5,6 +5,7 @@ LIBVPX_TEST_SRCS-yes += encode_test_driver.cc
 LIBVPX_TEST_SRCS-yes += encode_test_driver.h
 LIBVPX_TEST_SRCS-yes += idctllm_test.cc
 LIBVPX_TEST_SRCS-yes += keyframe_test.cc
+LIBVPX_TEST_SRCS-yes += pp_filter_test.cc
 LIBVPX_TEST_SRCS-yes += resize_test.cc
 LIBVPX_TEST_SRCS-yes += test_libvpx.cc
 LIBVPX_TEST_SRCS-yes += video_source.h
