63ef9c40a443ffbf1a8ff6d016de472d067c297a
Christian Duvivier <cduvivier@google.com>
2012-08-20 23:49:43 -0700

    SSE2 version of vectorized 8-tap filtering.
    About 20% overall encoder speedup (vs. about 30% for sse4 version).
    
    Change-Id: Ibf608a6a1bc94b14ec47e8046d3206b275b5a8bd

--- vp8/vp8_common.mk
+++ vp8/vp8_common.mk
@@ -119,6 +119,11 @@ ifeq ($(HAVE_SSE4_1),yes)
 vp8/common/x86/filter_sse4.c.o: CFLAGS += -msse4
 endif
 
+VP8_COMMON_SRCS-$(HAVE_SSE2) += common/x86/filter_sse2.c
+ifeq ($(HAVE_SSE2),yes)
+vp8/common/x86/filter_sse2.c.o: CFLAGS += -msse2
+endif
+
 VP8_COMMON_SRCS-$(ARCH_ARM)  += common/arm/arm_systemdependent.c
 VP8_COMMON_SRCS-$(ARCH_ARM)  += common/arm/bilinearfilter_arm.c
 VP8_COMMON_SRCS-$(ARCH_ARM)  += common/arm/bilinearfilter_arm.h
