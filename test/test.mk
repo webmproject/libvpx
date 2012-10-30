533470c998f53140dff083f9e3f9583dd81a498b
Jim Bankoski <jimbankoski@google.com>
2012-10-29 19:54:06 -0700

    add unit test for datarate control
    Adds some basic datarate control tests..
    
    Change-Id: I6eff18aa58ef7cfd14886c6b1e048247cd1ad247

--- test/test.mk
+++ test/test.mk
@@ -12,6 +12,7 @@ LIBVPX_TEST_SRCS-yes += video_source.h
 LIBVPX_TEST_SRCS-$(CONFIG_VP8_ENCODER) += altref_test.cc
 LIBVPX_TEST_SRCS-$(CONFIG_VP8_ENCODER) += config_test.cc
 LIBVPX_TEST_SRCS-$(CONFIG_VP8_ENCODER) += cq_test.cc
+LIBVPX_TEST_SRCS-$(CONFIG_VP8_ENCODER) += datarate_test.cc
 LIBVPX_TEST_SRCS-$(CONFIG_VP8_ENCODER) += encode_test_driver.cc
 LIBVPX_TEST_SRCS-$(CONFIG_VP8_ENCODER) += encode_test_driver.h
 LIBVPX_TEST_SRCS-$(CONFIG_VP8_ENCODER) += error_resilience_test.cc
