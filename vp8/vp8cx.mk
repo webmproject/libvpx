3e3fffe038e5461be1cb03e12d33326e6b203011
Jim Bankoski <jimbankoski@google.com>
2012-11-29 14:24:54 -0800

    a vp8 warning to boot.
    Change-Id: I32ed051c8b4d3e5afa4e13bb4e3b2ca5e922ff64

--- vp8/vp8cx.mk
+++ vp8/vp8cx.mk
@@ -102,6 +102,7 @@ ifeq ($(CONFIG_TEMPORAL_DENOISING),yes)
 VP8_CX_SRCS-$(HAVE_SSE2) += encoder/x86/denoising_sse2.c
 ifeq ($(HAVE_SSE2),yes)
 vp8/encoder/x86/denoising_sse2.c.o: CFLAGS += -msse2
+vp8/encoder/x86/denoising_sse2.c.d: CFLAGS += -msse2
 endif
 endif
 
