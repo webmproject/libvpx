1c0ee77589b05a99ec50abdd373d2ba15d4fc8d6
Frank Galligan <fgalligan@google.com>
2012-12-07 15:20:39 -0800

    Fix meaninglesss if.
    Change-Id: I0cb06d77805246fe39d39ad3bc5df3c3f52c7050

--- vp9/encoder/vp9_firstpass.c
+++ vp9/encoder/vp9_firstpass.c
@@ -780,7 +780,7 @@ void vp9_first_pass(VP9_COMP *cpi) {
     else
       recon_file = fopen(filename, "ab");
 
-    if (fwrite(lst_yv12->buffer_alloc, lst_yv12->frame_size, 1, recon_file));
+    (void)fwrite(lst_yv12->buffer_alloc, lst_yv12->frame_size, 1, recon_file);
     fclose(recon_file);
   }
 
