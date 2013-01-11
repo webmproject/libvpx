4987c0f07e1791099fe87199b27a2804079f62f0
Scott LaVarnway <slavarnway@google.com>
2013-01-11 14:54:14 -0800

    Initial sse2 version of the wide loopfilters
    Updated the rtcd_defs and used the sse2 uv version
    of the loopfilter.  The performance improved by ~8%
    for the test clip used.
    
    Change-Id: I5a0bca3b6674198d40ca4a77b8cc722ddde79c36

--- vp9/common/vp9_rtcd_defs.sh
+++ vp9/common/vp9_rtcd_defs.sh
@@ -233,11 +233,14 @@ vp9_loop_filter_simple_bh_c=vp9_loop_filter_bhs_c
 vp9_loop_filter_simple_bh_mmx=vp9_loop_filter_bhs_mmx
 vp9_loop_filter_simple_bh_sse2=vp9_loop_filter_bhs_sse2
 
+if [ "$CONFIG_WIDERLPF" = "yes" ]; then
 prototype void vp9_lpf_mbh_w "unsigned char *y_ptr, unsigned char *u_ptr, unsigned char *v_ptr, int y_stride, int uv_stride, struct loop_filter_info *lfi"
-specialize vp9_lpf_mbh_w
+specialize vp9_lpf_mbh_w sse2
 
 prototype void vp9_lpf_mbv_w "unsigned char *y_ptr, unsigned char *u_ptr, unsigned char *v_ptr, int y_stride, int uv_stride, struct loop_filter_info *lfi"
-specialize vp9_lpf_mbv_w
+specialize vp9_lpf_mbv_w sse2
+fi
+
 #
 # post proc
 #
