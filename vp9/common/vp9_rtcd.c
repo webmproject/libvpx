971ff2679fa09e64a19dd6dc349414e632ba3428
Dmitry Kovalev <dkovalev@google.com>
2013-02-26 15:52:05 -0800

    Removing redundant 'extern' keyword from function declarations.
    Change-Id: I893fa36297b9bd9cff93d082f1736f6860b15c0d

--- vp9/common/vp9_rtcd.c
+++ vp9/common/vp9_rtcd.c
@@ -12,10 +12,9 @@
 #include "vp9_rtcd.h"
 #include "vpx_ports/vpx_once.h"
 
-extern void vpx_scale_rtcd(void);
+void vpx_scale_rtcd(void);
 
-void vp9_rtcd()
-{
+void vp9_rtcd() {
     vpx_scale_rtcd();
     once(setup_rtcd_internal);
 }
