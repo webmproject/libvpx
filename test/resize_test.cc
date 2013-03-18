449f136886e96fcf448bf9b68952977da703c614
John Koleszar <jkoleszar@google.com>
2013-03-18 15:17:45 -0700

    VP9/ResizeInternalTest: adjust passing threshold
    Update to +/- 1dB.
    
    Change-Id: Idada001f261b36945c9334e288a415ee2c79c415

--- test/resize_test.cc
+++ test/resize_test.cc
@@ -118,7 +118,7 @@ class ResizeInternalTest : public ResizeTest {
   virtual void PSNRPktHook(const vpx_codec_cx_pkt_t *pkt) {
     if (!frame0_psnr_)
       frame0_psnr_ = pkt->data.psnr.psnr[0];
-    EXPECT_NEAR(pkt->data.psnr.psnr[0], frame0_psnr_, 0.03*frame0_psnr_);
+    EXPECT_NEAR(pkt->data.psnr.psnr[0], frame0_psnr_, 1.0);
   }
 
   double frame0_psnr_;
