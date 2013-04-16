9b84d113566216b700e65487cd2fbc5293be492c
Adrian Grange <agrange@google.com>
2013-04-16 15:55:31 -0700

    Move configure_arnr_filter function
    This function is now called from configures the ARNR
    filter so it belongs with the other temporal filter
    functions.
    
    Change-Id: I64211875918364b5b8edfb97743e573c6def1663

--- vp9/encoder/vp9_temporal_filter.h
+++ vp9/encoder/vp9_temporal_filter.h
@@ -12,5 +12,7 @@
 #define VP9_ENCODER_VP9_TEMPORAL_FILTER_H_
 
 void vp9_temporal_filter_prepare(VP9_COMP *cpi, int distance);
+void configure_arnr_filter(VP9_COMP *cpi, const unsigned int this_frame,
+                           const int group_boost);
 
 #endif  // VP9_ENCODER_VP9_TEMPORAL_FILTER_H_
