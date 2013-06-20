a32a086d23c2061344af7653892456bde3fffd0d
Jingning Han <jingning@google.com>
2013-06-20 09:00:23 -0700

    Enable sse2 implmentation of 8x8 ADST/DCT
    This commit makes use of the butterfly structure to enable the sse2
    version implementation of 8x8 ADST/DCT hybrid transform coding.
    
    The runtime of hybrid transform module goes down from 1170 cycles
    to 245 cycles. Overall speed-up around 1.5%.
    
    Change-Id: Ic808ffd21ece8a9d0410d8c0243d7b6c28ac3b3f

--- vp9/common/vp9_rtcd_defs.sh
+++ vp9/common/vp9_rtcd_defs.sh
@@ -551,7 +551,7 @@ prototype void vp9_short_fht4x4 "int16_t *InputData, int16_t *OutputData, int pi
 specialize vp9_short_fht4x4
 
 prototype void vp9_short_fht8x8 "int16_t *InputData, int16_t *OutputData, int pitch, int tx_type"
-specialize vp9_short_fht8x8
+specialize vp9_short_fht8x8 sse2
 
 prototype void vp9_short_fht16x16 "int16_t *InputData, int16_t *OutputData, int pitch, int tx_type"
 specialize vp9_short_fht16x16
