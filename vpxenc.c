b9e27610192bc78d8e576438ec876e1afb290446
John Koleszar <jkoleszar@google.com>
2013-06-25 09:15:07 -0700

    vpxenc: send usage to stderr
    Thanks to hiiragikei AT gmail.com for the fix.
    
    Change-Id: Iab6c0822593fc5557d86efbb014ff6409ff05b35

--- vpxenc.c
+++ vpxenc.c
@@ -1180,22 +1180,22 @@ static void usage_exit() {
           exec_name);
 
   fprintf(stderr, "\nOptions:\n");
-  arg_show_usage(stdout, main_args);
+  arg_show_usage(stderr, main_args);
   fprintf(stderr, "\nEncoder Global Options:\n");
-  arg_show_usage(stdout, global_args);
+  arg_show_usage(stderr, global_args);
   fprintf(stderr, "\nRate Control Options:\n");
-  arg_show_usage(stdout, rc_args);
+  arg_show_usage(stderr, rc_args);
   fprintf(stderr, "\nTwopass Rate Control Options:\n");
-  arg_show_usage(stdout, rc_twopass_args);
+  arg_show_usage(stderr, rc_twopass_args);
   fprintf(stderr, "\nKeyframe Placement Options:\n");
-  arg_show_usage(stdout, kf_args);
+  arg_show_usage(stderr, kf_args);
 #if CONFIG_VP8_ENCODER
   fprintf(stderr, "\nVP8 Specific Options:\n");
-  arg_show_usage(stdout, vp8_args);
+  arg_show_usage(stderr, vp8_args);
 #endif
 #if CONFIG_VP9_ENCODER
   fprintf(stderr, "\nVP9 Specific Options:\n");
-  arg_show_usage(stdout, vp9_args);
+  arg_show_usage(stderr, vp9_args);
 #endif
   fprintf(stderr, "\nStream timebase (--timebase):\n"
           "  The desired precision of timestamps in the output, expressed\n"
