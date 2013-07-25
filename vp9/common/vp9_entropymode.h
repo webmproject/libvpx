6c8ef8d95717b5ce272a3dfc8b254d3ff25b45c2
Johann <johann.koenig@duck.com>
2013-07-25 14:24:49 -0700

    Add const to vp9_accum_mv_refs parameter
    Change-Id: I0625d8ffddf590dfecd1bb8b8d6f57ef64b8bf18

--- vp9/common/vp9_entropymode.h
+++ vp9/common/vp9_entropymode.h
@@ -69,7 +69,9 @@ void vp9_init_mbmode_probs(struct VP9Common *x);
 
 void vp9_adapt_mode_probs(struct VP9Common *);
 
-void vp9_accum_mv_refs(struct VP9Common *pc, MB_PREDICTION_MODE m, int context);
+void vp9_accum_mv_refs(struct VP9Common *pc,
+                       MB_PREDICTION_MODE m,
+                       const int context);
 
 void tx_counts_to_branch_counts_32x32(unsigned int *tx_count_32x32p,
                                       unsigned int (*ct_32x32p)[2]);
