1ee66933c13834985b676f009bd16c1b78c72ff9
Yaowu Xu <yaowu@google.com>
2013-09-04 16:32:26 -0700

    make bsize requirement for SEG_LVL_SKIP explicit
    The segment feature SEG_LVL_SKIP requires the prediction unit size
    to be at least BLOCK_8X8. This commit makes the requirement to be
    explicit. This is to prevent future encoder implementations from
    making wrong choices.
    
    Change-Id: I0127f0bd4c66e130b81f0cb0a8d3dbfe3b2da5c2

--- vp9/decoder/vp9_decodemv.c
+++ vp9/decoder/vp9_decodemv.c
@@ -460,6 +460,7 @@ static void read_inter_block_mode_info(VP9D_COMP *pbi, MODE_INFO *mi,
 
   if (vp9_segfeature_active(&cm->seg, mbmi->segment_id, SEG_LVL_SKIP)) {
     mbmi->mode = ZEROMV;
+    assert(bsize >= BLOCK_8X8);
   } else {
     if (bsize >= BLOCK_8X8)
       mbmi->mode = read_inter_mode(cm, r, inter_mode_ctx);
