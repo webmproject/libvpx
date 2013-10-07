879e21ddfd2d76484c22590ddba655b77767f053
James Zern <jzern@google.com>
2013-10-07 11:45:16 +0200

    vp9_blockd.h: update get_tx_eob() signature
    as the name implies, the segmentation pointer can be const
    
    Change-Id: I945f01a077c112ec86c00e35a1e9395bc230c2d9

--- vp9/common/vp9_blockd.h
+++ vp9/common/vp9_blockd.h
@@ -578,7 +578,7 @@ static void set_contexts(MACROBLOCKD *xd, struct macroblockd_plane *pd,
   }
 }
 
-static int get_tx_eob(struct segmentation *seg, int segment_id,
+static int get_tx_eob(const struct segmentation *seg, int segment_id,
                       TX_SIZE tx_size) {
   const int eob_max = 16 << (tx_size << 1);
   return vp9_segfeature_active(seg, segment_id, SEG_LVL_SKIP) ? 0 : eob_max;
