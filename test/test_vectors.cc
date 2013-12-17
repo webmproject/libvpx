2424ba3d8b19c356acd8ec4a212895a43a2f0802
Yaowu Xu <yaowu@google.com>
2013-12-17 14:44:33 -0800

    Added a test vector that exercises lpf deltas
    From frame 2, the lpf deltas are all cleared for for even frames, and
    a set of values are set and used for odd frames. The intention is to
    exercise decoding code around lpf delta/update decoding.
    
    Change-Id: Ic9ff1bc2c2a023f4805852f8573398f2ec2249d7

--- test/test_vectors.cc
+++ test/test_vectors.cc
@@ -159,6 +159,7 @@ const char *kVP9TestVectors[kNumVp9TestVectors] = {
   "vp90-2-09-subpixel-00.ivf",
   "vp90-2-02-size-lf-1920x1080.webm",
   "vp90-2-09-aq2.webm",
+  "vp90-2-09-lf_deltas.webm",
 #if CONFIG_NON420
   "vp91-2-04-yv444.webm"
 #endif
