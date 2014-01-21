3fa1356994e2309b632d8863957c91f7f0813847
Jim Bankoski <jimbankoski@google.com>
2014-01-21 09:56:43 -0800

    remove complex rd modeling.
    The affect on quality was minimal.  Less than .1%, various sets
    yt ( +.15%), derf (-.1%), hd ( -.1% ), std hd(-.15%)...
    
    The affect on speed of encode at speed -5 was substantial ( ~3% ).
    
    Change-Id: I8903346fbae0c35f5b9ea20f81fdd239ae81247d

--- vp9/encoder/vp9_rdopt.c
+++ vp9/encoder/vp9_rdopt.c
@@ -419,18 +419,12 @@ static void model_rd_for_sb(VP9_COMP *cpi, BLOCK_SIZE bsize,
     struct macroblock_plane *const p = &x->plane[i];
     struct macroblockd_plane *const pd = &xd->plane[i];
     const BLOCK_SIZE bs = get_plane_block_size(bsize, pd);
-    int rate;
-    int64_t dist;
     (void) cpi->fn_ptr[bs].vf(p->src.buf, p->src.stride,
                               pd->dst.buf, pd->dst.stride, &sse);
     if (i == 0)
       x->pred_sse[ref] = sse;
-    // sse works better than var, since there is no dc prediction used
-    model_rd_from_var_lapndz(sse, 1 << num_pels_log2_lookup[bs],
-                             pd->dequant[1] >> 3, &rate, &dist);
 
-    rate_sum += rate;
-    dist_sum += (int)dist;
+    dist_sum += (int)sse;
   }
 
   *out_rate_sum = rate_sum;
