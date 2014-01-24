c2b336815dddcd9fab2d4fb8ee199b816ce8c347
Dmitry Kovalev <dkovalev@google.com>
2014-01-24 11:20:09 -0800

    Implementing simple API to read video files.
    New API is supposed to be used from example code. Current implementation
    only supports IVF containers (will be extended to Y4M).
    
    Change-Id: Ib7da87237690b1a28297bdf03bc41c6836a84b7e

--- examples/decode_to_md5.c
+++ examples/decode_to_md5.c
@@ -33,17 +33,19 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+
 #define VPX_CODEC_DISABLE_COMPAT 1
-#include "./vpx_config.h"
+
 #include "vpx/vp8dx.h"
 #include "vpx/vpx_decoder.h"
-#include "md5_utils.h"
+
+#include "./ivfdec.h"
+#include "./md5_utils.h"
+#include "./tools_common.h"
+#include "./vpx_config.h"
 
 #define VP8_FOURCC 0x30385056
 #define VP9_FOURCC 0x30395056
-
-#define IVF_FILE_HDR_SZ  (32)
-#define IVF_FRAME_HDR_SZ (12)
 
 static vpx_codec_iface_t *get_codec_interface(unsigned int fourcc) {
   switch (fourcc) {
@@ -53,20 +55,6 @@ static vpx_codec_iface_t *get_codec_interface(unsigned int fourcc) {
       return vpx_codec_vp9_dx();
   }
   return NULL;
-}
-
-static unsigned int mem_get_le32(const unsigned char *mem) {
-  return (mem[3] << 24) | (mem[2] << 16) | (mem[1] << 8) | (mem[0]);
-}
-
-static void die(const char *fmt, ...) {
-  va_list ap;
-
-  va_start(ap, fmt);
-  vprintf(fmt, ap);
-  if (fmt[strlen(fmt) - 1] != '\n')
-    printf("\n");
-  exit(EXIT_FAILURE);
 }
 
 static void die_codec(vpx_codec_ctx_t *ctx, const char *s) {
@@ -106,17 +94,24 @@ static void print_md5(FILE *stream, unsigned char digest[16]) {
     fprintf(stream, "%02x", digest[i]);
 }
 
+static const char *exec_name;
+
+void usage_exit() {
+  fprintf(stderr, "Usage: %s <infile> <outfile>\n", exec_name);
+  exit(EXIT_FAILURE);
+}
+
 int main(int argc, char **argv) {
   FILE *infile, *outfile;
   vpx_codec_ctx_t codec;
   vpx_codec_iface_t *iface;
   int flags = 0, frame_cnt = 0;
-  unsigned char file_hdr[IVF_FILE_HDR_SZ];
-  unsigned char frame_hdr[IVF_FRAME_HDR_SZ];
-  unsigned char frame[256 * 1024];
+  vpx_video_t *video;
+
+  exec_name = argv[0];
 
   if (argc != 3)
-    die("Usage: %s <infile> <outfile>\n", argv[0]);
+    die("Invalid number of arguments");
 
   if (!(infile = fopen(argv[1], "rb")))
     die("Failed to open %s for reading", argv[1]);
@@ -124,32 +119,24 @@ int main(int argc, char **argv) {
   if (!(outfile = fopen(argv[2], "wb")))
     die("Failed to open %s for writing", argv[2]);
 
-  if (!(fread(file_hdr, 1, IVF_FILE_HDR_SZ, infile) == IVF_FILE_HDR_SZ &&
-     file_hdr[0] == 'D' && file_hdr[1] == 'K' &&
-     file_hdr[2] == 'I' && file_hdr[3] == 'F'))
+  video = vpx_video_open_file(infile);
+  if (!video)
     die("%s is not an IVF file.", argv[1]);
 
-  iface = get_codec_interface(mem_get_le32(file_hdr + 8));
+  iface = get_codec_interface(vpx_video_get_fourcc(video));
   if (!iface)
     die("Unknown FOURCC code.");
-
 
   printf("Using %s\n", vpx_codec_iface_name(iface));
 
   if (vpx_codec_dec_init(&codec, iface, NULL, flags))
     die_codec(&codec, "Failed to initialize decoder");
 
-  while (fread(frame_hdr, 1, IVF_FRAME_HDR_SZ, infile) == IVF_FRAME_HDR_SZ) {
-    const int frame_size = mem_get_le32(frame_hdr);
+  while (vpx_video_read_frame(video)) {
     vpx_codec_iter_t iter = NULL;
-    vpx_image_t *img;
-
-    if (frame_size > sizeof(frame))
-      die("Frame %d data too big for example code buffer", frame_size);
-
-    if (fread(frame, 1, frame_size, infile) != frame_size)
-      die("Failed to read complete frame");
-
+    vpx_image_t *img = NULL;
+    size_t frame_size = 0;
+    const unsigned char *frame = vpx_video_get_frame(video, &frame_size);
     if (vpx_codec_decode(&codec, frame, frame_size, NULL, 0))
       die_codec(&codec, "Failed to decode frame");
 
@@ -166,6 +153,8 @@ int main(int argc, char **argv) {
   printf("Processed %d frames.\n", frame_cnt);
   if (vpx_codec_destroy(&codec))
     die_codec(&codec, "Failed to destroy codec");
+
+  vpx_video_close(video);
 
   fclose(outfile);
   fclose(infile);
