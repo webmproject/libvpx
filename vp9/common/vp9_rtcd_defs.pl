420f58f2d2657d16b31ba26a4aa8eb29cf2f830d
James Yu <james.yu@linaro.org>
2014-01-31 13:18:15 +0800

    VP9 common for ARMv8 by using NEON intrinsics 11
    Add vp9_idct16x16_1_add_neon.c
    - vp9_idct16x16_1_add_neon
    
    Change-Id: I7c6524024ad4cb4e66aa38f1c887e733503c39df
    Signed-off-by: James Yu <james.yu@linaro.org>

--- vp9/common/vp9_rtcd_defs.pl
+++ vp9/common/vp9_rtcd_defs.pl
@@ -440,8 +440,7 @@ if (vpx_config("CONFIG_VP9_HIGHBITDEPTH") eq "yes") {
     $vp9_idct8x8_12_add_neon_asm=vp9_idct8x8_12_add_neon;
 
     add_proto qw/void vp9_idct16x16_1_add/, "const tran_low_t *input, uint8_t *dest, int dest_stride";
-    specialize qw/vp9_idct16x16_1_add sse2 neon_asm dspr2/;
-    $vp9_idct16x16_1_add_neon_asm=vp9_idct16x16_1_add_neon;
+    specialize qw/vp9_idct16x16_1_add sse2 neon dspr2/;
 
     add_proto qw/void vp9_idct16x16_256_add/, "const tran_low_t *input, uint8_t *dest, int dest_stride";
     specialize qw/vp9_idct16x16_256_add sse2 ssse3 neon_asm dspr2/;
