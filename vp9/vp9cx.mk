9d6d35c5efeafc297bbb38e5581ac473be1328a1
Dmitry Kovalev <dkovalev@google.com>
2014-02-04 14:49:43 -0800

    Renaming vp9_variance_c.c to vp9_variance.c.
    Change-Id: I7b29cb18ad36d79e1c6329c7de88496059f49db4

--- vp9/vp9cx.mk
+++ vp9/vp9cx.mk
@@ -66,7 +66,7 @@ VP9_CX_SRCS-yes += encoder/vp9_resize.h
 VP9_CX_SRCS-$(CONFIG_INTERNAL_STATS) += encoder/vp9_ssim.c
 VP9_CX_SRCS-yes += encoder/vp9_tokenize.c
 VP9_CX_SRCS-yes += encoder/vp9_treewriter.c
-VP9_CX_SRCS-yes += encoder/vp9_variance_c.c
+VP9_CX_SRCS-yes += encoder/vp9_variance.c
 VP9_CX_SRCS-yes += encoder/vp9_vaq.c
 VP9_CX_SRCS-yes += encoder/vp9_vaq.h
 ifeq ($(CONFIG_VP9_POSTPROC),yes)
