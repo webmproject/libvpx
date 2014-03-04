a57cff2132e9cff8767c6df9ff05409d399f43ec
Jingning Han <jingning@google.com>
2014-03-04 10:24:25 -0800

    Move set_speed_feature out of frame encoding
    Set speed features before running frame encoding. This avoids
    redundant RD threshold calculation in key frame coding.
    
    Change-Id: If8e3cf2c02976baa59b310c1c23af9eea0c46e36

--- vp9/encoder/vp9_onyx_if.c
+++ vp9/encoder/vp9_onyx_if.c
@@ -3251,6 +3251,8 @@ static void encode_frame_to_data_rate(VP9_COMP *cpi,
     set_high_precision_mv(cpi, q < HIGH_PRECISION_MV_QTHRESH);
   }
 
+  vp9_set_speed_features(cpi);
+
   if (cpi->sf.recode_loop == DISALLOW_RECODE) {
     encode_without_recode_loop(cpi, size, dest, q);
   } else {
