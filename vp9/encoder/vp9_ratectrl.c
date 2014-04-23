38ff3e328f95c040f96de798d3d4ba3dd510ab43
Paul Wilkins <paulwilkins@google.com>
2014-04-22 17:09:47 -0700

    Rate control adjustment.
    Small adjustment to the active Q range calculations.
    These changes should slightly extend the available Q range
    for KF/GF/ARF and narrow it for other frames.
    
    The results for this change in isolation are broadly positive
    for SSIM and average PSNR and slightly up but mixed for opsnr.
    
    derf +0.293% opsnr, +1.286% SSIM
    std-hd + 0.528% opsnr, + 1.746% SSIM
    yt +0.056% opsnr, +0.457% SSIM
    yt-hd -0.147% opsnr, + 0.226% SSIM
    
    Change-Id: If065280342027ecc5d44b49fc1d440dfef041002

--- vp9/encoder/vp9_ratectrl.c
+++ vp9/encoder/vp9_ratectrl.c
@@ -81,13 +81,13 @@ void vp9_rc_init_minq_luts() {
 
   for (i = 0; i < QINDEX_RANGE; i++) {
     const double maxq = vp9_convert_qindex_to_q(i);
-    kf_low_motion_minq[i] = get_minq_index(maxq, 0.000001, -0.0004, 0.15);
+    kf_low_motion_minq[i] = get_minq_index(maxq, 0.000001, -0.0004, 0.125);
     kf_high_motion_minq[i] = get_minq_index(maxq, 0.000002, -0.0012, 0.50);
-    gf_low_motion_minq[i] = get_minq_index(maxq, 0.0000015, -0.0009, 0.32);
+    gf_low_motion_minq[i] = get_minq_index(maxq, 0.0000015, -0.0009, 0.30);
     gf_high_motion_minq[i] = get_minq_index(maxq, 0.0000021, -0.00125, 0.50);
-    afq_low_motion_minq[i] = get_minq_index(maxq, 0.0000015, -0.0009, 0.33);
+    afq_low_motion_minq[i] = get_minq_index(maxq, 0.0000015, -0.0009, 0.30);
     afq_high_motion_minq[i] = get_minq_index(maxq, 0.0000021, -0.00125, 0.55);
-    inter_minq[i] = get_minq_index(maxq, 0.00000271, -0.00113, 0.75);
+    inter_minq[i] = get_minq_index(maxq, 0.00000271, -0.00113, 0.90);
   }
 }
 
