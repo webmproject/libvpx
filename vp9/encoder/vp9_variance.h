e826b6130533571efff5007bc2f2a94098184318
Dmitry Kovalev <dkovalev@google.com>
2014-04-30 12:34:09 -0700

    Removing unused typedefs.
    Change-Id: I34ebc59980cf661ed658555e245bf0a93e5c3373

--- vp9/encoder/vp9_variance.h
+++ vp9/encoder/vp9_variance.h
@@ -69,13 +69,6 @@ typedef unsigned int (*vp9_subp_avg_variance_fn_t)(const uint8_t *src_ptr,
                                                    unsigned int *sse,
                                                    const uint8_t *second_pred);
 
-typedef unsigned int (*vp9_getmbss_fn_t)(const short *);
-
-typedef unsigned int (*vp9_get16x16prederror_fn_t)(const uint8_t *src_ptr,
-                                                   int source_stride,
-                                                   const uint8_t *ref_ptr,
-                                                   int  ref_stride);
-
 typedef struct vp9_variance_vtable {
   vp9_sad_fn_t               sdf;
   vp9_sad_avg_fn_t           sdaf;
