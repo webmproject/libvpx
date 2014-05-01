eae4a467a8dbeda683f247ec8434c4418d0e5fe2
Dmitry Kovalev <dkovalev@google.com>
2014-05-01 11:41:01 -0700

    Simplifying vp9_is_upper_layer_key_frame().
    Change-Id: Ib56df7cd282dadbfd202de23f0c746a93b5ce63e

--- vp9/encoder/vp9_svc_layercontext.c
+++ vp9/encoder/vp9_svc_layercontext.c
@@ -221,15 +221,9 @@ void vp9_inc_frame_in_layer(SVC *svc) {
   ++lc->current_video_frame_in_layer;
 }
 
-int vp9_is_upper_layer_key_frame(const struct VP9_COMP *const cpi) {
-  int is_upper_layer_key_frame = 0;
-
-  if (cpi->use_svc && cpi->svc.number_temporal_layers == 1 &&
-      cpi->svc.spatial_layer_id > 0) {
-    if (cpi->svc.layer_context[cpi->svc.spatial_layer_id].is_key_frame) {
-      is_upper_layer_key_frame = 1;
-    }
-  }
-
-  return is_upper_layer_key_frame;
+int vp9_is_upper_layer_key_frame(const VP9_COMP *cpi) {
+  return cpi->use_svc &&
+         cpi->svc.number_temporal_layers == 1 &&
+         cpi->svc.spatial_layer_id > 0 &&
+         cpi->svc.layer_context[cpi->svc.spatial_layer_id].is_key_frame;
 }
