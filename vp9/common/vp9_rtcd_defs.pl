9e7b09bc5d45e309c0deeb1c95981ccb7b857876
Jingning Han <jingning@google.com>
2014-05-02 16:29:08 -0700

    SSSE3 8x8 inverse 2D-DCT with first 10 coeffs non-zero
    This commit enables ssse3 assembly implementation of the 8x8
    inverse 2D-DCT with only first 10 coefficients non-zero. The
    average runtime for this unit goes down from 198 cycles to 129
    cycles (34.8% faster).
    
    Change-Id: Ie7fa4386f6d3a2fe0d47a2eb26fc2a6bbc592ac7

--- vp9/common/vp9_rtcd_defs.pl
+++ vp9/common/vp9_rtcd_defs.pl
@@ -313,7 +313,7 @@ add_proto qw/void vp9_idct8x8_64_add/, "const int16_t *input, uint8_t *dest, int
 specialize qw/vp9_idct8x8_64_add sse2 neon dspr2/, "$ssse3_x86_64";
 
 add_proto qw/void vp9_idct8x8_10_add/, "const int16_t *input, uint8_t *dest, int dest_stride";
-specialize qw/vp9_idct8x8_10_add sse2 neon dspr2/;
+specialize qw/vp9_idct8x8_10_add sse2 neon dspr2/, "$ssse3_x86_64";
 
 add_proto qw/void vp9_idct16x16_1_add/, "const int16_t *input, uint8_t *dest, int dest_stride";
 specialize qw/vp9_idct16x16_1_add sse2 neon dspr2/;
