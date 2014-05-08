b466ad5efc0cee97b5e3e31f7aeb72685beed45e
Jingning Han <jingning@google.com>
2014-05-08 09:56:38 -0700

    Turn on unit tests for SSSE3 8x8 forward and inverse 2D-DCT
    Change-Id: I3edd4b956a1273d65547771bf43c5cdaea25e5d6

--- test/partial_idct_test.cc
+++ test/partial_idct_test.cc
@@ -190,4 +190,13 @@ INSTANTIATE_TEST_CASE_P(
                    &vp9_idct4x4_1_add_sse2,
                    TX_4X4, 1)));
 #endif
+
+#if HAVE_SSSE3 && ARCH_X86_64
+INSTANTIATE_TEST_CASE_P(
+    SSSE3, PartialIDctTest,
+    ::testing::Values(
+        make_tuple(&vp9_idct8x8_64_add_c,
+                   &vp9_idct8x8_12_add_ssse3,
+                   TX_8X8, 12)));
+#endif
 }  // namespace
