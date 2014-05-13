051332a6a4a2247eeb42aa3034a54f44f5467950
Yaowu Xu <yaowu@google.com>
2014-05-13 10:44:40 -0700

    vp9_tokenize.c: cleanup -wextra warnings
    Change-Id: I85854f1ab2d60feea0bfe6b2141f89a998777ecb

--- vp9/encoder/vp9_tokenize.c
+++ vp9/encoder/vp9_tokenize.c
@@ -232,7 +232,6 @@ static void tokenize_b(int plane, int block, BLOCK_SIZE plane_bsize,
       cpi->common.fc.coef_probs[tx_size][type][ref];
   unsigned int (*const eob_branch)[COEFF_CONTEXTS] =
       cpi->common.counts.eob_branch[tx_size][type][ref];
-
   const uint8_t *const band = get_band_translate(tx_size);
   const int seg_eob = get_tx_eob(&cpi->common.seg, segment_id, tx_size);
 
@@ -294,6 +293,8 @@ static void is_skippable(int plane, int block,
                          BLOCK_SIZE plane_bsize, TX_SIZE tx_size,
                          void *argv) {
   struct is_skippable_args *args = argv;
+  (void)plane_bsize;
+  (void)tx_size;
   args->skippable[0] &= (!args->x->plane[plane].eobs[block]);
 }
 
