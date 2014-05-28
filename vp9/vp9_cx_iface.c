5c93c580f8d01dd824ff3c123e38c8c3a2a8644f
Deb Mukherjee <debargha@google.com>
2014-05-27 23:24:06 -0700

    Removing undeclared identifier - build fix
    Fixes build with --enable-internal-stats
    
    Change-Id: I137169c859f561478e45891defe976d595454166

--- vp9/vp9_cx_iface.c
+++ vp9/vp9_cx_iface.c
@@ -1033,8 +1033,6 @@ static vpx_codec_err_t ctrl_set_previewpp(vpx_codec_alg_priv_t *ctx,
                                           va_list args) {
 #if CONFIG_VP9_POSTPROC
   vp8_postproc_cfg_t *config = va_arg(args, vp8_postproc_cfg_t *);
-  (void)ctr_id;
-
   if (config != NULL) {
     ctx->preview_ppcfg = *config;
     return VPX_CODEC_OK;
