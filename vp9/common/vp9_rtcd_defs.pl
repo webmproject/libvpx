dd9f50293341e28de14462fb46bd60e06bdb2427
James Zern <jzern@google.com>
2014-06-08 18:25:37 -0700

    vp9_f(dct|ht): disable avx2 variants
    tests failing under Win32/Win64
    
    + dct16x16_test: add missing avx2 functions (partially disabled)
    
    exercises the forward transforms
    no idct/iht implementations, so the c-code is used
    
    Change-Id: I04f64a457fa0828a00f32b5c9fe4f55294f21f61

--- vp9/common/vp9_rtcd_defs.pl
+++ vp9/common/vp9_rtcd_defs.pl
@@ -739,7 +739,7 @@ add_proto qw/void vp9_fht8x8/, "const int16_t *input, int16_t *output, int strid
 specialize qw/vp9_fht8x8 sse2 avx2/;
 
 add_proto qw/void vp9_fht16x16/, "const int16_t *input, int16_t *output, int stride, int tx_type";
-specialize qw/vp9_fht16x16 sse2 avx2/;
+specialize qw/vp9_fht16x16 sse2/;
 
 add_proto qw/void vp9_fwht4x4/, "const int16_t *input, int16_t *output, int stride";
 specialize qw/vp9_fwht4x4/, "$mmx_x86inc";
@@ -751,7 +751,7 @@ add_proto qw/void vp9_fdct8x8/, "const int16_t *input, int16_t *output, int stri
 specialize qw/vp9_fdct8x8 sse2 avx2/, "$ssse3_x86_64";
 
 add_proto qw/void vp9_fdct16x16/, "const int16_t *input, int16_t *output, int stride";
-specialize qw/vp9_fdct16x16 sse2 avx2/;
+specialize qw/vp9_fdct16x16 sse2/;
 
 add_proto qw/void vp9_fdct32x32/, "const int16_t *input, int16_t *output, int stride";
 specialize qw/vp9_fdct32x32 sse2 avx2/;
