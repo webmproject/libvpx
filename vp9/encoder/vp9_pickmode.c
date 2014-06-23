9e558344260bfebf16142c9faa6b06efdceb8173
Jingning Han <jingning@google.com>
2014-06-23 15:52:16 -0700

    Replace cpi->common with preset variable cm
    This commit replaces a few use cases of cpi->common with preset
    variable cm, to avoid unnecessary pointer fetch in the non-RD
    coding mode.
    
    Change-Id: I4038f1c1a47373b8fd7bc5d69af61346103702f6

--- vp9/encoder/vp9_pickmode.c
+++ vp9/encoder/vp9_pickmode.c
@@ -241,9 +241,9 @@ int64_t vp9_pick_inter_mode(VP9_COMP *cpi, MACROBLOCK *x,
   mbmi->ref_frame[0] = NONE;
   mbmi->ref_frame[1] = NONE;
   mbmi->tx_size = MIN(max_txsize_lookup[bsize],
-                      tx_mode_to_biggest_tx_size[cpi->common.tx_mode]);
-  mbmi->interp_filter = cpi->common.interp_filter == SWITCHABLE ?
-                        EIGHTTAP : cpi->common.interp_filter;
+                      tx_mode_to_biggest_tx_size[cm->tx_mode]);
+  mbmi->interp_filter = cm->interp_filter == SWITCHABLE ?
+                        EIGHTTAP : cm->interp_filter;
   mbmi->skip = 0;
   mbmi->segment_id = segment_id;
 
