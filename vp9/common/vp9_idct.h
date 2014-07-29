0a2b25dcb9e3a2ad6f4d8f1bbb9a192429f32831
Yaowu Xu <yaowu@google.com>
2014-07-29 13:40:55 -0700

    configure: add --enable-coefficient-range-checking
    This commit adds a configure time option used to enable strict error
    checking in decoder to make sure intermediate stage cofficients of
    inverse transforms are within valid range of signed 16 bit integer.
    
    For valid VP9 input streams, intermediate stage coefficients should
    always stay within the range of a signed 16 bit integer. Coefficients
    can go out of this range for invalid/corrupt VP9 streams. However,
    strictly checking this range for every intermediate coefficient can
    be a burden for decoder, therefore such validation is only enabled
    with configure option --enable-coefficient-range-checking.
    
    Change-Id: I47d47c8c4e48a922c3d223ca59064f51b3f0f5ed

--- vp9/common/vp9_idct.h
+++ vp9/common/vp9_idct.h
@@ -81,6 +81,16 @@ static const int sinpi_4_9 = 15212;
 
 static INLINE int dct_const_round_shift(int input) {
   int rv = ROUND_POWER_OF_TWO(input, DCT_CONST_BITS);
+#if CONFIG_COEFFICIENT_RANGE_CHECKING
+  // For valid VP9 input streams, intermediate stage coefficients should always
+  // stay within the range of a signed 16 bit integer. Coefficients can go out
+  // of this range for invalid/corrupt VP9 streams. However, strictly checking
+  // this range for every intermediate coefficient can burdensome for a decoder,
+  // therefore the following assertion is only enabled when configured with
+  // --enable-coefficient-range-checking.
+  assert(INT16_MIN <= rv);
+  assert(rv <= INT16_MAX);
+#endif
   return (int16_t)rv;
 }
 
