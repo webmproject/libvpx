a5da7dea39d78c9d849406058df4231d50d474d6
James Zern <jzern@google.com>
2014-08-30 18:05:56 -0700

    vp9_dthread: simplify loop_filter_row_worker signature
    use the type names directly in the function declaration rather than
    (void *arg1, void *arg2)
    
    Change-Id: If8887e1dbcdf84842783a92f91668bef6223c9e5

--- vp9/decoder/vp9_dthread.c
+++ vp9/decoder/vp9_dthread.c
@@ -121,10 +121,10 @@ static void loop_filter_rows_mt(const YV12_BUFFER_CONFIG *const frame_buffer,
 }
 
 // Row-based multi-threaded loopfilter hook
-static int loop_filter_row_worker(void *arg1, void *arg2) {
-  TileWorkerData *const tile_data = (TileWorkerData*)arg1;
+static int loop_filter_row_worker(TileWorkerData *const tile_data,
+                                  void *unused) {
   LFWorkerData *const lf_data = &tile_data->lfdata;
-  (void) arg2;
+  (void)unused;
   loop_filter_rows_mt(lf_data->frame_buffer, lf_data->cm, lf_data->planes,
                       lf_data->start, lf_data->stop, lf_data->y_only,
                       lf_data->lf_sync, lf_data->num_lf_workers);
