15a3e5f74234b96559a4b116f8dca50901367587
hkuang <hkuang@google.com>
2014-10-09 11:58:39 -0700

    Remove unnecessary scale check in set_ref.
    Scale check has been done in read_inter_block_mode_info.
    
    Change-Id: I6c86f93bd579109ed30ff13a04a30e35f5ae6fc5

--- vp9/decoder/vp9_decodeframe.c
+++ vp9/decoder/vp9_decodeframe.c
@@ -389,9 +389,6 @@ static void set_ref(VP9_COMMON *const cm, MACROBLOCKD *const xd,
   MB_MODE_INFO *const mbmi = &xd->mi[0].src_mi->mbmi;
   RefBuffer *ref_buffer = &cm->frame_refs[mbmi->ref_frame[idx] - LAST_FRAME];
   xd->block_refs[idx] = ref_buffer;
-  if (!vp9_is_valid_scale(&ref_buffer->sf))
-    vpx_internal_error(&cm->error, VPX_CODEC_UNSUP_BITSTREAM,
-                       "Invalid scale factors");
   if (ref_buffer->buf->corrupted)
     vpx_internal_error(&cm->error, VPX_CODEC_CORRUPT_FRAME,
                        "Block reference is corrupt");
