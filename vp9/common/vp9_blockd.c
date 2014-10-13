c5fd035ce02e2b5499abb97dad95315ac1d9c176
hkuang <hkuang@google.com>
2014-10-13 14:04:16 -0700

    Use pre increment.
    Change-Id: I016b4e77d8268e189473f4c382603afe1ae1750f

--- vp9/common/vp9_blockd.c
+++ vp9/common/vp9_blockd.c
@@ -92,7 +92,7 @@ void vp9_foreach_transformed_block(const MACROBLOCKD* const xd,
                                    void *arg) {
   int plane;
 
-  for (plane = 0; plane < MAX_MB_PLANE; plane++)
+  for (plane = 0; plane < MAX_MB_PLANE; ++plane)
     vp9_foreach_transformed_block_in_plane(xd, bsize, plane, visit, arg);
 }
 
