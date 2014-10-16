db7192e0b014a331a1dcb102c8a1148e9f0e1081
Peter de Rivaz <peter.derivaz@gmail.com>
2014-10-16 13:38:46 +0100

    Added highbitdepth sse2 acceleration for quantize and block error
    Change-Id: Idef18f90b111a0d0c9546543d3347e551908fd78

--- test/test.mk
+++ test/test.mk
@@ -134,6 +134,8 @@ LIBVPX_TEST_SRCS-$(CONFIG_VP9_ENCODER) += variance_test.cc
 LIBVPX_TEST_SRCS-$(CONFIG_VP9_ENCODER) += vp9_subtract_test.cc
 LIBVPX_TEST_SRCS-$(CONFIG_VP9_ENCODER) += lpf_8_test.cc
 LIBVPX_TEST_SRCS-$(CONFIG_VP9_ENCODER) += vp9_avg_test.cc
+LIBVPX_TEST_SRCS-$(CONFIG_VP9_ENCODER) += quantize_test.cc
+LIBVPX_TEST_SRCS-$(CONFIG_VP9_ENCODER) += error_block_test.cc
 LIBVPX_TEST_SRCS-$(CONFIG_VP9)         += vp9_intrapred_test.cc
 
 ifeq ($(CONFIG_VP9_ENCODER),yes)
