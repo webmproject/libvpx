c42715b72142c7f4f6ddebe0394b18589133c1ed
Jingning Han <jingning@google.com>
2014-11-18 12:37:59 -0800

    Enable ssse3 version of vp9_fdct8x8_quant
    It improves the speed performance of vp9_fdct8x8_quant_sse2 by
    about 5%.
    
    Change-Id: I74b093ba4d81df64caf71ac7693f3d917f673097

--- vp9/vp9cx.mk
+++ vp9/vp9cx.mk
@@ -131,6 +131,7 @@ VP9_CX_SRCS-$(HAVE_AVX2) += encoder/x86/vp9_sad_intrin_avx2.c
 VP9_CX_SRCS-$(ARCH_X86_64) += encoder/x86/vp9_ssim_opt_x86_64.asm
 
 VP9_CX_SRCS-$(HAVE_SSE2) += encoder/x86/vp9_dct_sse2.c
+VP9_CX_SRCS-$(HAVE_SSSE3) += encoder/x86/vp9_dct_ssse3.c
 VP9_CX_SRCS-$(HAVE_SSE2) += encoder/x86/vp9_dct32x32_sse2.c
 
 ifeq ($(CONFIG_VP9_TEMPORAL_DENOISING),yes)
