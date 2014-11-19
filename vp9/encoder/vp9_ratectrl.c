21db24efcb9978244a5c10e70ce74f7ffcb04444
Yaowu Xu <yaowu@google.com>
2014-11-19 15:32:11 -0800

    Add a reset to rc tracking for dropped frames
    VP9/DatarateTestVP9Large.ChangingDropFrameThresh/[34] fails post the
    merge of commit#ffa06b37. This commit adds reset of rc tracking info
    when frame is dropped, and fixes the causes of the bad interaction
    between the tests and the previous commit.
    
    Change-Id: I848acfd9fcb336359662274325190f94aac76eae

--- vp9/encoder/vp9_ratectrl.c
+++ vp9/encoder/vp9_ratectrl.c
@@ -1289,6 +1289,8 @@ void vp9_rc_postencode_update_drop_frame(VP9_COMP *cpi) {
   cpi->common.last_frame_type = cpi->common.frame_type;
   cpi->rc.frames_since_key++;
   cpi->rc.frames_to_key--;
+  cpi->rc.rc_2_frame = 0;
+  cpi->rc.rc_1_frame = 0;
 }
 
 // Use this macro to turn on/off use of alt-refs in one-pass mode.
