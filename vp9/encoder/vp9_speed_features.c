a6df0cbcca923862c26abf775c3303413fc1b8b9
Jingning Han <jingning@google.com>
2014-11-25 18:34:42 -0800

    Remove repeated search_type_check_frequency assign
    This parameter is initialized as 50. No need to re-assign the
    same value in speed -6.
    
    Change-Id: I8735a5593412df2fdcee53ae45c8ebd1c3d792e7

--- vp9/encoder/vp9_speed_features.c
+++ vp9/encoder/vp9_speed_features.c
@@ -322,7 +322,6 @@ static void set_rt_speed_feature(VP9_COMP *cpi, SPEED_FEATURES *sf,
 
     // Turn on this to use non-RD key frame coding mode.
     // sf->use_nonrd_pick_mode = 1;
-    sf->search_type_check_frequency = 50;
     sf->mv.search_method = NSTEP;
     sf->tx_size_search_method = is_keyframe ? USE_LARGESTALL : USE_TX_8X8;
     sf->mv.reduce_first_step_size = 1;
