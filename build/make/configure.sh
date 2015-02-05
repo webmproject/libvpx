769fe6e2b38d9dbb8b60f3af9e0555614d77caf8
James Zern <jzern@google.com>
2015-02-04 19:16:12 -0800

    configure: enable x86inc for all intel platforms
    there are no known issues since:
    10d5e09 Fix issues in 32bit PIC enabled build
    
    related issues: #808, #924
    
    Change-Id: I80454f95fe6b4ce630fdd434d740ce8b0d42951b

--- build/make/configure.sh
+++ build/make/configure.sh
@@ -1259,14 +1259,7 @@ EOF
     fi
   fi
 
-  tgt_os_no_version=$(echo "${tgt_os}" | tr -d "[0-9]")
-  if [ "${tgt_os_no_version}" = "openbsd" ] || [ "`uname`" = "OpenBSD" ]; then
-    openbsd_like=yes
-  fi
-  # Default use_x86inc to yes when we are 64 bit, non-pic, or on any
-  # non-Darwin target.
-  if [ "${tgt_isa}" = "x86_64" ] || [ "${pic}" != "yes" ] || \
-      [ "${openbsd_like}" != "yes" ]; then
+  if [ "${tgt_isa}" = "x86_64" ] || [ "${tgt_isa}" = "x86" ]; then
     soft_enable use_x86inc
   fi
 
