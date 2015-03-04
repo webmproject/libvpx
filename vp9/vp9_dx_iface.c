31fae6ac08ef45bc93f279bedbc7d4c0bebee2c5
hkuang <hkuang@google.com>
2015-03-04 11:40:20 -0800

    Fix variable shadowing.
    Change-Id: I287c61ad4aa85654aef3c871a62131bcd0d891ac

--- vp9/vp9_dx_iface.c
+++ vp9/vp9_dx_iface.c
@@ -500,7 +500,6 @@ static vpx_codec_err_t decode_one(vpx_codec_alg_priv_t *ctx,
 
     check_resync(ctx, frame_worker_data->pbi);
   } else {
-    const VP9WorkerInterface *const winterface = vp9_get_worker_interface();
     VP9Worker *const worker = &ctx->frame_workers[ctx->next_submit_worker_id];
     FrameWorkerData *const frame_worker_data = (FrameWorkerData *)worker->data1;
     // Copy context from last worker thread to next worker thread.
