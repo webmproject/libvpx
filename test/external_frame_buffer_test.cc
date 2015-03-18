b373301e1fa1ac229a08858442f3621cc7022feb
James Zern <jzern@google.com>
2015-03-18 14:37:20 -0700

    external_frame_buffer_test: quiet static analysis warnings
    add explicit returns in cases where ASSERT_* can't be used due to the
    function returning a value; retain the EXPECT_* for reporting purposes.
    
    Change-Id: I1f514728537fee42a99277d3aba538e832d3b65b

--- test/external_frame_buffer_test.cc
+++ test/external_frame_buffer_test.cc
@@ -97,13 +97,19 @@ class ExternalFrameBufferList {
     return 0;
   }
 
-  // Marks the external frame buffer that |fb| is pointing too as free.
+  // Marks the external frame buffer that |fb| is pointing to as free.
   // Returns < 0 on an error.
   int ReturnFrameBuffer(vpx_codec_frame_buffer_t *fb) {
-    EXPECT_TRUE(fb != NULL);
+    if (fb == NULL) {
+      EXPECT_TRUE(fb != NULL);
+      return -1;
+    }
     ExternalFrameBuffer *const ext_fb =
         reinterpret_cast<ExternalFrameBuffer*>(fb->priv);
-    EXPECT_TRUE(ext_fb != NULL);
+    if (ext_fb == NULL) {
+      EXPECT_TRUE(ext_fb != NULL);
+      return -1;
+    }
     EXPECT_EQ(1, ext_fb->in_use);
     ext_fb->in_use = 0;
     return 0;
