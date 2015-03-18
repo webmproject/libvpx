eb8c667570aa83134c7db0690de9dbdde4d90291
Marco <marpan@google.com>
2015-03-18 13:55:19 -0700

    Force_split on 16x16 blocks in variance partition.
    Force split on 16x16 block (to 8x8) based on the minmax over the 8x8 sub-blocks.
    
    Also increase variance threshold for 32x32, and add exit condiiton in choose_partition
    (with very safe threshold) based on sad used to select reference frame.
    
    Some visual improvement near moving boundaries.
    Average gain in psnr/ssim: ~0.6%, some clips go up ~1 or 2%.
    Encoding time increase (due to more 8x8 blocks) from ~1-4%, depending on clip.
    
    Change-Id: I4759bb181251ac41517cd45e326ce2997dadb577

--- vp9/common/vp9_rtcd_defs.pl
+++ vp9/common/vp9_rtcd_defs.pl
@@ -1114,6 +1114,9 @@ specialize qw/vp9_avg_8x8 sse2 neon/;
 add_proto qw/unsigned int vp9_avg_4x4/, "const uint8_t *, int p";
 specialize qw/vp9_avg_4x4 sse2/;
 
+add_proto qw/void vp9_minmax_8x8/, "const uint8_t *s, int p, const uint8_t *d, int dp, int *min, int *max";
+specialize qw/vp9_minmax_8x8 sse2/;
+
 add_proto qw/void vp9_hadamard_8x8/, "int16_t const *src_diff, int src_stride, int16_t *coeff";
 specialize qw/vp9_hadamard_8x8 sse2/, "$ssse3_x86_64";
 
@@ -1137,6 +1140,8 @@ if (vpx_config("CONFIG_VP9_HIGHBITDEPTH") eq "yes") {
   specialize qw/vp9_highbd_avg_8x8/;
   add_proto qw/unsigned int vp9_highbd_avg_4x4/, "const uint8_t *, int p";
   specialize qw/vp9_highbd_avg_4x4/;
+  add_proto qw/unsigned int vp9_highbd_minmax_8x8/, "const uint8_t *s, int p, const uint8_t *d, int dp, int *min, int *max";
+  specialize qw/vp9_highbd_minmax_8x8/;
 }
 
 # ENCODEMB INVOKE
