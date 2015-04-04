7f629dfca4a2ea8c53fe0b42c0a7498edc7a7f72
Jingning Han <jingning@google.com>
2015-04-04 09:48:18 -0700

    SSSE3 assembly implementation of 8x8 Hadamard transform
    It uses about 10% less CPU cycles than the SSE2 intrinsic
    implementation.
    
    Change-Id: I91017c0c068679a214b98cdd4cff3a6facfb7499

--- vp9/common/vp9_rtcd_defs.pl
+++ vp9/common/vp9_rtcd_defs.pl
@@ -1110,7 +1110,7 @@ add_proto qw/unsigned int vp9_avg_4x4/, "const uint8_t *, int p";
 specialize qw/vp9_avg_4x4 sse2/;
 
 add_proto qw/void vp9_hadamard_8x8/, "int16_t const *src_diff, int src_stride, int16_t *coeff";
-specialize qw/vp9_hadamard_8x8 sse2/;
+specialize qw/vp9_hadamard_8x8 sse2/, "$ssse3_x86_64";
 
 add_proto qw/void vp9_hadamard_16x16/, "int16_t const *src_diff, int src_stride, int16_t *coeff";
 specialize qw/vp9_hadamard_16x16 sse2/;
