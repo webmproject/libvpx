1777413a2a160f3d2585e54ff155dc876c5bca1b
Jim Bankoski <jimbankoski@google.com>
2015-04-17 11:13:18 -0700

    Adds a blockiness metric to internal stats.
    Change-Id: Iedceeb020492050063acf3fd2326f96c29db9ae5

--- vp9/vp9cx.mk
+++ vp9/vp9cx.mk
@@ -81,6 +81,8 @@ VP9_CX_SRCS-yes += encoder/vp9_resize.c
 VP9_CX_SRCS-yes += encoder/vp9_resize.h
 VP9_CX_SRCS-$(CONFIG_INTERNAL_STATS) += encoder/vp9_ssim.c
 VP9_CX_SRCS-$(CONFIG_INTERNAL_STATS) += encoder/vp9_ssim.h
+VP9_CX_SRCS-$(CONFIG_INTERNAL_STATS) += encoder/vp9_blockiness.c
+
 VP9_CX_SRCS-yes += encoder/vp9_tokenize.c
 VP9_CX_SRCS-yes += encoder/vp9_treewriter.c
 VP9_CX_SRCS-yes += encoder/vp9_variance.c
