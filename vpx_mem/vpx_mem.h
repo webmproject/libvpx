3e9263e1cadaccbc454ebbb35b60e32211763b6a
James Zern <jzern@google.com>
2015-04-23 20:31:59 -0700

    vpx_mem: remove REPLACE_BUILTIN_FUNCTIONS
    vestigial. there are other ways to achieve this.
    
    Change-Id: Ie977ce952527e596479df3084f331b694a682e39

--- vpx_mem/vpx_mem.h
+++ vpx_mem/vpx_mem.h
@@ -17,11 +17,6 @@
 # include <lddk.h>
 #endif
 
-#ifndef REPLACE_BUILTIN_FUNCTIONS
-# define REPLACE_BUILTIN_FUNCTIONS 0  /* replace builtin functions with their
-vpx_ equivalents */
-#endif
-
 #include <stdlib.h>
 #include <stddef.h>
 
@@ -41,24 +36,6 @@ extern "C" {
   void *vpx_memset16(void *dest, int val, size_t length);
 #endif
   void *vpx_memmove(void *dest, const void *src, size_t count);
-
-  /* some defines for backward compatibility */
-#define DMEM_GENERAL 0
-
-// (*)<
-
-#if REPLACE_BUILTIN_FUNCTIONS
-# ifndef __VPX_MEM_C__
-#  define memalign vpx_memalign
-#  define malloc   vpx_malloc
-#  define calloc   vpx_calloc
-#  define realloc  vpx_realloc
-#  define free     vpx_free
-#  define memcpy   vpx_memcpy
-#  define memmove  vpx_memmove
-#  define memset   vpx_memset
-# endif
-#endif
 
 #ifndef __VPX_MEM_C__
 # include <string.h>
