5504b39f5e14e69e7b0b0609384fa71e88cb9840
hkuang <hkuang@google.com>
2015-05-01 11:09:30 -0700

    Remove a line.
    Change-Id: I2965d25efafeee487b4de75fddfa22e4907525ed

--- vp9/decoder/vp9_decodeframe.c
+++ vp9/decoder/vp9_decodeframe.c
@@ -47,7 +47,6 @@ static int is_compound_reference_allowed(const VP9_COMMON *cm) {
   for (i = 1; i < REFS_PER_FRAME; ++i)
     if (cm->ref_frame_sign_bias[i + 1] != cm->ref_frame_sign_bias[1])
       return 1;
-
   return 0;
 }
 
