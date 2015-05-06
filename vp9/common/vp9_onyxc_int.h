240767b29db9fa0b89699c0b144cb9eacf1584fc
hkuang <hkuang@google.com>
2015-05-06 14:15:00 -0700

    Remove an unnecessary check.
    Change-Id: Id0f224ac4667dd173363b0f05711678448291d4e

--- vp9/common/vp9_onyxc_int.h
+++ vp9/common/vp9_onyxc_int.h
@@ -380,7 +380,7 @@ static INLINE void set_mi_row_col(MACROBLOCKD *xd, const TileInfo *const tile,
   xd->left_available  = (mi_col > tile->mi_col_start);
   if (xd->up_available) {
     xd->above_mi = xd->mi[-xd->mi_stride];
-    xd->above_mbmi = xd->above_mi ? &xd->above_mi->mbmi : NULL;
+    xd->above_mbmi = &xd->above_mi->mbmi;
   } else {
     xd->above_mi = NULL;
     xd->above_mbmi = NULL;
@@ -388,7 +388,7 @@ static INLINE void set_mi_row_col(MACROBLOCKD *xd, const TileInfo *const tile,
 
   if (xd->left_available) {
     xd->left_mi = xd->mi[-1];
-    xd->left_mbmi = xd->left_mi ? &xd->left_mi->mbmi : NULL;
+    xd->left_mbmi = &xd->left_mi->mbmi;
   } else {
     xd->left_mi = NULL;
     xd->left_mbmi = NULL;
