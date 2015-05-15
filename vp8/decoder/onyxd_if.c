632177fa7f3547484ac728bb9ce009225b683fed
James Zern <jzern@google.com>
2015-05-14 19:59:32 -0700

    vp8: make some functions static
    silences missing prototype warnings
    
    Change-Id: I9f24a3214c832c982ca0dc5a032316eba48472ff

--- vp8/decoder/onyxd_if.c
+++ vp8/decoder/onyxd_if.c
@@ -259,7 +259,7 @@ static int swap_frame_buffers (VP8_COMMON *cm)
     return err;
 }
 
-int check_fragments_for_errors(VP8D_COMP *pbi)
+static int check_fragments_for_errors(VP8D_COMP *pbi)
 {
     if (!pbi->ec_active &&
         pbi->fragments.count <= 1 && pbi->fragments.sizes[0] == 0)
