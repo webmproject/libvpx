97db651ce0ae082e1bb69687b643c9e0fcb90d9d
James Zern <jzern@google.com>
2015-05-14 20:21:01 -0700

    vp9: add some missing includes
    mostly: <file>.c should include <file>.h
    silences missing prototype warnings
    
    Change-Id: Ic05ec32c6f7b2224b78825904d96d73aacad6000

--- vp9/common/vp9_alloccommon.c
+++ vp9/common/vp9_alloccommon.c
@@ -11,6 +11,7 @@
 #include "./vpx_config.h"
 #include "vpx_mem/vpx_mem.h"
 
+#include "vp9/common/vp9_alloccommon.h"
 #include "vp9/common/vp9_blockd.h"
 #include "vp9/common/vp9_entropymode.h"
 #include "vp9/common/vp9_entropymv.h"
