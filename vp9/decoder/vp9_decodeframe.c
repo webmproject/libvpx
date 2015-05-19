97adfb32bd9a77de87bd611e855ea81b4ce3525d
Jingning Han <jingning@google.com>
2015-05-19 15:41:57 -0700

    Support up to 1024 row/column tiles in bit-stream syntax
    Add syntax support to allow up to 1024 row/column tiles
    respectively.
    
    Change-Id: I84278589364b658d6c4dd05f0771795f732ad68f

--- vp9/decoder/vp9_decodeframe.c
+++ vp9/decoder/vp9_decodeframe.c
@@ -1869,7 +1869,7 @@ static void setup_tile_info(VP9_COMMON *cm, struct vp9_read_bit_buffer *rb) {
   max_ones = max_log2_tiles - min_log2_tiles;
   cm->log2_tile_cols = min_log2_tiles;
   while (max_ones-- && vp9_rb_read_bit(rb))
-    cm->log2_tile_cols++;
+    ++cm->log2_tile_cols;
 
   if (cm->log2_tile_cols > 10)
     vpx_internal_error(&cm->error, VPX_CODEC_CORRUPT_FRAME,
@@ -1877,6 +1877,12 @@ static void setup_tile_info(VP9_COMMON *cm, struct vp9_read_bit_buffer *rb) {
 
   // rows
 #if CONFIG_ROW_TILE
+  vp9_get_tile_n_bits(cm->mi_rows, &min_log2_tiles, &max_log2_tiles);
+  max_ones = max_log2_tiles - min_log2_tiles;
+  cm->log2_tile_rows = min_log2_tiles;
+  while (max_ones-- && vp9_rb_read_bit(rb))
+    ++cm->log2_tile_rows;
+
   if (cm->log2_tile_rows > 10)
     vpx_internal_error(&cm->error, VPX_CODEC_CORRUPT_FRAME,
                        "Invalid number of tile columns");
