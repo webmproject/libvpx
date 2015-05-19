46afda09088cdd668287a3d8533d58b16401a3fe
Jingning Han <jingning@google.com>
2015-05-19 15:26:40 -0700

    Set row and col tile number upper limit as 1024
    Increase the supported row and col tile numbers from 4 and 64,
    respectively to 1024 both.
    
    Change-Id: Ic07435ff6783940a466a549a69a11ab3faf3247a

--- vp9/decoder/vp9_decodeframe.c
+++ vp9/decoder/vp9_decodeframe.c
@@ -1871,14 +1871,20 @@ static void setup_tile_info(VP9_COMMON *cm, struct vp9_read_bit_buffer *rb) {
   while (max_ones-- && vp9_rb_read_bit(rb))
     cm->log2_tile_cols++;
 
-  if (cm->log2_tile_cols > 6)
+  if (cm->log2_tile_cols > 10)
     vpx_internal_error(&cm->error, VPX_CODEC_CORRUPT_FRAME,
                        "Invalid number of tile columns");
 
   // rows
+#if CONFIG_ROW_TILE
+  if (cm->log2_tile_rows > 10)
+    vpx_internal_error(&cm->error, VPX_CODEC_CORRUPT_FRAME,
+                       "Invalid number of tile columns");
+#else
   cm->log2_tile_rows = vp9_rb_read_bit(rb);
   if (cm->log2_tile_rows)
     cm->log2_tile_rows += vp9_rb_read_bit(rb);
+#endif
 }
 
 typedef struct TileBuffer {
