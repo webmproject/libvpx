ef2b3cce50cf2c71cefb6bfe4e1aca8c5bd04e3b
James Zern <jzern@google.com>
2015-05-20 20:14:30 -0700

    add vp9/common/x86/convolve.h
    collect the vp9_convolve function definition macros there; this will
    allow some relocation of functions from vp9_asm_stubs.c
    
    Change-Id: Idadd117fa256dd48748379856973fd985b8204e8

--- vp9/vp9_common.mk
+++ vp9/vp9_common.mk
@@ -69,6 +69,7 @@ VP9_COMMON_SRCS-yes += common/vp9_common_data.h
 VP9_COMMON_SRCS-yes += common/vp9_scan.c
 VP9_COMMON_SRCS-yes += common/vp9_scan.h
 
+VP9_COMMON_SRCS-$(ARCH_X86)$(ARCH_X86_64) += common/x86/convolve.h
 VP9_COMMON_SRCS-$(ARCH_X86)$(ARCH_X86_64) += common/x86/vp9_asm_stubs.c
 VP9_COMMON_SRCS-$(ARCH_X86)$(ARCH_X86_64) += common/x86/vp9_loopfilter_intrin_sse2.c
 VP9_COMMON_SRCS-$(HAVE_AVX2) += common/x86/vp9_loopfilter_intrin_avx2.c
