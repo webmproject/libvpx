1b3d77536626515f9040b8646cbf72daf9a59747
James Zern <jzern@google.com>
2015-09-10 12:54:01 -0700

    build: modify default ARFLAGS / .a target
    remove 'u' and specify all objects to allow objects with the same
    basename to be added and a incremental rebuild to succeed
    
    fixes issue #1067
    
    Change-Id: Id0ebc89be826a026f1bbf21b4e32a2b1af45154d

--- build/make/configure.sh
+++ build/make/configure.sh
@@ -428,7 +428,7 @@ NM=${NM}
 
 CFLAGS  = ${CFLAGS}
 CXXFLAGS  = ${CXXFLAGS}
-ARFLAGS = -rus\$(if \$(quiet),c,v)
+ARFLAGS = -crs\$(if \$(quiet),,v)
 LDFLAGS = ${LDFLAGS}
 ASFLAGS = ${ASFLAGS}
 extralibs = ${extralibs}
