6002212d2b56b62f680450641e568138b9086601
Tom Finegan <tomfinegan@google.com>
2015-09-23 11:42:37 -0700

    build/make/configure.sh: docs for soft_{dis|en}able.
    Add function comments explaining what the functions do and do not do.
    
    Change-Id: I23dea09f93bc5cdbea6a0077f90683a1df2f74dc

--- build/make/configure.sh
+++ build/make/configure.sh
@@ -201,6 +201,10 @@ disabled(){
   eval test "x\$$1" = "xno"
 }
 
+# Iterates through positional parameters, checks to confirm the parameter has
+# not been explicitly (force) disabled, and enables the setting controlled by
+# the parameter when the setting is not disabled.
+# Note: Does NOT alter RTCD generation options ($RTCD_OPTIONS).
 soft_enable() {
   for var in $*; do
     if ! disabled $var; then
@@ -210,6 +214,10 @@ soft_enable() {
   done
 }
 
+# Iterates through positional parameters, checks to confirm the parameter has
+# not been explicitly (force) enabled, and disables the setting controlled by
+# the parameter when the setting is not enabled.
+# Note: Does NOT alter RTCD generation options ($RTCD_OPTIONS).
 soft_disable() {
   for var in $*; do
     if ! enabled $var; then
