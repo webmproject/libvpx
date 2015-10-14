0149fb3d6b24f1df7f215ca12a2d8c8f70fd64f3
paulwilkins <paulwilkins@google.com>
2015-10-14 10:38:49 +0100

    Changes to exhaustive motion search.
    This change alters the nature and use of exhaustive motion search.
    
    Firstly any exhaustive search is preceded by a normal step search.
    The exhaustive search is only carried out if the distortion resulting
    from the step search is above a threshold value.
    
    Secondly the simple +/- 64 exhaustive search is replaced by a
    multi stage mesh based search where each stage has a range
    and step/interval size. Subsequent stages use the best position from
    the previous stage as the center of the search but use a reduced range
    and interval size.
    
    For example:
      stage 1: Range +/- 64 interval 4
      stage 2: Range +/- 32 interval 2
      stage 3: Range +/- 15 interval 1
    
    This process, especially when it follows on from a normal step
    search, has shown itself to be almost as effective as a full range
    exhaustive search with step 1 but greatly lowers the computational
    complexity such that it can be used in some cases for speeds 0-2.
    
    This patch also removes a double exhaustive search for sub 8x8 blocks
    which also contained  a bug (the two searches used different distortion
    metrics).
    
    For best quality in my test animation sequence this patch has almost
    no impact on quality but improves encode speed by more than 5X.
    
    Restricted use in good quality speeds 0-2 yields significant quality gains
    on the animation test of 0.2 - 0.5 db with only a small impact on encode
    speed. On most clips though the quality gain and speed impact are small.
    
    Change-Id: Id22967a840e996e1db273f6ac4ff03f4f52d49aa

--- vp9/encoder/vp9_encoder.h
+++ vp9/encoder/vp9_encoder.h
@@ -260,6 +260,8 @@ typedef struct RD_COUNTS {
   vp9_coeff_count coef_counts[TX_SIZES][PLANE_TYPES];
   int64_t comp_pred_diff[REFERENCE_MODES];
   int64_t filter_diff[SWITCHABLE_FILTER_CONTEXTS];
+  int m_search_count;
+  int ex_search_count;
 } RD_COUNTS;
 
 typedef struct ThreadData {
