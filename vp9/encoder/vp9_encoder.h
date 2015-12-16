02345be98674f5ae57dc9508f95aba312ad6b633
Debargha Mukherjee <debargha@google.com>
2015-12-16 15:34:07 -0800

    Adding an aq mode for 360 videos
    Different quality levels are used for different regions in
    the frame depending on how far they are vertically from the
    center. Specifically, three segments are used based on the
    mi_row index with respect number to the number of mi_rows in
    the frame.
    
    Change-Id: Ifc8b777bc58ea8521dffc4640360c67d99f8d381

--- vp9/encoder/vp9_encoder.h
+++ vp9/encoder/vp9_encoder.h
@@ -112,6 +112,7 @@ typedef enum {
   VARIANCE_AQ = 1,
   COMPLEXITY_AQ = 2,
   CYCLIC_REFRESH_AQ = 3,
+  EQUATOR360_AQ = 4,
   AQ_MODE_COUNT  // This should always be the last member of the enum
 } AQ_MODE;
 
