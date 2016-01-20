75b57d39d4f90a0fe108efeacf9da3a663622061
Yaowu Xu <yaowu@google.com>
2016-01-20 15:15:42 -0800

    VP9_ -> VPX_ in function names
    Change-Id: If02f1cfb2fc52d612434d12f8679d5707bbe2ecf

--- vp10/encoder/mips/msa/fdct8x8_msa.c
+++ vp10/encoder/mips/msa/fdct8x8_msa.c
@@ -31,7 +31,7 @@ void vp10_fht8x8_msa(const int16_t *input, int16_t *output, int32_t stride,
                 in0, in1, in2, in3, in4, in5, in6, in7);
       break;
     case ADST_DCT:
-      VP9_ADST8(in0, in1, in2, in3, in4, in5, in6, in7,
+      VPX_ADST8(in0, in1, in2, in3, in4, in5, in6, in7,
                 in0, in1, in2, in3, in4, in5, in6, in7);
       TRANSPOSE8x8_SH_SH(in0, in1, in2, in3, in4, in5, in6, in7,
                          in0, in1, in2, in3, in4, in5, in6, in7);
@@ -43,15 +43,15 @@ void vp10_fht8x8_msa(const int16_t *input, int16_t *output, int32_t stride,
                 in0, in1, in2, in3, in4, in5, in6, in7);
       TRANSPOSE8x8_SH_SH(in0, in1, in2, in3, in4, in5, in6, in7,
                          in0, in1, in2, in3, in4, in5, in6, in7);
-      VP9_ADST8(in0, in1, in2, in3, in4, in5, in6, in7,
+      VPX_ADST8(in0, in1, in2, in3, in4, in5, in6, in7,
                 in0, in1, in2, in3, in4, in5, in6, in7);
       break;
     case ADST_ADST:
-      VP9_ADST8(in0, in1, in2, in3, in4, in5, in6, in7,
+      VPX_ADST8(in0, in1, in2, in3, in4, in5, in6, in7,
                 in0, in1, in2, in3, in4, in5, in6, in7);
       TRANSPOSE8x8_SH_SH(in0, in1, in2, in3, in4, in5, in6, in7,
                          in0, in1, in2, in3, in4, in5, in6, in7);
-      VP9_ADST8(in0, in1, in2, in3, in4, in5, in6, in7,
+      VPX_ADST8(in0, in1, in2, in3, in4, in5, in6, in7,
                 in0, in1, in2, in3, in4, in5, in6, in7);
       break;
     default:
