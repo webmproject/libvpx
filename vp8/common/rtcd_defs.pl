ce11055d57687930c5ee5f6796d2d76787de5f5c
Johann <johannkoenig@google.com>
2016-05-06 17:20:15 -0700

    Remove sixtap/bilinear 4x4 neon implementations
    These implementations rely on casting the pointers to load the data.
    Clang implemented optimizations which automatically add alignment hints
    to such loads. The 4x4 filters do not guarantee the necessary alignment
    so the resulting assembly is broken.
    https://llvm.org/bugs/show_bug.cgi?id=24421
    
    BUG=webm:817
    BUG=webm:892
    
    Change-Id: I608885299f1f86ff83653b65e0e40d0ae87fb3fe

--- vp8/common/rtcd_defs.pl
+++ vp8/common/rtcd_defs.pl
@@ -205,7 +205,6 @@ $vp8_sixtap_predict8x4_media=vp8_sixtap_predict8x4_armv6;
 $vp8_sixtap_predict8x4_dspr2=vp8_sixtap_predict8x4_dspr2;
 
 add_proto qw/void vp8_sixtap_predict4x4/, "unsigned char *src, int src_pitch, int xofst, int yofst, unsigned char *dst, int dst_pitch";
-#TODO(johannkoenig): fix the neon version https://code.google.com/p/webm/issues/detail?id=817
 specialize qw/vp8_sixtap_predict4x4 mmx ssse3 media dspr2 msa/;
 $vp8_sixtap_predict4x4_media=vp8_sixtap_predict4x4_armv6;
 $vp8_sixtap_predict4x4_dspr2=vp8_sixtap_predict4x4_dspr2;
@@ -223,7 +222,6 @@ specialize qw/vp8_bilinear_predict8x4 mmx media neon msa/;
 $vp8_bilinear_predict8x4_media=vp8_bilinear_predict8x4_armv6;
 
 add_proto qw/void vp8_bilinear_predict4x4/, "unsigned char *src, int src_pitch, int xofst, int yofst, unsigned char *dst, int dst_pitch";
-#TODO(johannkoenig): fix the neon version https://code.google.com/p/webm/issues/detail?id=892
 specialize qw/vp8_bilinear_predict4x4 mmx media msa/;
 $vp8_bilinear_predict4x4_media=vp8_bilinear_predict4x4_armv6;
 
