d29062c4da7f6fe799d94056b93126a4620eec27
Geza Lore <gezalore@gmail.com>
2016-05-09 16:23:35 +0100

    Use multiple tiles in V10 tile independence tests.
    Change-Id: I6e5c1cbe1bf40d2f7a0d8bd821cac8ce626ce3b8

--- test/tile_independence_test.cc
+++ test/tile_independence_test.cc
@@ -97,8 +97,8 @@ TEST_P(TileIndependenceTest, MD5Match) {
   cfg_.g_lag_in_frames = 12;
   cfg_.rc_end_usage = VPX_VBR;
 
-  libvpx_test::I420VideoSource video("hantro_collage_w352h288.yuv", 704, 144,
-                                     timebase.den, timebase.num, 0, 15);
+  libvpx_test::I420VideoSource video("hantro_collage_w352h288.yuv", 704, 576,
+                                     timebase.den, timebase.num, 0, 5);
   ASSERT_NO_FATAL_FAILURE(RunLoop(&video));
 
   const char *md5_fw_str = md5_fw_order_.Get();
@@ -110,14 +110,14 @@ TEST_P(TileIndependenceTest, MD5Match) {
   ASSERT_STREQ(md5_fw_str, md5_inv_str);
 }
 
-VP9_INSTANTIATE_TEST_CASE(TileIndependenceTest, ::testing::Range(0, 2, 1),
+VP9_INSTANTIATE_TEST_CASE(TileIndependenceTest, ::testing::Values(0, 1),
                                                 ::testing::Values(0));
 
 #if CONFIG_EXT_TILE
 VP10_INSTANTIATE_TEST_CASE(TileIndependenceTest, ::testing::Values(1, 2, 32),
                                                  ::testing::Values(1, 2, 32));
 #else
-VP10_INSTANTIATE_TEST_CASE(TileIndependenceTest, ::testing::Range(0, 1, 1),
-                                                 ::testing::Range(0, 1, 1));
+VP10_INSTANTIATE_TEST_CASE(TileIndependenceTest, ::testing::Values(0, 1),
+                                                 ::testing::Values(0, 1));
 #endif  // CONFIG_EXT_TILE
 }  // namespace
