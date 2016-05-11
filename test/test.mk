b5331459c2d0761e2064ac96a86ce83be9b63730
Angie Chiang <angiebird@google.com>
2016-05-10 17:06:40 -0700

    Remove vp10_fwd_txfm2d_sse4_test.cc
    Functions vp10_fwd_txfm2d_#x#_sse4_1 tested in this file
    will be tested in vp10_fhts#x#_test.cc
    Remove this to avoid duplication
    
    Change-Id: Iaf21ab85b9a164fcf2a4574b3e13217e43b6255e

--- test/test.mk
+++ test/test.mk
@@ -174,8 +174,6 @@ LIBVPX_TEST_SRCS-$(CONFIG_ANS)          += vp10_ans_test.cc
 LIBVPX_TEST_SRCS-$(CONFIG_VP10_ENCODER) += sum_squares_test.cc
 LIBVPX_TEST_SRCS-$(CONFIG_VP10_ENCODER) += subtract_test.cc
 
-LIBVPX_TEST_SRCS-$(HAVE_SSE4_1) += vp10_fwd_txfm2d_sse4_test.cc
-
 ifeq ($(CONFIG_EXT_INTER),yes)
 LIBVPX_TEST_SRCS-$(HAVE_SSSE3) += masked_variance_test.cc
 LIBVPX_TEST_SRCS-$(HAVE_SSSE3) += masked_sad_test.cc
