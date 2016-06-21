d26815569fabd400535658dfa2b99214a5d892dd
Jingning Han <jingning@google.com>
2016-06-21 12:55:21 -0700

    Fix uninitialized context use case in supertx and var-tx
    This commit fixes the use of uninitialized context values in the
    combination of supertx and var-tx.
    
    Change-Id: I2d36badf5c9806ea402ce3e19515cc299e6b79e8

--- vp10/encoder/encodemb.c
+++ vp10/encoder/encodemb.c
@@ -1277,14 +1277,18 @@ void vp10_encode_sb_supertx(MACROBLOCK *x, BLOCK_SIZE bsize) {
 
   for (plane = 0; plane < MAX_MB_PLANE; ++plane) {
     const struct macroblockd_plane* const pd = &xd->plane[plane];
+#if CONFIG_VAR_TX
+    const TX_SIZE tx_size = TX_4X4;
+#else
     const TX_SIZE tx_size = plane ? get_uv_tx_size(mbmi, pd) : mbmi->tx_size;
+#endif
     vp10_subtract_plane(x, bsize, plane);
     vp10_get_entropy_contexts(bsize, tx_size, pd,
                               ctx.ta[plane], ctx.tl[plane]);
     arg.ta = ctx.ta[plane];
     arg.tl = ctx.tl[plane];
     vp10_foreach_transformed_block_in_plane(xd, bsize, plane, encode_block,
-                                           &arg);
+                                            &arg);
   }
 }
 #endif  // CONFIG_SUPERTX
