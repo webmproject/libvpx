240f8e56e4039e7c3911bf7bfceef3e349190b55
Johann <johannkoenig@google.com>
2016-06-28 16:40:03 -0700

    vp9: Fix potential SEGV in decoder_peek_si_internal
    decoder_peek_si_internal could potentially read more bytes than
    what actually exists in the input buffer. We check for the buffer
    size to be at least 8, but we try to read up to 10 bytes in the
    worst case. A well crafted file could thus cause a segfault.
    Likely change that introduced this bug was:
    https://chromium-review.googlesource.com/#/c/70439 (git hash:
    7c43fb6)
    
    BUG=chromium:621095
    
    (cherry picked from commit aa1c813c43c6b2e43036d5573f361924195d65b7)
    
    Change-Id: Id74880cfdded44caaa45bbdbaac859c09d3db752

--- vp9/vp9_dx_iface.c
+++ vp9/vp9_dx_iface.c
@@ -127,7 +127,7 @@ static vpx_codec_err_t decoder_peek_si_internal(const uint8_t *data,
                                                 vpx_decrypt_cb decrypt_cb,
                                                 void *decrypt_state) {
   int intra_only_flag = 0;
-  uint8_t clear_buffer[9];
+  uint8_t clear_buffer[10];
 
   if (data + data_sz <= data)
     return VPX_CODEC_INVALID_PARAM;
@@ -140,6 +140,11 @@ static vpx_codec_err_t decoder_peek_si_internal(const uint8_t *data,
     decrypt_cb(decrypt_state, data, clear_buffer, data_sz);
     data = clear_buffer;
   }
+
+  // A maximum of 6 bits are needed to read the frame marker, profile and
+  // show_existing_frame.
+  if (data_sz < 1)
+    return VPX_CODEC_UNSUP_BITSTREAM;
 
   {
     int show_frame;
@@ -154,15 +159,19 @@ static vpx_codec_err_t decoder_peek_si_internal(const uint8_t *data,
     if (profile >= MAX_PROFILES)
       return VPX_CODEC_UNSUP_BITSTREAM;
 
-    if ((profile >= 2 && data_sz <= 1) || data_sz < 1)
-      return VPX_CODEC_UNSUP_BITSTREAM;
-
     if (vpx_rb_read_bit(&rb)) {  // show an existing frame
+      // If profile is > 2 and show_existing_frame is true, then at least 1 more
+      // byte (6+3=9 bits) is needed.
+      if (profile > 2 && data_sz < 2)
+        return VPX_CODEC_UNSUP_BITSTREAM;
       vpx_rb_read_literal(&rb, 3);  // Frame buffer to show.
       return VPX_CODEC_OK;
     }
 
-    if (data_sz <= 8)
+    // For the rest of the function, a maximum of 9 more bytes are needed
+    // (computed by taking the maximum possible bits needed in each case). Note
+    // that this has to be updated if we read any more bits in this function.
+    if (data_sz < 10)
       return VPX_CODEC_UNSUP_BITSTREAM;
 
     si->is_kf = !vpx_rb_read_bit(&rb);
