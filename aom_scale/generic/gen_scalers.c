b42827f650c081ff0771b56092c6f60b4e50057f
Urvang Joshi <urvang@google.com>
2016-07-15 15:58:25 -0700

    Rewrite some loops to avoid -Wunsafe-loop-optimizations warnings.
    For example, loops of the form:
    "for (i = 0; i < 1 + max_value; ++i) ..." or
    "for (i = 0; i <= max_value; ++i) ..." are possibly infinite loops,
    theoretically speaking (even if practically, they aren't).
    So, compiler cannot optimize those loops.
    
    When possible, I rewrote such loops to be finite even theoretically.
    
    Cherry-picked from aomedia/master: 4e69284
    
    Change-Id: Ied47a24833b689c0ec011f8645cf1c01856f7c59

--- aom_scale/generic/gen_scalers.c
+++ aom_scale/generic/gen_scalers.c
@@ -39,27 +39,23 @@ void aom_horizontal_line_5_4_scale_c(const unsigned char *source,
                                      unsigned int source_width,
                                      unsigned char *dest,
                                      unsigned int dest_width) {
-  unsigned i;
-  unsigned int a, b, c, d, e;
-  unsigned char *des = dest;
-  const unsigned char *src = source;
-
+  const unsigned char *const source_end = source + source_width;
   (void)dest_width;
 
-  for (i = 0; i < source_width; i += 5) {
-    a = src[0];
-    b = src[1];
-    c = src[2];
-    d = src[3];
-    e = src[4];
+  while (source < source_end) {
+    const unsigned int a = source[0];
+    const unsigned int b = source[1];
+    const unsigned int c = source[2];
+    const unsigned int d = source[3];
+    const unsigned int e = source[4];
 
-    des[0] = (unsigned char)a;
-    des[1] = (unsigned char)((b * 192 + c * 64 + 128) >> 8);
-    des[2] = (unsigned char)((c * 128 + d * 128 + 128) >> 8);
-    des[3] = (unsigned char)((d * 64 + e * 192 + 128) >> 8);
+    dest[0] = (unsigned char)a;
+    dest[1] = (unsigned char)((b * 192 + c * 64 + 128) >> 8);
+    dest[2] = (unsigned char)((c * 128 + d * 128 + 128) >> 8);
+    dest[3] = (unsigned char)((d * 64 + e * 192 + 128) >> 8);
 
-    src += 5;
-    des += 4;
+    source += 5;
+    dest += 4;
   }
 }
 
@@ -67,25 +63,21 @@ void aom_vertical_band_5_4_scale_c(unsigned char *source,
                                    unsigned int src_pitch, unsigned char *dest,
                                    unsigned int dest_pitch,
                                    unsigned int dest_width) {
-  unsigned int i;
-  unsigned int a, b, c, d, e;
-  unsigned char *des = dest;
-  unsigned char *src = source;
+  const unsigned char *const dest_end = dest + dest_width;
+  while (dest < dest_end) {
+    const unsigned int a = source[0 * src_pitch];
+    const unsigned int b = source[1 * src_pitch];
+    const unsigned int c = source[2 * src_pitch];
+    const unsigned int d = source[3 * src_pitch];
+    const unsigned int e = source[4 * src_pitch];
 
-  for (i = 0; i < dest_width; i++) {
-    a = src[0 * src_pitch];
-    b = src[1 * src_pitch];
-    c = src[2 * src_pitch];
-    d = src[3 * src_pitch];
-    e = src[4 * src_pitch];
+    dest[0 * dest_pitch] = (unsigned char)a;
+    dest[1 * dest_pitch] = (unsigned char)((b * 192 + c * 64 + 128) >> 8);
+    dest[2 * dest_pitch] = (unsigned char)((c * 128 + d * 128 + 128) >> 8);
+    dest[3 * dest_pitch] = (unsigned char)((d * 64 + e * 192 + 128) >> 8);
 
-    des[0 * dest_pitch] = (unsigned char)a;
-    des[1 * dest_pitch] = (unsigned char)((b * 192 + c * 64 + 128) >> 8);
-    des[2 * dest_pitch] = (unsigned char)((c * 128 + d * 128 + 128) >> 8);
-    des[3 * dest_pitch] = (unsigned char)((d * 64 + e * 192 + 128) >> 8);
-
-    src++;
-    des++;
+    ++source;
+    ++dest;
   }
 }
 
@@ -114,26 +106,21 @@ void aom_horizontal_line_5_3_scale_c(const unsigned char *source,
                                      unsigned int source_width,
                                      unsigned char *dest,
                                      unsigned int dest_width) {
-  unsigned int i;
-  unsigned int a, b, c, d, e;
-  unsigned char *des = dest;
-  const unsigned char *src = source;
-
+  const unsigned char *const source_end = source + source_width;
   (void)dest_width;
+  while (source < source_end) {
+    const unsigned int a = source[0];
+    const unsigned int b = source[1];
+    const unsigned int c = source[2];
+    const unsigned int d = source[3];
+    const unsigned int e = source[4];
 
-  for (i = 0; i < source_width; i += 5) {
-    a = src[0];
-    b = src[1];
-    c = src[2];
-    d = src[3];
-    e = src[4];
+    dest[0] = (unsigned char)a;
+    dest[1] = (unsigned char)((b * 85 + c * 171 + 128) >> 8);
+    dest[2] = (unsigned char)((d * 171 + e * 85 + 128) >> 8);
 
-    des[0] = (unsigned char)a;
-    des[1] = (unsigned char)((b * 85 + c * 171 + 128) >> 8);
-    des[2] = (unsigned char)((d * 171 + e * 85 + 128) >> 8);
-
-    src += 5;
-    des += 3;
+    source += 5;
+    dest += 3;
   }
 }
 
@@ -141,24 +128,20 @@ void aom_vertical_band_5_3_scale_c(unsigned char *source,
                                    unsigned int src_pitch, unsigned char *dest,
                                    unsigned int dest_pitch,
                                    unsigned int dest_width) {
-  unsigned int i;
-  unsigned int a, b, c, d, e;
-  unsigned char *des = dest;
-  unsigned char *src = source;
+  const unsigned char *const dest_end = dest + dest_width;
+  while (dest < dest_end) {
+    const unsigned int a = source[0 * src_pitch];
+    const unsigned int b = source[1 * src_pitch];
+    const unsigned int c = source[2 * src_pitch];
+    const unsigned int d = source[3 * src_pitch];
+    const unsigned int e = source[4 * src_pitch];
 
-  for (i = 0; i < dest_width; i++) {
-    a = src[0 * src_pitch];
-    b = src[1 * src_pitch];
-    c = src[2 * src_pitch];
-    d = src[3 * src_pitch];
-    e = src[4 * src_pitch];
+    dest[0 * dest_pitch] = (unsigned char)a;
+    dest[1 * dest_pitch] = (unsigned char)((b * 85 + c * 171 + 128) >> 8);
+    dest[2 * dest_pitch] = (unsigned char)((d * 171 + e * 85 + 128) >> 8);
 
-    des[0 * dest_pitch] = (unsigned char)a;
-    des[1 * dest_pitch] = (unsigned char)((b * 85 + c * 171 + 128) >> 8);
-    des[2 * dest_pitch] = (unsigned char)((d * 171 + e * 85 + 128) >> 8);
-
-    src++;
-    des++;
+    ++source;
+    ++dest;
   }
 }
 
@@ -186,18 +169,12 @@ void aom_horizontal_line_2_1_scale_c(const unsigned char *source,
                                      unsigned int source_width,
                                      unsigned char *dest,
                                      unsigned int dest_width) {
-  unsigned int i;
-  unsigned int a;
-  unsigned char *des = dest;
-  const unsigned char *src = source;
-
+  const unsigned char *const source_end = source + source_width;
   (void)dest_width;
-
-  for (i = 0; i < source_width; i += 2) {
-    a = src[0];
-    des[0] = (unsigned char)(a);
-    src += 2;
-    des += 1;
+  while (source < source_end) {
+    dest[0] = source[0];
+    source += 2;
+    ++dest;
   }
 }
 
@@ -215,18 +192,14 @@ void aom_vertical_band_2_1_scale_i_c(unsigned char *source,
                                      unsigned char *dest,
                                      unsigned int dest_pitch,
                                      unsigned int dest_width) {
-  int i;
-  int temp;
-  int width = dest_width;
-
+  const unsigned char *const dest_end = dest + dest_width;
   (void)dest_pitch;
-
-  for (i = 0; i < width; i++) {
-    temp = 8;
-    temp += source[i - (int)src_pitch] * 3;
-    temp += source[i] * 10;
-    temp += source[i + src_pitch] * 3;
-    temp >>= 4;
-    dest[i] = (unsigned char)(temp);
+  while (dest < dest_end) {
+    const unsigned int a = source[-src_pitch] * 3;
+    const unsigned int b = source[0] * 10;
+    const unsigned int c = source[src_pitch] * 3;
+    dest[0] = (unsigned char)((8 + a + b + c) >> 4);
+    ++source;
+    ++dest;
   }
 }
