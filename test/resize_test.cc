325bdddc38ad15cf7dd2bc618461a13cfb006812
James Zern <jzern@google.com>
2016-07-22 13:11:07 -0700

    resize_test: fix implicit float->int conversion
    Change-Id: I1efc16fa158740a06da719a1ea90c6dd6a182bb4

--- test/resize_test.cc
+++ test/resize_test.cc
@@ -369,7 +369,7 @@ class ResizeInternalTest : public ResizeTest {
   }
 
   virtual void PSNRPktHook(const vpx_codec_cx_pkt_t *pkt) {
-    if (!frame0_psnr_)
+    if (frame0_psnr_ == 0.)
       frame0_psnr_ = pkt->data.psnr.psnr[0];
     EXPECT_NEAR(pkt->data.psnr.psnr[0], frame0_psnr_, 2.0);
   }
@@ -671,7 +671,7 @@ class ResizeCspTest : public ResizeTest {
   }
 
   virtual void PSNRPktHook(const vpx_codec_cx_pkt_t *pkt) {
-    if (!frame0_psnr_)
+    if (frame0_psnr_ == 0.)
       frame0_psnr_ = pkt->data.psnr.psnr[0];
     EXPECT_NEAR(pkt->data.psnr.psnr[0], frame0_psnr_, 2.0);
   }
