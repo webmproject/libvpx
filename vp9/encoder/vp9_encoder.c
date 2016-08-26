37e83789f18c9be4ff7f295b979303c0a3a41e81
Sarah Parker <sarahparker@google.com>
2016-08-25 17:25:26 -0700

    Fix formatting in internal stats for vp8 and vp9
    This corrects a formatting error introduced in:
    I1e9d548ce445d29002f0c59ebfd3957a6f15e702
    where spaces were used as delimiters instead of tabs.
    
    The corresponding fix for vp10 is in
    Ica3d625d6672b3c47e0e208b45eede29b9004030.
    
    Change-Id: Ibc4eb8fd82e6b926ba259a679dc98557cadba9b1

--- vp9/encoder/vp9_encoder.c
+++ vp9/encoder/vp9_encoder.c
@@ -1984,9 +1984,11 @@ void vp9_remove_compressor(VP9_COMP *cpi) {
           SNPRINT2(results, "\t%7.3f", consistency);
           SNPRINT2(results, "\t%7.3f", cpi->worst_consistency);
         }
-        fprintf(f, "%s\t    Time  Rc-Err Abs Err\n", headings);
-        fprintf(f, "%s\t%8.0f %7.2f %7.2f\n", results, total_encode_time,
-                rate_err, fabs(rate_err));
+
+        SNPRINT(headings, "\tTime\tRcErr\tAbsErr");
+        SNPRINT2(results, "\t%7.3f", total_encode_time);
+        SNPRINT2(results, "\t%7.3f", rate_err);
+        SNPRINT2(results, "\t%7.3f", fabs(rate_err));
       }
 
       fclose(f);
