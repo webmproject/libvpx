a8eee97b43b532c9ee35280bfdf910ab468004a4
Linfeng Zhang <linfengz@google.com>
2016-10-06 17:01:30 -0700

    Check in vpx_lpf_vertical_4_dual_neon() assembly
    This replaces its C version.
    
    Change-Id: Ie39e9324305fdc0fff610ced608a037e44a85a1a

--- vpx_dsp/vpx_dsp.mk
+++ vpx_dsp/vpx_dsp.mk
@@ -141,14 +141,11 @@ DSP_SRCS-$(ARCH_X86)$(ARCH_X86_64)   += x86/loopfilter_sse2.c
 DSP_SRCS-$(HAVE_AVX2)                += x86/loopfilter_avx2.c
 
 ifeq ($(HAVE_NEON_ASM),yes)
-DSP_SRCS-yes  += arm/loopfilter_vertical_4_dual_neon.c
 DSP_SRCS-yes  += arm/loopfilter_16_neon$(ASM)
 DSP_SRCS-yes  += arm/loopfilter_8_neon$(ASM)
 DSP_SRCS-yes  += arm/loopfilter_4_neon$(ASM)
 else
-ifeq ($(HAVE_NEON),yes)
-DSP_SRCS-yes   += arm/loopfilter_neon.c
-endif  # HAVE_NEON
+DSP_SRCS-$(HAVE_NEON)   += arm/loopfilter_neon.c
 endif  # HAVE_NEON_ASM
 
 DSP_SRCS-$(HAVE_MSA)    += mips/loopfilter_msa.h
