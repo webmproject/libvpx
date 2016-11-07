856c55e93d54f549ecedfffb1ba0302506aad7f2
Yaowu Xu <yaowu@google.com>
2016-11-07 14:50:35 -0800

    Add transform parameter initialization
    The initialization of transform parameters was missing, that led to a
    crash in encoder.
    
    Change-Id: I9e35830d5f24e771c845f0d8881671d6b7228c5e

--- av1/encoder/encodemb.c
+++ av1/encoder/encodemb.c
@@ -1285,7 +1285,10 @@ void av1_encode_block_intra(int plane, int block, int blk_row, int blk_col,
     }
 
   fwd_txfm_param.rd_transform = 0;
-
+  fwd_txfm_param.tx_type = tx_type;
+  fwd_txfm_param.tx_size = tx_size;
+  fwd_txfm_param.fwd_txfm_opt = FWD_TXFM_OPT_NORMAL;
+  fwd_txfm_param.lossless = xd->lossless[mbmi->segment_id];
   fwd_txfm(src_int16, coeff, diff_stride, &fwd_txfm_param);
   fwd_txfm(pred, ref_coeff, diff_stride, &fwd_txfm_param);
 
