911bb980b1d2501ee9fa053ff3f6faffbba0b0a2
Linfeng Zhang <linfengz@google.com>
2016-12-28 13:51:44 -0800

    Clean DC only idct NEON intrinsics
    BUG=webm:1301
    
    Change-Id: Iffc83854218460b3f687f3774e71d45b552382a5

--- vpx_dsp/arm/idct4x4_1_add_neon.c
+++ vpx_dsp/arm/idct4x4_1_add_neon.c
@@ -14,28 +14,32 @@
 #include "./vpx_dsp_rtcd.h"
 #include "vpx_dsp/inv_txfm.h"
 
+static INLINE void idct4x4_1_add_kernel(uint8_t **dest, const int stride,
+                                        const int16x8_t res,
+                                        uint32x2_t *const d) {
+  uint16x8_t a;
+  uint8x8_t b;
+  *d = vld1_lane_u32((const uint32_t *)*dest, *d, 0);
+  *d = vld1_lane_u32((const uint32_t *)(*dest + stride), *d, 1);
+  a = vaddw_u8(vreinterpretq_u16_s16(res), vreinterpret_u8_u32(*d));
+  b = vqmovun_s16(vreinterpretq_s16_u16(a));
+  vst1_lane_u32((uint32_t *)*dest, vreinterpret_u32_u8(b), 0);
+  *dest += stride;
+  vst1_lane_u32((uint32_t *)*dest, vreinterpret_u32_u8(b), 1);
+  *dest += stride;
+}
+
 void vpx_idct4x4_1_add_neon(const tran_low_t *input, uint8_t *dest,
                             int stride) {
-  int i;
-  const int16_t out0 = dct_const_round_shift((int16_t)input[0] * cospi_16_64);
-  const int16_t out1 = dct_const_round_shift(out0 * cospi_16_64);
+  const int16_t out0 = WRAPLOW(dct_const_round_shift(input[0] * cospi_16_64));
+  const int16_t out1 = WRAPLOW(dct_const_round_shift(out0 * cospi_16_64));
   const int16_t a1 = ROUND_POWER_OF_TWO(out1, 4);
   const int16x8_t dc = vdupq_n_s16(a1);
   uint32x2_t d = vdup_n_u32(0);
-  uint16x8_t a;
-  uint8x8_t b;
 
   assert(!((intptr_t)dest % sizeof(uint32_t)));
   assert(!(stride % sizeof(uint32_t)));
 
-  for (i = 0; i < 2; i++) {
-    d = vld1_lane_u32((const uint32_t *)dest, d, 0);
-    d = vld1_lane_u32((const uint32_t *)(dest + stride), d, 1);
-    a = vaddw_u8(vreinterpretq_u16_s16(dc), vreinterpret_u8_u32(d));
-    b = vqmovun_s16(vreinterpretq_s16_u16(a));
-    vst1_lane_u32((uint32_t *)dest, vreinterpret_u32_u8(b), 0);
-    dest += stride;
-    vst1_lane_u32((uint32_t *)dest, vreinterpret_u32_u8(b), 1);
-    dest += stride;
-  }
+  idct4x4_1_add_kernel(&dest, stride, dc, &d);
+  idct4x4_1_add_kernel(&dest, stride, dc, &d);
 }
--- vpx_dsp/arm/idct8x8_1_add_neon.c
+++ vpx_dsp/arm/idct8x8_1_add_neon.c
@@ -12,47 +12,53 @@
 
 #include "./vpx_dsp_rtcd.h"
 #include "vpx_dsp/inv_txfm.h"
-#include "vpx_ports/mem.h"
+
+static INLINE uint8x8_t create_dcd(const int16_t dc) {
+  int16x8_t t = vdupq_n_s16(dc);
+  return vqmovun_s16(t);
+}
+
+static INLINE void idct8x8_1_add_pos_kernel(uint8_t **dest, const int stride,
+                                            const uint8x8_t res) {
+  const uint8x8_t a = vld1_u8(*dest);
+  const uint8x8_t b = vqadd_u8(a, res);
+  vst1_u8(*dest, b);
+  *dest += stride;
+}
+
+static INLINE void idct8x8_1_add_neg_kernel(uint8_t **dest, const int stride,
+                                            const uint8x8_t res) {
+  const uint8x8_t a = vld1_u8(*dest);
+  const uint8x8_t b = vqsub_u8(a, res);
+  vst1_u8(*dest, b);
+  *dest += stride;
+}
 
 void vpx_idct8x8_1_add_neon(const tran_low_t *input, uint8_t *dest,
                             int stride) {
-  int i;
-  const int16_t out0 = dct_const_round_shift(input[0] * cospi_16_64);
-  const int16_t out1 = dct_const_round_shift(out0 * cospi_16_64);
-  const int16_t out2 = ROUND_POWER_OF_TWO(out1, 5);
-  const int16x8_t dc = vdupq_n_s16(out2);
-  const uint16x8_t dc_u16 = vreinterpretq_u16_s16(dc);
-  const uint8_t *dst = dest;
-  uint8x8_t d0, d1, d2, d3;
-  uint16x8_t d0_u16, d1_u16, d2_u16, d3_u16;
+  const int16_t out0 = WRAPLOW(dct_const_round_shift(input[0] * cospi_16_64));
+  const int16_t out1 = WRAPLOW(dct_const_round_shift(out0 * cospi_16_64));
+  const int16_t a1 = ROUND_POWER_OF_TWO(out1, 5);
 
-  for (i = 0; i < 2; i++) {
-    d0 = vld1_u8(dst);
-    dst += stride;
-    d1 = vld1_u8(dst);
-    dst += stride;
-    d2 = vld1_u8(dst);
-    dst += stride;
-    d3 = vld1_u8(dst);
-    dst += stride;
-
-    d0_u16 = vaddw_u8(dc_u16, d0);
-    d1_u16 = vaddw_u8(dc_u16, d1);
-    d2_u16 = vaddw_u8(dc_u16, d2);
-    d3_u16 = vaddw_u8(dc_u16, d3);
-
-    d0 = vqmovun_s16(vreinterpretq_s16_u16(d0_u16));
-    d1 = vqmovun_s16(vreinterpretq_s16_u16(d1_u16));
-    d2 = vqmovun_s16(vreinterpretq_s16_u16(d2_u16));
-    d3 = vqmovun_s16(vreinterpretq_s16_u16(d3_u16));
-
-    vst1_u8(dest, d0);
-    dest += stride;
-    vst1_u8(dest, d1);
-    dest += stride;
-    vst1_u8(dest, d2);
-    dest += stride;
-    vst1_u8(dest, d3);
-    dest += stride;
+  if (a1 >= 0) {
+    const uint8x8_t dc = create_dcd(a1);
+    idct8x8_1_add_pos_kernel(&dest, stride, dc);
+    idct8x8_1_add_pos_kernel(&dest, stride, dc);
+    idct8x8_1_add_pos_kernel(&dest, stride, dc);
+    idct8x8_1_add_pos_kernel(&dest, stride, dc);
+    idct8x8_1_add_pos_kernel(&dest, stride, dc);
+    idct8x8_1_add_pos_kernel(&dest, stride, dc);
+    idct8x8_1_add_pos_kernel(&dest, stride, dc);
+    idct8x8_1_add_pos_kernel(&dest, stride, dc);
+  } else {
+    const uint8x8_t dc = create_dcd(-a1);
+    idct8x8_1_add_neg_kernel(&dest, stride, dc);
+    idct8x8_1_add_neg_kernel(&dest, stride, dc);
+    idct8x8_1_add_neg_kernel(&dest, stride, dc);
+    idct8x8_1_add_neg_kernel(&dest, stride, dc);
+    idct8x8_1_add_neg_kernel(&dest, stride, dc);
+    idct8x8_1_add_neg_kernel(&dest, stride, dc);
+    idct8x8_1_add_neg_kernel(&dest, stride, dc);
+    idct8x8_1_add_neg_kernel(&dest, stride, dc);
   }
 }
