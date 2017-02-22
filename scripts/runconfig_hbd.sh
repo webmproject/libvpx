29812d14600013acc8a2d22f4f0455c14d9b2cfc
Yi Luo <luoyi@google.com>
2017-02-22 10:22:37 -0800

    Add configure scripts
    Change-Id: If732b427e04e32f873d547606c0bcdd39ae2f1f3

---
+++ scripts/runconfig_hbd.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+platform=x86_64-linux-gcc
+codec=--enable-av1
+libsrc=aom
+
+bitdepth=--enable-aom-highbitdepth
+#bitdepth=
+
+if [ "$#" -eq 1 ]; then
+  tool=$1
+  ../$libsrc/configure --target=$platform $codec $bitdepth --enable-experimental --enable-$tool --disable-unit-tests --disable-daala-ec > /dev/null
+else
+  tool_1=$1
+  tool_2=$2
+  ../$libsrc/configure --target=$platform $codec $bitdepth --enable-experimental --enable-$tool_1 --enable-$tool_2 --disable-unit-tests --disable-daala-ec > /dev/null
+fi
