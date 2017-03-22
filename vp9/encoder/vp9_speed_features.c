4d50991320d51c0560249ea9e607d15f63058d33
Marco <marpan@google.com>
2017-03-22 12:15:06 -0700

    vp9: Adjust some speed settings for speed 8.
    Allow for simple_block_rd for VGA resoln, and reduce
    adaptive_rd_thresh to 1.
    
    On average no loss on RTC set, ~4% speedup on mac.
    
    Change-Id: Ib549c4061c853776062b5e34040f839d470fbebc

--- vp9/encoder/vp9_speed_features.c
+++ vp9/encoder/vp9_speed_features.c
@@ -573,10 +573,10 @@ static void set_rt_speed_feature_framesize_independent(
       }
       // Since the short_circuit_low_temp_var is used, reduce the
       // adaptive_rd_thresh level.
-      sf->adaptive_rd_thresh = 2;
+      sf->adaptive_rd_thresh = 1;
     }
     sf->limit_newmv_early_exit = 0;
-    if (cm->width > 640 && cm->height > 480) sf->use_simple_block_yrd = 1;
+    if (cm->width > 320 && cm->height > 240) sf->use_simple_block_yrd = 1;
   }
   // Turn off adaptive_rd_thresh if row_mt is on for all the non-rd paths. This
   // causes too many locks in realtime mode in certain platforms (Android ARM,
