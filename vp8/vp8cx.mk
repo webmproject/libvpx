1d86383512f8cf1551fc81048164885f1ef6f089
James Zern <jzern@google.com>
2017-06-16 01:33:57 -0700

    vp8: remove vp8_fast_quantize_b_mmx
    and vp8_fast_quantize_b_impl_mmx; this was never enabled in rtcd
    an sse2 version exists so there isn't much reason to keep a mmx
    implementation around.
    
    Change-Id: I8b3ee7f46ba194ffa0d0a6225a0f299f2a4dea90

--- vp8/vp8cx.mk
+++ vp8/vp8cx.mk
@@ -78,7 +78,6 @@ VP8_CX_SRCS_REMOVE-yes += encoder/firstpass.c
 VP8_CX_SRCS_REMOVE-yes += encoder/temporal_filter.c
 endif
 
-VP8_CX_SRCS-$(HAVE_MMX) += encoder/x86/vp8_enc_stubs_mmx.c
 VP8_CX_SRCS-$(HAVE_SSE2) += encoder/x86/dct_sse2.asm
 VP8_CX_SRCS-$(HAVE_SSE2) += encoder/x86/fwalsh_sse2.asm
 VP8_CX_SRCS-$(HAVE_SSE2) += encoder/x86/vp8_quantize_sse2.c
@@ -91,7 +90,6 @@ endif
 
 VP8_CX_SRCS-$(HAVE_SSE2) += encoder/x86/temporal_filter_apply_sse2.asm
 VP8_CX_SRCS-$(HAVE_SSE2) += encoder/x86/vp8_enc_stubs_sse2.c
-VP8_CX_SRCS-$(ARCH_X86)$(ARCH_X86_64) += encoder/x86/quantize_mmx.asm
 VP8_CX_SRCS-$(ARCH_X86)$(ARCH_X86_64) += encoder/x86/encodeopt.asm
 
 ifeq ($(CONFIG_REALTIME_ONLY),yes)
