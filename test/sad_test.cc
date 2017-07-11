e1bde306c840585d1afb007a4c962256851c2061
Johann <johannkoenig@google.com>
2017-07-11 07:39:28 -0700

    sad4d neon: 32x[16,32,64]
    Rewrite 32x32. Use half the accumulator registers.
    
    BUG=webm:1425
    
    Change-Id: Ibf5e61dc4ba15056102aef8495f4a02c668c5d13

--- test/sad_test.cc
+++ test/sad_test.cc
@@ -676,7 +676,9 @@ INSTANTIATE_TEST_CASE_P(NEON, SADavgTest, ::testing::ValuesIn(avg_neon_tests));
 
 const SadMxNx4Param x4d_neon_tests[] = {
   SadMxNx4Param(64, 64, &vpx_sad64x64x4d_neon),
+  SadMxNx4Param(32, 64, &vpx_sad32x64x4d_neon),
   SadMxNx4Param(32, 32, &vpx_sad32x32x4d_neon),
+  SadMxNx4Param(32, 16, &vpx_sad32x16x4d_neon),
   SadMxNx4Param(16, 32, &vpx_sad16x32x4d_neon),
   SadMxNx4Param(16, 16, &vpx_sad16x16x4d_neon),
   SadMxNx4Param(16, 8, &vpx_sad16x8x4d_neon),
--- vpx_dsp/vpx_dsp_rtcd_defs.pl
+++ vpx_dsp/vpx_dsp_rtcd_defs.pl
@@ -863,13 +863,13 @@ add_proto qw/void vpx_sad64x32x4d/, "const uint8_t *src_ptr, int src_stride, con
 specialize qw/vpx_sad64x32x4d msa sse2 vsx/;
 
 add_proto qw/void vpx_sad32x64x4d/, "const uint8_t *src_ptr, int src_stride, const uint8_t * const ref_ptr[], int ref_stride, uint32_t *sad_array";
-specialize qw/vpx_sad32x64x4d msa sse2 vsx/;
+specialize qw/vpx_sad32x64x4d neon msa sse2 vsx/;
 
 add_proto qw/void vpx_sad32x32x4d/, "const uint8_t *src_ptr, int src_stride, const uint8_t * const ref_ptr[], int ref_stride, uint32_t *sad_array";
 specialize qw/vpx_sad32x32x4d avx2 neon msa sse2 vsx/;
 
 add_proto qw/void vpx_sad32x16x4d/, "const uint8_t *src_ptr, int src_stride, const uint8_t * const ref_ptr[], int ref_stride, uint32_t *sad_array";
-specialize qw/vpx_sad32x16x4d msa sse2 vsx/;
+specialize qw/vpx_sad32x16x4d neon msa sse2 vsx/;
 
 add_proto qw/void vpx_sad16x32x4d/, "const uint8_t *src_ptr, int src_stride, const uint8_t * const ref_ptr[], int ref_stride, uint32_t *sad_array";
 specialize qw/vpx_sad16x32x4d neon msa sse2 vsx/;
