339a8b9df98ece23765ad8b0916665a334b0cdf5
Yi Luo <luoyi@google.com>
2017-07-21 13:47:56 -0700

    Add disabled section for runconfig.sh
    Change-Id: I0f08d5d88925322c13f674735d6ec5be49136edd

--- scripts/runconfig.sh
+++ scripts/runconfig.sh
@@ -79,6 +79,9 @@ experiment_dependency() {
 
 common="--disable-unit-tests --disable-docs --enable-experimental"
 
+disabled="--disable-chroma_sub8x8 --disable-filter_7bit --disable-reference_buffer
+ --disable-delta_q --disable-tile_groups --disable-rect_tx --disable-global_motion --disable-ext_tx --disable-cdef --disable-ext_intra --disable-mv_compress --disable-ext_refs --disable-dual_filter --disable-motion_var --disable-warped_motion --disable-ext_delta_q --disable-loopfiltering_across_tiles --disable-ec_smallmul --disable-var_tx --disable-ext_inter --disable-wedge --disable-compound_segment --disable-interintra --disable-one_sided_compound --disable-smooth_hv --disable-parallel_deblocking --disable-palette --disable-alt_intra --disable-palette_throughput --disable-tempmv_signaling"
+
 experiment_dependency $1
 
-../$libsrc/configure $common $tool > /dev/null
+../$libsrc/configure $common $disabled $tool > /dev/null
