441ad49ddc2d8a2f7c24b64a03dfe59edaf6e1e9
Yi Luo <luoyi@google.com>
2017-07-24 11:11:57 -0700

    Use --cc to cc email
    Change-Id: I47e28f33353a623f797b05f5d02d539f407b9128

--- scripts/nightly_run.sh
+++ scripts/nightly_run.sh
@@ -8,9 +8,9 @@ log_file=rep_$date_str.txt
 
 $script_path/nightly_speed.sh $codebase > $log_path/$log_file 2>&1
 
-#users="luoyi,yunqingwang"
 users=luoyi
 host_name=`hostname`
 sender=luoyi
+cc_list=yunqingwang
 
-sendgmr --to=$users --subject="AV1 Build/Running Report" --from=$sender --reply_to=$sender < $log_path/$log_file
+sendgmr --to=$users --cc=$cc_list --subject="AV1 Build/Running Report" --from=$sender --reply_to=$sender < $log_path/$log_file
