cfe2e94154b05998ffdaca7cd99bea5f2eb16850
Yi Luo <luoyi@google.com>
2017-07-24 15:06:06 -0700

    Add video_sequence.sh which is shared by av1 and vp9 tests
    - Minor format change.
    
    Change-Id: Ic851351d27c3cff9a37c0517c738b3b2a93e9ce6

--- scripts/nightly_speed.sh
+++ scripts/nightly_speed.sh
@@ -22,19 +22,7 @@ build_dir=$root_dir/release
 test_dir=~/Dev/nightly
 script_dir=~/Dev/sandbox/libvpx/scripts
 
-video=~/Dev/samples/videos/yaowu/soccer_cif.y4m
-wi=352
-he=288
-frames=5
-bitrate=500
-fps="30/1"
-
-# video=~/Dev/samples/videos/speed-set/BasketballDrive_1920x1080_50.y4m
-# wi=1920
-# he=1080
-# frames=150
-# bitrate=4000
-# fps="50/1"
+. video_sequence.sh
 
 # General options
 codec="--codec=av1"
--- scripts/vp9_nightly_speed.sh
+++ scripts/vp9_nightly_speed.sh
@@ -22,19 +22,7 @@ build_dir=$root_dir/release
 test_dir=~/Dev/nightly
 script_dir=~/Dev/sandbox/libvpx/scripts
 
-video=~/Dev/samples/videos/yaowu/soccer_cif.y4m
-wi=352
-he=288
-frames=5
-bitrate=500
-fps="30/1"
-
-# video=~/Dev/samples/videos/speed-set/BasketballDrive_1920x1080_50.y4m
-# wi=1920
-# he=1080
-# frames=150
-# bitrate=4000
-# fps="50/1"
+. video_sequence.sh
 
 # General options
 codec="--codec=vp9"
@@ -107,5 +95,5 @@ dfps=`awk '{print $9}' < $dlog`
 dfps=`echo $dfps | sed 's/(//'`
 
 echo -e '\t'"Enc fps   Dec fps    PSNR"'\t\t\t\t\t\t\t'"Enc status   Dec status"
-echo -e '\t'$efps"        "$dfps"    "$psnr'\t'$eflag"            "$dflag
+echo -e '\t'$efps"        "$dfps"   "$psnr'\t'$eflag"            "$dflag
 
