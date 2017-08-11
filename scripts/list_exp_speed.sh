efebcc1787675f4c16fb039f52bb4391edc6edb8
Yi Luo <luoyi@google.com>
2017-08-11 16:06:15 -0700

    Increase weekly test frame=150 Fix a disabled list space/return Clean-up $test_dir/*.txt
    Change-Id: I7511c00607d6a6518a1620c2d1da3ea6516263a1

--- scripts/list_exp_speed.sh
+++ scripts/list_exp_speed.sh
@@ -41,9 +41,11 @@ verbose=
 #   3) Change runconfig.sh, bitdepth=--enable-highbitdepth
 
 profile=0
+
+cd $test_dir
 rm *.txt
 
-for exp_tool in experimental chroma_sub8x8 filter_7bit reference_buffer delta_q rect_tx global_motion ext_tx cdef ext_intra mv_compress ext_refs dual_filter motion_var warped_motion ext_delta_q loopfiltering_across_tiles ec_smallmul var_tx ext_inter wedge compound_segment interintra one_sided_compound smooth_hv parallel_deblocking rect_intra_pred convolve_round ext_comp_refs altref2
+for exp_tool in experimental chroma_sub8x8 filter_7bit reference_buffer delta_q rect_tx global_motion ext_tx cdef ext_intra mv_compress ext_refs dual_filter motion_var warped_motion ext_delta_q loopfiltering_across_tiles ec_smallmul var_tx ext_inter wedge compound_segment interintra one_sided_compound smooth_hv parallel_deblocking rect_intra_pred convolve_round altref2
 
 do
   cd $build_dir
--- scripts/video_sequence_weekly.sh
+++ scripts/video_sequence_weekly.sh
@@ -17,6 +17,6 @@
 video=~/Dev/samples/videos/speed-set/BasketballDrive_1920x1080_50.y4m
 wi=1920
 he=1080
-frames=75
+frames=150
 bitrate=4000
 fps="50/1"
