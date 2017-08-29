57564c008df5ce535812bcb022fb326a36130462
Yi Luo <luoyi@google.com>
2017-08-29 08:20:22 -0700

    List disabled flags in shorter symbol
    Change-Id: Ie2ceb219d51c9b44402ce72efc0a5d894f8a9926

--- scripts/nightly_config.sh
+++ scripts/nightly_config.sh
@@ -8,9 +8,17 @@ test_dir=~/Dev/nightly
 
 common="--disable-unit-tests --disable-docs --enable-experimental"
 
-disabled="--disable-chroma_sub8x8 --disable-filter_7bit --disable-reference_buffer --disable-delta_q --disable-rect_tx --disable-global_motion --disable-ext_tx --disable-cdef --disable-ext_intra --disable-mv_compress --disable-ext_refs --disable-dual_filter --disable-motion_var --disable-warped_motion --disable-ext_delta_q --disable-loopfiltering_across_tiles --disable-ec_smallmul --disable-var_tx --disable-ext_inter --disable-wedge --disable-compound_segment --disable-interintra --disable-one_sided_compound --disable-smooth_hv --disable-parallel_deblocking --disable-rect_intra_pred --disable-convolve_round --disable-palette_throughput --disable-tempmv_signaling"
+d1="--disable-chroma_sub8x8 --disable-filter_7bit --disable-reference_buffer"
+d2="--disable-delta_q --disable-rect_tx --disable-global_motion --disable-ext_tx"
+d3="--disable-cdef --disable-ext_intra --disable-mv_compress --disable-ext_refs"
+d4="--disable-dual_filter --disable-motion_var --disable-warped_motion"
+d5="--disable-ext_delta_q --disable-loopfiltering_across_tiles --disable-ec_smallmul"
+d6="--disable-var_tx --disable-ext_inter --disable-wedge --disable-compound_segment"
+d7="--disable-interintra --disable-one_sided_compound --disable-smooth_hv"
+d8="--disable-parallel_deblocking --disable-rect_intra_pred --disable-convolve_round"
+d9="--disable-palette_throughput --disable-tempmv_signaling --disable-ext-comp-refs"
 
-../$libsrc/configure $common $disabled --enable-$tool > /dev/null
+../$libsrc/configure $common $d1 $d2 $d3 $d4 $d5 $d6 $d7 $d8 $d9 --enable-$tool > /dev/null
 if [ $? -ne 0 ]; then
   echo "Error: configure fails!" > $test_dir/error_config.txt
   exit 1
