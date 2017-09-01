efbece29455ef3e4c7ac96a509f14e16bfe14cdc
Yi Luo <luoyi@google.com>
2017-09-01 10:02:10 -0700

    Disable parallel_deblocking in daily run
    Change-Id: I2443611dfa2f353b36a898341aef5e6a050f1042

--- scripts/nightly_config.sh
+++ scripts/nightly_config.sh
@@ -16,7 +16,7 @@ d4="--disable-dual_filter --disable-motion_var --disable-warped_motion"
 d5="--disable-ext_delta_q --disable-loopfiltering_across_tiles --disable-ec_smallmul"
 d6="--disable-var_tx --disable-ext_inter --disable-wedge --disable-compound_segment"
 d7="--disable-interintra --disable-one_sided_compound --disable-smooth_hv"
-d8="--disable-rect_intra_pred --disable-convolve_round"
+d8="--disable-rect_intra_pred --disable-convolve_round --disable-parallel_deblocking"
 d9="--disable-palette_throughput --disable-tempmv_signaling --disable-ext-comp-refs"
 
 ../$libsrc/configure $debug $common $d1 $d2 $d3 $d4 $d5 $d6 $d7 $d8 $d9 $tool > /dev/null
