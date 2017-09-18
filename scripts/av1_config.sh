5d58690a319c17e6fd1d3329025048b204664613
Yi Luo <luoyi@google.com>
2017-09-18 10:04:41 -0700

    We use the 2nd argument to control debug build or not
    Change-Id: Id4dc9fd1a60badcb193650c264a2bcee5a5038bf

--- scripts/av1_config.sh
+++ scripts/av1_config.sh
@@ -1,16 +1,24 @@
 #!/bin/sh
 #set -x
 
+if [ "$2" == "dbg" ]; then
+  debug="--enable-debug --disable-optimizations"
+else
+  debug=
+fi
+tool=--enable-$1
+
 platform=x86_64-linux-gcc
 codec=--enable-av1
 libsrc=aom
 test_dir=~/Dev/info
 script_dir=~/Dev/sandbox/libvpx/scripts
 
-tool=--enable-$1
+
 common="--disable-docs --enable-experimental"
-#debug="--enable-debug --disable-optimizations"
-bd_config="--enable-lowbitdepth --disable-highbitdepth"
+
+bd_config="--enable-lowbitdepth"
+#--disable-highbitdepth"
 
 . $script_dir/disabled_list.sh
 
