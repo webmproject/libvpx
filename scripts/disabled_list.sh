66e9349debcf5ad276e3b7557dca363983838b70
Yi Luo <luoyi@google.com>
2017-09-28 08:25:50 -0700

    Remove disable-rect-intra-pred flag in configuration
    Refer to:
    df48d293c Remove rect_intra_pred experimental flag.
    
    Change-Id: Ife5c82324337da4cfb6f7cb5e703ed8c5e3f135a

--- scripts/disabled_list.sh
+++ scripts/disabled_list.sh
@@ -7,7 +7,7 @@ d4="--disable-dual_filter --disable-motion_var --disable-warped_motion"
 d5="--disable-ext_delta_q --disable-loopfiltering_across_tiles"
 d6="--disable-var_tx --disable-ext_inter --disable-wedge --disable-compound_segment"
 d7="--disable-interintra --disable-one_sided_compound  --disable-ext-comp-refs"
-d8="--disable-smooth_hv --disable-parallel_deblocking --disable-rect_intra_pred"
+d8="--disable-smooth_hv --disable-parallel_deblocking"
 d9="--disable-convolve_round --disable-aom-qm --disable-dist_8x8"
 d10=" --disable-palette_throughput --disable-tempmv_signaling"
 
