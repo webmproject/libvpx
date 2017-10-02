2df0e4074788e936bc0cbf8769dcb5cfddeaa8ed
Yi Luo <luoyi@google.com>
2017-10-02 16:00:55 -0700

    Remove ext_inter and adapt_q from disabled list
    Change-Id: Id85df9430e0dd2c8d116635c7f2acd6304f27d97

--- scripts/disabled_list.sh
+++ scripts/disabled_list.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 
 d1="--disable-chroma_sub8x8 --disable-filter_7bit --disable-reference_buffer"
-d2="--disable-delta_q --disable-rect_tx --disable-global_motion --disable-ext_tx"
+d2="--disable-rect_tx --disable-global_motion --disable-ext_tx"
 d3="--disable-cdef --disable-ext_intra --disable-mv_compress --disable-ext_refs"
 d4="--disable-dual_filter --disable-motion_var --disable-warped_motion"
 d5="--disable-ext_delta_q --disable-loopfiltering_across_tiles"
-d6="--disable-var_tx --disable-ext_inter --disable-wedge --disable-compound_segment"
+d6="--disable-var_tx --disable-wedge --disable-compound_segment"
 d7="--disable-interintra --disable-one_sided_compound  --disable-ext-comp-refs"
 d8="--disable-smooth_hv --disable-parallel_deblocking"
 d9="--disable-convolve_round --disable-aom-qm --disable-dist_8x8"
