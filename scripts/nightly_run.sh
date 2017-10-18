6308ecd5f7c50a3af0bfee2bf990b71eeeaab6a0
Yi Luo <luoyi@google.com>
2017-10-18 16:30:37 -0700

    Diff tomorrow's new log against today's log
    Anticipate the nightly test will run longer. The
    starting time is changed to evening after work.
    Then the decoding time difference should be tomorrow
    new log compared against today log.
    
    Change-Id: I21ada6f53e8ce28e225ce3338f213ae288c16e5a

--- scripts/nightly_run.sh
+++ scripts/nightly_run.sh
@@ -6,13 +6,13 @@ av1_code=~/Dev/av1d
 vp9_code=~/Dev/vp9d
 
 log_path=~/Dev/log
-#date_str=`date +%H:%M_%b_%d_%Y`
-date_str=`date +%b_%d_%Y`
+
+date_str=`date -d tomorrow +%b_%d_%Y`
 log_file=report_$date_str.txt
 html_log_file=report_$date_str.html
 
-yest_str=`date -d yesterday +%b_%d_%Y`
-yest_log_file=report_$yest_str.txt
+prev_date_str=`date +%b_%d_%Y`
+prev_log_file=report_$prev_date_str.txt
 
 test_dir=~/Dev/nightly
 rm $test_dir/*
@@ -28,10 +28,10 @@ echo "<p>" >> $log_path/$html_log_file
 $script_path/av1_conf_build.sh $av1_code >> $log_path/$html_log_file 2>&1
 echo "</p>" >> $log_path/$html_log_file
 
-pdfps=`cat $log_path/$yest_log_file | grep e_ok | awk '{print $2}' | awk 'NR==1 {print $1}'`
+pdfps=`cat $log_path/$prev_log_file | grep e_ok | awk '{print $2}' | awk 'NR==1 {print $1}'`
 $script_path/nightly_speed.sh $av1_code 0 $pdfps $html_log_file >> $log_path/$log_file 2>&1
 
-pdfps=`cat $log_path/$yest_log_file | grep e_ok | awk '{print $2}' | awk 'NR==2 {print $1}'`
+pdfps=`cat $log_path/$prev_log_file | grep e_ok | awk '{print $2}' | awk 'NR==2 {print $1}'`
 $script_path/nightly_speed.sh $av1_code 2 $pdfps $html_log_file >> $log_path/$log_file 2>&1
 
 # VP9
@@ -43,14 +43,14 @@ echo "<p>" >> $log_path/$html_log_file
 $script_path/vp9_conf_build.sh $vp9_code lowbitdepth >> $log_path/$html_log_file 2>&1
 echo "</p>" >> $log_path/$html_log_file
 
-pdfps=`cat $log_path/$yest_log_file | grep e_ok | awk '{print $2}' | awk 'NR==3 {print $1}'`
+pdfps=`cat $log_path/$prev_log_file | grep e_ok | awk '{print $2}' | awk 'NR==3 {print $1}'`
 $script_path/vp9_nightly_speed.sh $vp9_code 0 $pdfps $html_log_file >> $log_path/$log_file 2>&1
 
 echo "<p>" >> $log_path/$html_log_file
 $script_path/vp9_conf_build.sh $vp9_code highbitdepth >> $log_path/$log_file 2>&1
 echo "</p>" >> $log_path/$html_log_file
 
-pdfps=`cat $log_path/$yest_log_file | grep e_ok | awk '{print $2}' | awk 'NR==4 {print $1}'`
+pdfps=`cat $log_path/$prev_log_file | grep e_ok | awk '{print $2}' | awk 'NR==4 {print $1}'`
 $script_path/vp9_nightly_speed.sh $vp9_code 2 $pdfps $html_log_file >> $log_path/$log_file 2>&1
 
 users=luoyi
