40e468d69cfdab42009da4b19bdeda8674d72ae8
Yi Luo <luoyi@google.com>
2017-10-25 08:20:08 -0700

    Disable new_multisymbol
    Change-Id: I55018a7d4ecbff792789af6fe3e7a0c39ab3e578

--- scripts/disabled_list.sh
+++ scripts/disabled_list.sh
@@ -1,6 +1,5 @@
 #!/bin/sh
 
-#d1="--disable-chroma_sub8x8"
 d2="--disable-rect_tx --disable-global_motion --disable-ext_tx"
 d3="--disable-cdef --disable-ext_intra --disable-intra-edge --disable-mv_compress"
 d4="--disable-dual_filter --disable-motion_var --disable-warped_motion"
@@ -9,11 +8,14 @@ d6="--disable-interintra --disable-one_sided_compound"
 d7="--disable-convolve_round --disable-aom-qm --disable-dist_8x8"
 d8="--disable-loop_restoration"
 d9="--disable-ext-partition --disable-ext-partition-types"
-#d10="--disable-txmg"
+d91="--disable-new-multisymbol"
 d10="--disable-reference_buffer --disable-loopfiltering_across_tiles"
 d11="--disable-palette_throughput --disable-smooth_hv --disable-tempmv_signaling"
 d12="--disable-ext-comp-refs --disable-ext_delta_q --disable-parallel_deblocking"
 d13="--disable-simple_bwd_adapt --disable-loopfilter-level"
 
+#d1="--disable-chroma_sub8x8"
+#d10="--disable-txmg"
 # --disable-cdef_singlepass
+
 disabled="$d2 $d3 $d4 $d5 $d6 $d7 $d8 $d9 $d10 $d11 $d12 $d13"
