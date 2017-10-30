726bc7e0d6c93298a260665e8c3ac0ad4bea4bf2
Yi Luo <luoyi@google.com>
2017-10-30 15:52:43 -0700

    Remove wedge and compound-segment
    Change-Id: I66d775147e436530164d350f077a592073d0b2fa

--- scripts/disabled_list.sh
+++ scripts/disabled_list.sh
@@ -3,7 +3,7 @@
 d2="--disable-ext_tx"
 d3="--disable-cdef --disable-ext_intra --disable-intra-edge --disable-mv_compress"
 d4="--disable-dual_filter --disable-motion_var --disable-warped_motion"
-d5="--disable-wedge --disable-compound_segment"
+#d5="--disable-wedge --disable-compound_segment"
 d6="--disable-interintra --disable-one_sided_compound"
 d7="--disable-convolve_round --disable-aom-qm --disable-dist_8x8"
 d8="--disable-loop_restoration"
@@ -17,4 +17,4 @@ d13="--disable-simple_bwd_adapt --disable-loopfilter-level"
 #d10="--disable-txmg"
 # --disable-cdef_singlepass
 
-disabled="$d2 $d3 $d4 $d5 $d6 $d7 $d8 $d9 $d10 $d11 $d12 $d13"
+disabled="$d2 $d3 $d4 $d6 $d7 $d8 $d9 $d10 $d11 $d12 $d13"
