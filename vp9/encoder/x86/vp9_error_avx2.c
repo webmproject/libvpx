6746ba6d01efdaac207fabf34899999a778c661d
Johann <johannkoenig@google.com>
2017-12-14 11:54:16 -0800

    fix typo in boilerplate
    The extra 'e' was causing the chromium license check to flag this file.
    
    BUG=chromium:98319
    
    Change-Id: Ic875ba66370298bf998438d14ff5f7e760293706

--- vp9/encoder/x86/vp9_error_avx2.c
+++ vp9/encoder/x86/vp9_error_avx2.c
@@ -1,7 +1,7 @@
 /*
  *  Copyright (c) 2014 The WebM project authors. All Rights Reserved.
  *
- *  Usee of this source code is governed by a BSD-style license
+ *  Use of this source code is governed by a BSD-style license
  *  that can be found in the LICENSE file in the root of the source
  *  tree. An additional intellectual property rights grant can be found
  *  in the file PATENTS.  All contributing project authors may
