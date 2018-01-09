bf88f9d0ec03f9e450b75fa78f70db2f0fc6d80a
Yi Luo <luoyi@google.com>
2018-01-09 09:30:26 -0800

    Change the email subject to av1 nightly report
    Change-Id: Ie3d58b847975b5bc5e8a11f84a0e2cd7b593e611

--- scripts/manemail.sh
+++ scripts/manemail.sh
@@ -8,4 +8,4 @@ report_html=$1
 report_path=/usr/local/google/home/luoyi/Dev/log
 report_send=$report_path/$report_html
 
-sendgmr --to=luoyi --cc=yunqingwang,vpx-eng --from=luoyi --reply_to=luoyi --subject="Codec Daily Report" --html_file=$report_send --body_file=$report_send
+sendgmr --to=luoyi --cc=yunqingwang,vpx-eng --from=luoyi --reply_to=luoyi --subject="AV1 Nightly Speed Report" --html_file=$report_send --body_file=$report_send
