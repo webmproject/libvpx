b14b616d968b3cb1b955651754b9761d72bd05ac
Linfeng Zhang <linfengz@google.com>
2018-01-29 15:17:08 -0800

    Update vp9_iht8x8_64_add_neon()
    Change-Id: Ie70ed8b9273df5e1fd06bc93cb469e80630941d2

--- vpx_dsp/arm/idct8x8_add_neon.c
+++ vpx_dsp/arm/idct8x8_add_neon.c
@@ -17,28 +17,6 @@
 #include "vpx_dsp/arm/transpose_neon.h"
 #include "vpx_dsp/txfm_common.h"
 
-static INLINE void idct8x8_add8x1(const int16x8_t a, uint8_t **const dest,
-                                  const int stride) {
-  const uint8x8_t s = vld1_u8(*dest);
-  const int16x8_t res = vrshrq_n_s16(a, 5);
-  const uint16x8_t q = vaddw_u8(vreinterpretq_u16_s16(res), s);
-  const uint8x8_t d = vqmovun_s16(vreinterpretq_s16_u16(q));
-  vst1_u8(*dest, d);
-  *dest += stride;
-}
-
-static INLINE void add8x8(int16x8_t *const out, uint8_t *dest,
-                          const int stride) {
-  idct8x8_add8x1(out[0], &dest, stride);
-  idct8x8_add8x1(out[1], &dest, stride);
-  idct8x8_add8x1(out[2], &dest, stride);
-  idct8x8_add8x1(out[3], &dest, stride);
-  idct8x8_add8x1(out[4], &dest, stride);
-  idct8x8_add8x1(out[5], &dest, stride);
-  idct8x8_add8x1(out[6], &dest, stride);
-  idct8x8_add8x1(out[7], &dest, stride);
-}
-
 void vpx_idct8x8_64_add_neon(const tran_low_t *input, uint8_t *dest,
                              int stride) {
   const int16x8_t cospis = vld1q_s16(kCospi);
@@ -57,7 +35,7 @@ void vpx_idct8x8_64_add_neon(const tran_low_t *input, uint8_t *dest,
 
   idct8x8_64_1d_bd8(cospis0, cospis1, a);
   idct8x8_64_1d_bd8(cospis0, cospis1, a);
-  add8x8(a, dest, stride);
+  idct8x8_add8x8_neon(a, dest, stride);
 }
 
 void vpx_idct8x8_12_add_neon(const tran_low_t *input, uint8_t *dest,
@@ -77,5 +55,5 @@ void vpx_idct8x8_12_add_neon(const tran_low_t *input, uint8_t *dest,
 
   idct8x8_12_pass1_bd8(cospis0, cospisd0, cospisd1, a);
   idct8x8_12_pass2_bd8(cospis0, cospisd0, cospisd1, a, b);
-  add8x8(b, dest, stride);
+  idct8x8_add8x8_neon(b, dest, stride);
 }
