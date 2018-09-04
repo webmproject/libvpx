88cd871418a790b29ad24e70d9de97494903ae53
Hui Su <huisu@google.com>
2018-09-04 10:05:15 -0700

    Move partition search ML models to a seperate file
    Clean up vp9_encodeframe.c.
    
    Change-Id: I4035fee94da746c74d72f71ca8334f91c5d10116

--- vp9/vp9cx.mk
+++ vp9/vp9cx.mk
@@ -64,6 +64,7 @@ VP9_CX_SRCS-yes += encoder/vp9_ratectrl.c
 VP9_CX_SRCS-yes += encoder/vp9_rd.c
 VP9_CX_SRCS-yes += encoder/vp9_rdopt.c
 VP9_CX_SRCS-yes += encoder/vp9_pickmode.c
+VP9_CX_SRCS-yes += encoder/vp9_partition_models.h
 VP9_CX_SRCS-yes += encoder/vp9_segmentation.c
 VP9_CX_SRCS-yes += encoder/vp9_segmentation.h
 VP9_CX_SRCS-yes += encoder/vp9_speed_features.c
