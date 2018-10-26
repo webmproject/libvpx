17004c71bcd7ae7e6a65bd5b8e756fb2311accdc
Johann <johann.koenig@duck.com>
2018-10-26 14:55:26 -0700

    remove "register" keyword
    This has been deprecated for a long time. c++17 is trying to recover the name.
    
    Change-Id: Iade6bebce03a50b76061695f9e634a107cd989cd

--- vp8/decoder/treereader.h
+++ vp8/decoder/treereader.h
@@ -30,7 +30,7 @@ typedef BOOL_DECODER vp8_reader;
 static INLINE int vp8_treed_read(
     vp8_reader *const r, /* !!! must return a 0 or 1 !!! */
     vp8_tree t, const vp8_prob *const p) {
-  register vp8_tree_index i = 0;
+  vp8_tree_index i = 0;
 
   while ((i = t[i + vp8_read(r, p[i >> 1])]) > 0) {
   }
