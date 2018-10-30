dc89abccdb6544b69a95c484a54307159494f786
Johann <johann.koenig@duck.com>
2018-10-30 11:56:17 -0700

    clang-tidy: fix vp9/common parameters
    BUG=webm:1444
    
    Change-Id: I1a14ad119b3bcbaddcf2291a7521513cf6425635

--- vp9/common/vp9_scale.h
+++ vp9/common/vp9_scale.h
@@ -42,7 +42,7 @@ MV32 vp9_scale_mv(const MV *mv, int x, int y, const struct scale_factors *sf);
 #if CONFIG_VP9_HIGHBITDEPTH
 void vp9_setup_scale_factors_for_frame(struct scale_factors *sf, int other_w,
                                        int other_h, int this_w, int this_h,
-                                       int use_high);
+                                       int use_highbd);
 #else
 void vp9_setup_scale_factors_for_frame(struct scale_factors *sf, int other_w,
                                        int other_h, int this_w, int this_h);
