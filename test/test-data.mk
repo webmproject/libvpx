bb3a82ec36358d2c0b439b36e86e925ae870ea5c
Jerome Jiang <jianj@google.com>
2018-12-06 18:24:13 -0800

    vp9 svc: add test for scaling partition on 1080p crash.
    BUG=webm:1578
    Change-Id: Ie03ed454394933fa89f751edc6928651393f3f12

--- test/test-data.mk
+++ test/test-data.mk
@@ -4,6 +4,7 @@ LIBVPX_TEST_SRCS-yes += test-data.mk
 LIBVPX_TEST_DATA-$(CONFIG_ENCODERS) += hantro_collage_w352h288.yuv
 LIBVPX_TEST_DATA-$(CONFIG_ENCODERS) += hantro_odd.yuv
 LIBVPX_TEST_DATA-$(CONFIG_ENCODERS) += desktop_office1.1280_720-020.yuv
+LIBVPX_TEST_DATA-$(CONFIG_ENCODERS) += slides_code_term_web_plot.1920_1080.yuv
 
 LIBVPX_TEST_DATA-$(CONFIG_ENCODERS) += park_joy_90p_10_420.y4m
 LIBVPX_TEST_DATA-$(CONFIG_ENCODERS) += park_joy_90p_10_422.y4m
