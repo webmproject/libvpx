1358fdf2cb3ff608fa71ab273e980bef57e61408
Jingning Han <jingning@google.com>
2018-12-17 10:30:17 -0800

    Add PSNR_AQ mode
    Placeholder to support adaptive quantizer for PSNR and SSIM coding
    quality improvement.
    
    Change-Id: Id967c9914bb1d72a6f480ef1ba9d6650914dd658

--- vp9/encoder/vp9_encoder.h
+++ vp9/encoder/vp9_encoder.h
@@ -119,9 +119,10 @@ typedef enum {
   COMPLEXITY_AQ = 2,
   CYCLIC_REFRESH_AQ = 3,
   EQUATOR360_AQ = 4,
+  PSNR_AQ = 5,
   // AQ based on lookahead temporal
   // variance (only valid for altref frames)
-  LOOKAHEAD_AQ = 5,
+  LOOKAHEAD_AQ = 6,
   AQ_MODE_COUNT  // This should always be the last member of the enum
 } AQ_MODE;
 
