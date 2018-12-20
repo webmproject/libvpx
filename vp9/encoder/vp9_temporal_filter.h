3b013f7ff8650219aaa9c99cb608441f0d7e18c2
Yunqing Wang <yunqingwang@google.com>
2018-12-19 17:03:30 -0800

    Refactor temporal filtering
    Refactored temporal filtering, so that it was not hard-coded to
    16x16 block size.
    
    Change-Id: I06d0787660ff6eee6a8f02a846ad0e26c6825f54

--- vp9/encoder/vp9_temporal_filter.h
+++ vp9/encoder/vp9_temporal_filter.h
@@ -17,6 +17,29 @@ extern "C" {
 
 #define ARNR_FILT_QINDEX 128
 
+// Block size used in temporal filtering
+#if 1
+#define TF_BLOCK BLOCK_16X16
+#define BH 16
+#define BH_LOG2 4
+#define BW 16
+#define BW_LOG2 4
+#define BLK_PELS 256  // Pixels in the block
+#define TF_SHIFT 1
+#define TF_ROUND 1
+#define THR_SHIFT 0
+#else
+#define TF_BLOCK BLOCK_32X32
+#define BH 32
+#define BH_LOG2 5
+#define BW 32
+#define BW_LOG2 5
+#define BLK_PELS 1024  // Pixels in the block
+#define TF_SHIFT 2
+#define TF_ROUND 3
+#define THR_SHIFT 2
+#endif
+
 void vp9_temporal_filter_init(void);
 void vp9_temporal_filter(VP9_COMP *cpi, int distance);
 
