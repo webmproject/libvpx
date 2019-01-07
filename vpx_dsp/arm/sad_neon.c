57d476bd8f86cf2df5821605ecb2a7e1d132b938
Johann <johannkoenig@google.com>
2019-01-07 14:05:58 -0800

    arm neon: resolve missing declarations
    BUG=webm:1584
    
    Change-Id: I2dcf39f2327b72b58be72c27f952ea781a790dd3

--- vpx_dsp/arm/sad_neon.c
+++ vpx_dsp/arm/sad_neon.c
@@ -11,6 +11,7 @@
 #include <arm_neon.h>
 
 #include "./vpx_config.h"
+#include "./vpx_dsp_rtcd.h"
 
 #include "vpx/vpx_integer.h"
 #include "vpx_dsp/arm/mem_neon.h"
--- vpx_dsp/arm/subtract_neon.c
+++ vpx_dsp/arm/subtract_neon.c
@@ -12,6 +12,7 @@
 #include <assert.h>
 
 #include "./vpx_config.h"
+#include "./vpx_dsp_rtcd.h"
 #include "vpx/vpx_integer.h"
 #include "vpx_dsp/arm/mem_neon.h"
 
