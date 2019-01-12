495282774eb033896044cabfb1f05ce59e66831d
James Zern <jzern@google.com>
2019-01-11 16:36:45 -0800

    convolve_test: Add missing init of HBD buffers
    this resolves some msan errors.
    the same change was done in libaom:
    5ab58722c Add missing initializations of HBD buffers
    
    Change-Id: I8882af45b95c90ba43bf138c7d305a6c3b99e61c

--- test/convolve_test.cc
+++ test/convolve_test.cc
@@ -416,8 +416,14 @@ class ConvolveTest : public ::testing::TestWithParam<ConvolveParam> {
     for (int i = 0; i < kOutputBufferSize; ++i) {
       if (IsIndexInBorder(i)) {
         output_[i] = 255;
+#if CONFIG_VP9_HIGHBITDEPTH
+        output16_[i] = mask_;
+#endif
       } else {
         output_[i] = 0;
+#if CONFIG_VP9_HIGHBITDEPTH
+        output16_[i] = 0;
+#endif
       }
     }
 
