b580f76dbce59fdd0e88a02ca954fa81bcd0cf31
chiyotsai <chiyotsai@google.com>
2019-01-16 15:33:06 -0800

    Add SSE4 version of new apply_temporal_filter
    This adds a preliminary version of vp9_apply_temporal_filter in SSE4.1.
    This patch merely adds the function and does not enable it yet.
    
    Speed Up:
             | ss_x=1 | ss_x=0 |
      ss_y=1 | 19.80X | 19.04X |
      ss_y=0 | 21.09X | 20.21X |
    
    BUG=webm:1591
    
    Change-Id: If590f1ccf1d0c6c3b47410541d54f2ce37d8305b

--- vp9/common/vp9_rtcd_defs.pl
+++ vp9/common/vp9_rtcd_defs.pl
@@ -188,6 +188,7 @@ add_proto qw/void vp9_temporal_filter_apply/, "const uint8_t *frame1, unsigned i
 specialize qw/vp9_temporal_filter_apply sse4_1/;
 
 add_proto qw/void vp9_apply_temporal_filter/, "const uint8_t *y_src, int y_src_stride, const uint8_t *y_pre, int y_pre_stride, const uint8_t *u_src, const uint8_t *v_src, int uv_src_stride, const uint8_t *u_pre, const uint8_t *v_pre, int uv_pre_stride, unsigned int block_width, unsigned int block_height, int ss_x, int ss_y, int strength, const int *const blk_fw, int use_32x32, uint32_t *y_accumulator, uint16_t *y_count, uint32_t *u_accumulator, uint16_t *u_count, uint32_t *v_accumulator, uint16_t *v_count";
+specialize qw/vp9_apply_temporal_filter sse4_1/;
 }
 
 if (vpx_config("CONFIG_VP9_HIGHBITDEPTH") eq "yes") {
