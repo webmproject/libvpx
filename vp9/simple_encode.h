9b79c51b02c05b592d7baa2d769acc213f62bf2e
angiebird <angiebird@google.com>
2019-11-13 13:39:23 -0800

    Add copyright and header guard for simple_encode.h
    Change-Id: Ib4502fc35202b36aa25f06c7c2bb5203673faa06

--- vp9/simple_encode.h
+++ vp9/simple_encode.h
@@ -1,3 +1,15 @@
+/*
+ *  Copyright (c) 2019 The WebM project authors. All Rights Reserved.
+ *
+ *  Use of this source code is governed by a BSD-style license
+ *  that can be found in the LICENSE file in the root of the source
+ *  tree. An additional intellectual property rights grant can be found
+ *  in the file PATENTS.  All contributing project authors may
+ *  be found in the AUTHORS file in the root of the source tree.
+ */
+
+#ifndef VPX_VP9_SIMPLE_ENCODE_H_
+#define VPX_VP9_SIMPLE_ENCODE_H_
 #include <memory>
 #include <vector>
 
@@ -69,3 +81,4 @@ class SimpleEncode {
   FILE *file;
   std::unique_ptr<impl> pimpl;
 };
+#endif  // VPX_VP9_SIMPLE_ENCODE
