1d49040369c3e553f13ffb1690c4a27a58796abd
Jerome Jiang <jianj@google.com>
2019-12-04 12:46:56 -0800

    remove init_motion_estimation from update_initial_width
    Change-Id: I04da24eb6a87425490b25e50ead7a8fd8117e7cb

--- vp9/encoder/vp9_encoder.c
+++ vp9/encoder/vp9_encoder.c
@@ -2171,8 +2171,6 @@ static void init_ref_frame_bufs(VP9_COMMON *cm) {
   }
 }
 
-static void init_motion_estimation(VP9_COMP *cpi);
-
 static void update_initial_width(VP9_COMP *cpi, int use_highbitdepth,
                                  int subsampling_x, int subsampling_y) {
   VP9_COMMON *const cm = &cpi->common;
@@ -2193,7 +2191,6 @@ static void update_initial_width(VP9_COMP *cpi, int use_highbitdepth,
     cm->use_highbitdepth = use_highbitdepth;
 #endif
     alloc_util_frame_buffers(cpi);
-    init_motion_estimation(cpi);
     cpi->initial_width = cm->width;
     cpi->initial_height = cm->height;
     cpi->initial_mbs = cm->MBs;
