b809ac3c9e5037286c0401897fc83d6305261913
James Zern <jzern@google.com>
2020-01-10 16:47:01 -0800

    simple_encode*.cc: add missing copyright
    Change-Id: I58ddf13698e3892aa591af4196ca03d7c09426c6

--- vp9/simple_encode.cc
+++ vp9/simple_encode.cc
@@ -1,3 +1,13 @@
+/*
+ *  Copyright (c) 2019 The WebM project authors. All Rights Reserved.
+ *
+ *  Use of this source code is governed by a BSD-style license
+ *  that can be found in the LICENSE file in the root of the source
+ *  tree. An additional intellectual property rights grant can be found
+ *  in the file PATENTS.  All contributing project authors may
+ *  be found in the AUTHORS file in the root of the source tree.
+ */
+
 #include <vector>
 #include "vp9/common/vp9_onyxc_int.h"
 #include "vp9/vp9_iface_common.h"
