05e0cd7c4f21f10ee62010f80d20486e9b16b7af
angiebird <angiebird@google.com>
2020-07-15 14:52:02 -0700

    Build libsimple_encode.a separately
    BUG=webm:1689
    
    Change-Id: Id920816315c6586cd652ba6cd1b3a76dfc1f12b7
    (cherry picked from commit 56345d256ae191e6de042ad82ccf458de3102b8a)

--- test/simple_encode_test.cc
+++ test/simple_encode_test.cc
@@ -473,3 +473,8 @@ TEST(SimpleEncode, GetFramePixelCount) {
 
 }  // namespace
 }  // namespace vp9
+
+int main(int argc, char **argv) {
+  ::testing::InitGoogleTest(&argc, argv);
+  return RUN_ALL_TESTS();
+}
