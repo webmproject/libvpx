8ac72859e16934df6e598283997d141e9493fc05
Jerome Jiang <jianj@google.com>
2022-05-03 10:44:49 -0400

    vp9 svc sample: set fps from y4m file
    Change-Id: I082c0409910da4cda5bf852b20ffa11ba5c2ebd6

--- examples/vp9_spatial_svc_encoder.c
+++ examples/vp9_spatial_svc_encoder.c
@@ -361,6 +361,8 @@ static void parse_command_line(int argc, const char **argv_,
   if (app_input->input_ctx.file_type == FILE_TYPE_Y4M) {
     enc_cfg->g_w = app_input->input_ctx.width;
     enc_cfg->g_h = app_input->input_ctx.height;
+    enc_cfg->g_timebase.den = app_input->input_ctx.framerate.numerator;
+    enc_cfg->g_timebase.num = app_input->input_ctx.framerate.denominator;
   }
 
   if (enc_cfg->g_w < 16 || enc_cfg->g_w % 2 || enc_cfg->g_h < 16 ||
