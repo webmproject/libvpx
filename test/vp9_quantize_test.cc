17959f9c94be43de57972052ebfdc40870170b0e
yuanhecai <yuanhecai@loongson.cn>
2022-05-06 12:17:39 +0800

    vp9[loongarch]: Optimize vpx_quantize_b/b_32x32
    1. vpx_quantize_b_lsx
    2. vpx_quantize_b_32x32_lsx
    
    Bug: webm:1755
    
    Change-Id: I476c8677a2c2aed7248e088e62c3777c9bed2adb

--- test/vp9_quantize_test.cc
+++ test/vp9_quantize_test.cc
@@ -568,6 +568,16 @@ INSTANTIATE_TEST_SUITE_P(
                                  VPX_BITS_8, 32, true)));
 #endif  // HAVE_VSX && !CONFIG_VP9_HIGHBITDEPTH
 
+#if HAVE_LSX && !CONFIG_VP9_HIGHBITDEPTH
+INSTANTIATE_TEST_SUITE_P(LSX, VP9QuantizeTest,
+                         ::testing::Values(make_tuple(&vpx_quantize_b_lsx,
+                                                      &vpx_quantize_b_c,
+                                                      VPX_BITS_8, 16, false),
+                                           make_tuple(&vpx_quantize_b_32x32_lsx,
+                                                      &vpx_quantize_b_32x32_c,
+                                                      VPX_BITS_8, 32, false)));
+#endif  // HAVE_LSX && !CONFIG_VP9_HIGHBITDEPTH
+
 // Only useful to compare "Speed" test results.
 INSTANTIATE_TEST_SUITE_P(
     DISABLED_C, VP9QuantizeTest,
