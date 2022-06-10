46bfeed2c9a7e52c8d1624f9e388af137e02ff19
Wan-Teh Chang <wtc@google.com>
2022-06-10 13:52:31 -0700

    Convert EncoderTest::last_pts_ to a local variable
    Convert the data member EncoderTest::last_pts_ to a local variable in
    the EncoderTest::RunLoop() and VP9FrameSizeTestsLarge::RunLoop()
    methods. EncoderTest::last_pts_ is only used in these two methods, and
    these two methods first set EncoderTest::last_pts_ to 0 before using it.
    So EncoderTest::last_pts_ is effectively a local variable in these two
    methods.
    
    Note that several subclasses of EncoderTest declare their own last_pts_
    data member and use it to calculate the data rate. Apparently their own
    last_pts_ data member hides the same-named data member in the base
    class. Although this is allowed by C++, this is very confusing.
    
    Change-Id: I55ce1cf8cc62e07333d8a902d65b46343a3d5881

--- test/encode_test_driver.h
+++ test/encode_test_driver.h
@@ -206,8 +206,7 @@ class Encoder {
 class EncoderTest {
  protected:
   explicit EncoderTest(const CodecFactory *codec)
-      : codec_(codec), abort_(false), init_flags_(0), frame_flags_(0),
-        last_pts_(0) {
+      : codec_(codec), abort_(false), init_flags_(0), frame_flags_(0) {
     // Default to 1 thread.
     cfg_.g_threads = 1;
   }
@@ -291,7 +290,6 @@ class EncoderTest {
   TwopassStatsStore stats_;
   unsigned long init_flags_;
   unsigned long frame_flags_;
-  vpx_codec_pts_t last_pts_;
 };
 
 }  // namespace libvpx_test
