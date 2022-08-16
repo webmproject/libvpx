d939886809d1bf8428d7bfb515fa8ff544c44fe7
James Zern <jzern@google.com>
2022-08-16 16:52:06 -0700

    load_unaligned_u32: use an int w/_mm_cvtsi32_si128
    this matches the type of the function parameter; quiets integer
    sanitizer warnings of the form:
    implicit conversion from type 'uint32_t' (aka 'unsigned int') of value
    3215646151 (32-bit, unsigned) to type 'int' changed the value to
    -1079321145 (32-bit, signed)
    
    Bug: b/229626362
    Change-Id: Ia9a5dc5e1f57cbf4f8f8fa457bb674ef43369d37

--- vpx_dsp/x86/mem_sse2.h
+++ vpx_dsp/x86/mem_sse2.h
@@ -27,7 +27,7 @@ static INLINE int32_t loadu_int32(const void *src) {
 }
 
 static INLINE __m128i load_unaligned_u32(const void *a) {
-  uint32_t val;
+  int val;
   memcpy(&val, a, sizeof(val));
   return _mm_cvtsi32_si128(val);
 }
