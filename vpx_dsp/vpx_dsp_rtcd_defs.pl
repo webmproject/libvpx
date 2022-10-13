5d26626e7aabaa8cd0aeb01618191216d2db90b9
Andrew Salkeld <andrew.salkeld@arm.com>
2022-10-13 16:28:41 +0100

    Add Neon implementation of vpx_hadamard_32x32
    Add an Arm Neon implementation of vpx_hadamard_32x32 and use it
    instead of the scalar C implementation.
    
    Also add test coverage for the new Neon implementation.
    
    Change-Id: Iccc018eec4dbbe629fb0c6f8ad6ea8554e7a0b13

--- vpx_dsp/vpx_dsp_rtcd_defs.pl
+++ vpx_dsp/vpx_dsp_rtcd_defs.pl
@@ -799,7 +799,7 @@ if (vpx_config("CONFIG_VP9_ENCODER") eq "yes") {
     specialize qw/vpx_hadamard_16x16 avx2 sse2 neon vsx lsx/;
 
     add_proto qw/void vpx_hadamard_32x32/, "const int16_t *src_diff, ptrdiff_t src_stride, tran_low_t *coeff";
-    specialize qw/vpx_hadamard_32x32 sse2 avx2/;
+    specialize qw/vpx_hadamard_32x32 sse2 avx2 neon/;
 
     add_proto qw/void vpx_highbd_hadamard_8x8/, "const int16_t *src_diff, ptrdiff_t src_stride, tran_low_t *coeff";
     specialize qw/vpx_highbd_hadamard_8x8 avx2/;
@@ -823,7 +823,7 @@ if (vpx_config("CONFIG_VP9_ENCODER") eq "yes") {
     specialize qw/vpx_hadamard_16x16 avx2 sse2 neon msa vsx lsx/;
 
     add_proto qw/void vpx_hadamard_32x32/, "const int16_t *src_diff, ptrdiff_t src_stride, int16_t *coeff";
-    specialize qw/vpx_hadamard_32x32 sse2 avx2/;
+    specialize qw/vpx_hadamard_32x32 sse2 avx2 neon/;
 
     add_proto qw/int vpx_satd/, "const int16_t *coeff, int length";
     specialize qw/vpx_satd avx2 sse2 neon msa/;
