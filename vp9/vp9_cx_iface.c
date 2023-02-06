5eea5c76669fefb07e5dcc4677942c28a5de4257
Jerome Jiang <jianj@google.com>
2023-02-06 13:29:58 -0500

    Remove duplicated VPX_SCALING declaration
    Use VPX_SCALING_MODE instead
    
    Change-Id: Iab9d29f20838703e00bd9f7641035d8ebd69af53

--- vp9/vp9_cx_iface.c
+++ vp9/vp9_cx_iface.c
@@ -1675,9 +1675,8 @@ static vpx_codec_err_t ctrl_set_scale_mode(vpx_codec_alg_priv_t *ctx,
   vpx_scaling_mode_t *const mode = va_arg(args, vpx_scaling_mode_t *);
 
   if (mode) {
-    const int res =
-        vp9_set_internal_size(ctx->cpi, (VPX_SCALING)mode->h_scaling_mode,
-                              (VPX_SCALING)mode->v_scaling_mode);
+    const int res = vp9_set_internal_size(ctx->cpi, mode->h_scaling_mode,
+                                          mode->v_scaling_mode);
     return (res == 0) ? VPX_CODEC_OK : VPX_CODEC_INVALID_PARAM;
   }
   return VPX_CODEC_INVALID_PARAM;
