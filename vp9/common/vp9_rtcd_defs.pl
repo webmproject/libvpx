a9d91d7a0a2a8ed3fb6806aabfd1f241d77688c1
Gerda Zsejke More <gerdazsejke.more@arm.com>
2024-01-27 12:01:11 +0100

    Add SVE implementation of vp9_block_error function
    Add SVE implementation of vp9_block_error function. Add the
    corresponding tests as well.
    
    Change-Id: Iebba73ee845855be939e120326e1005237230c2a

--- vp9/common/vp9_rtcd_defs.pl
+++ vp9/common/vp9_rtcd_defs.pl
@@ -138,12 +138,12 @@ add_proto qw/void vp9_quantize_fp_32x32/, "const tran_low_t *coeff_ptr, intptr_t
 specialize qw/vp9_quantize_fp_32x32 neon ssse3 avx2 vsx/;
 
 if (vpx_config("CONFIG_VP9_HIGHBITDEPTH") eq "yes") {
-  specialize qw/vp9_block_error neon avx2 sse2/;
+  specialize qw/vp9_block_error neon sve avx2 sse2/;
 
   add_proto qw/int64_t vp9_highbd_block_error/, "const tran_low_t *coeff, const tran_low_t *dqcoeff, intptr_t block_size, int64_t *ssz, int bd";
   specialize qw/vp9_highbd_block_error neon sse2/;
 } else {
-  specialize qw/vp9_block_error neon avx2 msa sse2/;
+  specialize qw/vp9_block_error neon sve avx2 msa sse2/;
 }
 
 # fdct functions
