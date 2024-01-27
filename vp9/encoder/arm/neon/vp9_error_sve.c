58fb0f1d2730374e3516acbb4f744549b12c4a89
Gerda Zsejke More <gerdazsejke.more@arm.com>
2024-01-27 12:49:41 +0100

    Add SVE implementation of vp9_block_error_fp
    Add SVE implementation of vp9_block_error_fp function. Add the
    corresponding tests as well.
    
    Change-Id: I81f4b11bd2f1d0b9f377553bb9298d735308da30

--- vp9/encoder/arm/neon/vp9_error_sve.c
+++ vp9/encoder/arm/neon/vp9_error_sve.c
@@ -48,3 +48,31 @@ int64_t vp9_block_error_sve(const tran_low_t *coeff, const tran_low_t *dqcoeff,
   *ssz = horizontal_add_int64x2(ssz_v);
   return horizontal_add_int64x2(err_v);
 }
+
+int64_t vp9_block_error_fp_sve(const tran_low_t *coeff,
+                               const tran_low_t *dqcoeff, int block_size) {
+  int64x2_t err = vdupq_n_s64(0);
+
+  assert(block_size >= 16);
+  assert((block_size % 16) == 0);
+
+  do {
+    const int16x8_t c0 = load_tran_low_to_s16q(coeff);
+    const int16x8_t c1 = load_tran_low_to_s16q(coeff + 8);
+
+    const int16x8_t d0 = load_tran_low_to_s16q(dqcoeff);
+    const int16x8_t d1 = load_tran_low_to_s16q(dqcoeff + 8);
+
+    const int16x8_t diff0 = vabdq_s16(c0, d0);
+    const int16x8_t diff1 = vabdq_s16(c1, d1);
+
+    err = vpx_dotq_s16(err, diff0, diff0);
+    err = vpx_dotq_s16(err, diff1, diff1);
+
+    coeff += 16;
+    dqcoeff += 16;
+    block_size -= 16;
+  } while (block_size != 0);
+
+  return horizontal_add_int64x2(err);
+}
