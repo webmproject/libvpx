a0f3eb8ce4a8f9ed17f06ebdca98329b939ee855
Wan-Teh Chang <wtc@google.com>
2024-02-01 16:12:54 -0800

    Delete a useless clamp(q, x, y) call
    In https://chromium-review.googlesource.com/c/webm/libvpx/+/71356, the
    statement
      clamp(q, active_best_quality, active_worst_quality);
    was added to rc_pick_q_and_bounds_two_pass() (recently renamed
    vp9_rc_pick_q_and_bounds_two_pass()).
    
    The result of the clamp() call is not used, so the clamp() call has no
    side effect.
    
    Fix Coverity CID 1577645 Useless call:
      side_effect_free: Calling
      clamp(q, active_best_quality, active_worst_quality) is only useful for
      its return value, which is ignored.
    
    Change-Id: I014c3e4caf2bc999fe480000acc4e49e7ad15aaf

--- vp9/encoder/vp9_ratectrl.c
+++ vp9/encoder/vp9_ratectrl.c
@@ -1582,7 +1582,6 @@ int vp9_rc_pick_q_and_bounds_two_pass(const VP9_COMP *cpi, int *bottom_index,
         q = active_worst_quality;
     }
   }
-  clamp(q, active_best_quality, active_worst_quality);
 
   *top_index = active_worst_quality;
   *bottom_index = active_best_quality;
