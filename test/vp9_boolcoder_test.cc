3bfd83a70c9ab80ce9f42eb9d3ec1349043dd3d5
Wan-Teh Chang <wtc@google.com>
2024-03-22 20:41:22 -0700

    Perform bounds checks in vpx_writer
    In the vpx_writer struct, change the buffer_end field to the size field.
    Change vpx_stop_encode() to return true on success, false on failure
    (output buffer full).
    
    In write_compressed_header(), remove the assertion
    assert(header_bc.pos <= 0xffff). The caller (vp9_pack_bitstream()) will
    check that condition.
    
    In vp9_pack_bitstream(), the variable "first_part_size" is renamed
    "compressed_hdr_size".
    
    Bug: webm:1844
    Change-Id: I4ed6ab905a707ad44d875e53036d5a42523a65d0
    (cherry picked from commit 73703c188b81f41bae59a72c9c86f8383587e994)

--- test/vp9_boolcoder_test.cc
+++ test/vp9_boolcoder_test.cc
@@ -65,7 +65,7 @@ TEST(VP9, TestBitIO) {
           vpx_write(&bw, bit, static_cast<int>(probas[i]));
         }
 
-        vpx_stop_encode(&bw);
+        GTEST_ASSERT_EQ(vpx_stop_encode(&bw), 0);
         // vpx_reader_fill() may read into uninitialized data that
         // isn't used meaningfully, but may trigger an MSan warning.
         memset(bw_buffer + bw.pos, 0, sizeof(BD_VALUE) - 1);
@@ -89,4 +89,25 @@ TEST(VP9, TestBitIO) {
       }
     }
   }
+}
+
+TEST(VP9, TestBitIOBufferSize0) {
+  vpx_writer bw;
+  uint8_t bw_buffer[1];
+  vpx_start_encode(&bw, bw_buffer, 0);
+  GTEST_ASSERT_EQ(vpx_stop_encode(&bw), -1);
+}
+
+TEST(VP9, TestBitIOBufferSize1) {
+  vpx_writer bw;
+  uint8_t bw_buffer[1];
+  vpx_start_encode(&bw, bw_buffer, sizeof(bw_buffer));
+  GTEST_ASSERT_EQ(vpx_stop_encode(&bw), -1);
+}
+
+TEST(VP9, TestBitIOBufferSize2) {
+  vpx_writer bw;
+  uint8_t bw_buffer[2];
+  vpx_start_encode(&bw, bw_buffer, sizeof(bw_buffer));
+  GTEST_ASSERT_EQ(vpx_stop_encode(&bw), 0);
 }
