976134c50d1f9d67c582c4e35805121117b0f178
Wan-Teh Chang <wtc@google.com>
2024-04-12 14:06:40 -0700

    Add 10 and 12b ranges to vpx_color_range_t comment
    Add note about undefined behavior in vpx_codec_encode() description.
    
    A port of the libaom CL
    https://aomedia-review.googlesource.com/c/aom/+/158001
    by Yannis Guyon <yguyon@google.com>.
    
    Bug: webm:1850
    Change-Id: Ia90f0bfd8265e35e9f33c17400c1c065d7915b77

--- vpx/vpx_image.h
+++ vpx/vpx_image.h
@@ -64,8 +64,12 @@ typedef enum vpx_color_space {
 
 /*!\brief List of supported color range */
 typedef enum vpx_color_range {
-  VPX_CR_STUDIO_RANGE = 0, /**< Y [16..235], UV [16..240] */
-  VPX_CR_FULL_RANGE = 1    /**< YUV/RGB [0..255] */
+  VPX_CR_STUDIO_RANGE = 0, /**<- Y  [16..235],  UV  [16..240]  (bit depth 8) */
+                           /**<- Y  [64..940],  UV  [64..960]  (bit depth 10) */
+                           /**<- Y [256..3760], UV [256..3840] (bit depth 12) */
+  VPX_CR_FULL_RANGE = 1    /**<- YUV/RGB [0..255]  (bit depth 8) */
+                           /**<- YUV/RGB [0..1023] (bit depth 10) */
+                           /**<- YUV/RGB [0..4095] (bit depth 12) */
 } vpx_color_range_t;       /**< alias for enum vpx_color_range */
 
 /**\brief Image Descriptor */
