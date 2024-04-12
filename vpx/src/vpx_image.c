89efe85cd48055ffd027f6bab6490dc639e19009
Wan-Teh Chang <wtc@google.com>
2024-04-12 14:36:40 -0700

    Clarify comment about buf_align in vpx_img_wrap.
    If img_data is not NULL, img_alloc_helper ignores buf_align, so
    vpx_img_wrap can set buf_align to any placeholder value.
    
    A port of the libaom CL
    https://aomedia-review.googlesource.com/c/aom/+/90362.
    
    Bug: webm:1850
    Change-Id: I42bc45aecf822a9314caf23058fe123d0574dc20

--- vpx/src/vpx_image.c
+++ vpx/src/vpx_image.c
@@ -164,8 +164,8 @@ vpx_image_t *vpx_img_alloc(vpx_image_t *img, vpx_img_fmt_t fmt,
 vpx_image_t *vpx_img_wrap(vpx_image_t *img, vpx_img_fmt_t fmt, unsigned int d_w,
                           unsigned int d_h, unsigned int stride_align,
                           unsigned char *img_data) {
-  /* By setting buf_align = 1, we don't change buffer alignment in this
-   * function. */
+  /* Set buf_align = 1. It is ignored by img_alloc_helper because img_data is
+   * not NULL. */
   return img_alloc_helper(img, fmt, d_w, d_h, 1, stride_align, img_data);
 }
 
