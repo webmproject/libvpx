ee2552d9035dd7b0e328e37fefc29a88d8e5ae06
Jerome Jiang <jianj@google.com>
2024-08-16 17:16:29 -0400

    vp9 ext rc: TPL & final encoding use same QP
    Removed codec control VP9E_ENABLE_EXTERNAL_RC_TPL since it is
    no longer needed.
    
    Change-Id: I151254ff3f0496c017ddf73c2caf94783ef38f31

--- vp9/encoder/vp9_encoder.h
+++ vp9/encoder/vp9_encoder.h
@@ -1067,8 +1067,6 @@ typedef struct VP9_COMP {
    */
   uint64_t frame_component_time[kTimingComponents];
 #endif
-  // Flag to indicate if QP and GOP for TPL are controlled by external RC.
-  int tpl_with_external_rc;
 } VP9_COMP;
 
 #if CONFIG_RATE_CTRL
--- vpx/vp8cx.h
+++ vpx/vp8cx.h
@@ -768,16 +768,6 @@ enum vp8e_enc_control_id {
    *
    */
   VP9E_SET_QUANTIZER_ONE_PASS,
-
-  /*!\brief Codec control to use external RC to control TPL.
-   *
-   * This will use external RC to control the QP and GOP structure for TPL.
-   * (rc_type & VPX_RC_QP) in vpx_rc_funcs_t must be non zero.
-   * get_encodeframe_decision callback in vpx_rc_funcs_t also needs to be set.
-   *
-   * Supported in codecs: VP9
-   */
-  VP9E_ENABLE_EXTERNAL_RC_TPL,
 };
 
 /*!\brief vpx 1-D scaling mode
@@ -1108,8 +1098,6 @@ VPX_CTRL_USE_TYPE(VP8E_SET_RTC_EXTERNAL_RATECTRL, int)
 #define VPX_CTRL_VP8E_SET_RTC_EXTERNAL_RATECTRL
 VPX_CTRL_USE_TYPE(VP9E_SET_QUANTIZER_ONE_PASS, int)
 #define VPX_CTRL_VP9E_SET_QUANTIZER_ONE_PASS
-VPX_CTRL_USE_TYPE(VP9E_ENABLE_EXTERNAL_RC_TPL, int)
-#define VPX_CTRL_VP9E_ENABLE_EXTERNAL_RC_TPL
 
 /*!\endcond */
 /*! @} - end defgroup vp8_encoder */
