9b3d41cfacdf740076ac88ba97dc5288c879e5ac
Diksha Singh <diksha.singh@ittiam.com>
2024-09-24 18:31:39 +0530

    Add AVX2 for 12-tap highbd filter of temporal filter prediction
    This CL adds AVX2 implementation for the function
    vpx_highbd_convolve_12_c().The relevant unit tests are added.
    
    The module level scaling w.r.t. C implementation are as follows:
    
      For vpx_convolve8_12_avx2()
    Blk Size        Scaling w.r.t. C
      8x8                11.0x
     16x16               12.8x
     32x32               15.3x
    
      For convolve_horiz_12_avx2()
    Blk Size        Scaling w.r.t. C
      8x8                11.1x
     16x16               11.5x
     32x32               14.2x
    
       For convolve_vert_12_avx2()
    Blk Size        Scaling w.r.t. C
      8x8                11.7x
     16x16               16.0x
     32x32               17.4x
    
      For convolve_copy_12_avx2()
    Blk Size        Scaling w.r.t. C
      8x8                4.3x
     16x16               4.3x
     32x32               2.2x
    
    Change-Id: If66fbef914589783f68feb7eb71aee175630498e

--- vp9/common/vp9_rtcd_defs.pl
+++ vp9/common/vp9_rtcd_defs.pl
@@ -96,6 +96,18 @@ if (vpx_config("CONFIG_VP9_HIGHBITDEPTH") eq "yes") {
     add_proto qw/void vp9_highbd_post_proc_down_and_across/, "const uint16_t *src_ptr, uint16_t *dst_ptr, int src_pixels_per_line, int dst_pixels_per_line, int rows, int cols, int flimit";
   }
 
+  add_proto qw/void vpx_highbd_convolve_copy_12/, "const uint16_t *src, ptrdiff_t src_stride, uint16_t *dst, ptrdiff_t dst_stride, const InterpKernel12 *filter, int x0_q4, int x_step_q4, int y0_q4, int y_step_q4, int w, int h, int bd";
+  specialize qw/vpx_highbd_convolve_copy_12 avx2/;
+
+  add_proto qw/void vpx_highbd_convolve_vert_12/, "const uint16_t *src, ptrdiff_t src_stride, uint16_t *dst, ptrdiff_t dst_stride, const InterpKernel12 *filter, int x0_q4, int x_step_q4, int y0_q4, int y_step_q4, int w, int h, int bd";
+  specialize qw/vpx_highbd_convolve_vert_12 avx2/;
+
+  add_proto qw/void vpx_highbd_convolve_horiz_12/, "const uint16_t *src, ptrdiff_t src_stride, uint16_t *dst, ptrdiff_t dst_stride, const InterpKernel12 *filter, int x0_q4, int x_step_q4, int y0_q4, int y_step_q4, int w, int h, int bd";
+  specialize qw/vpx_highbd_convolve_horiz_12 avx2/;
+
+  add_proto qw/void vpx_highbd_convolve_12/, "const uint16_t *src, ptrdiff_t src_stride, uint16_t *dst, ptrdiff_t dst_stride, const InterpKernel12 *filter, int x0_q4, int x_step_q4, int y0_q4, int y_step_q4, int w, int h, int bd";
+  specialize qw/vpx_highbd_convolve_12 avx2/;
+
   #
   # dct
   #
