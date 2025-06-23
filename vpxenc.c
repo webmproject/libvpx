337f4bde89f32e7d8facb586dfef58b37a4aa6d4
Marco Paniconi <marpan@google.com>
2025-06-23 15:02:52 -0700

    Fix to division by zero crash in vpxenc
    Caused by --fps input with 0 value for numerator.
    Fix proposed in the issue below.
    
    Bug: b:426781256
    Change-Id: I589b49dadbffb6fcfb1784d2fde39deeb449ec01

--- vpxenc.c
+++ vpxenc.c
@@ -719,7 +719,7 @@ static void validate_positive_rational(const char *msg,
     rat->den *= -1;
   }
 
-  if (rat->num < 0) die("Error: %s must be positive\n", msg);
+  if (rat->num <= 0) die("Error: %s must be positive\n", msg);
 
   if (!rat->den) die("Error: %s has zero denominator\n", msg);
 }
