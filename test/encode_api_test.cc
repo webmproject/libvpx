auraecosystem/webm

Fix to issue with tile_rows
Add proper limit on the number of tile_rows
in set_tile_limits(().

Update and enable the tests covering
the issue below.

Bug: b:442105459
Change-Id: Iedbfc2efacbba3f4ebe57e4085277cc4d0ca9316

test/encode_api_test.cc
@@ -1788,9 +1788,9 @@ TEST(EncodeAPI, Buganizer441668134) {
 // with row-mt enabled. This triggers an assertion in vp9_bitstream.c (in
 // function write_modes()), as in the issue:42105459. In this test it happens on
 // very first encoded frame since lag_in_frames = 0. Issue is due to enabling
-// TILE_ROWS: passes if tile_rows is disabled (set to 0), or if height is above
-// 64 (so both row-tiles are non-empty).
-TEST(EncodeAPI, DISABLED_Buganizer442105459) {
+// TILE_ROWS, with number of tile_rows more than the number of superblocks.
+// This tests sets 2 tile_rows with height corresponding to 1 superblock (sb).
+TEST(EncodeAPI, Buganizer442105459_2RowTiles) {
   // Initialize VP9 encoder interface
   vpx_codec_iface_t *iface = vpx_codec_vp9_cx();
   // Get default encoder configuration
@@ -1798,7 +1798,7 @@ TEST(EncodeAPI, DISABLED_Buganizer442105459) {
   ASSERT_EQ(vpx_codec_enc_config_default(iface, &cfg, 0), VPX_CODEC_OK);
   // Configure encoder
   cfg.g_w = 946u;
-  cfg.g_h = 64u;
+  cfg.g_h = 64u;  // 1 sb row, 2 tiles_rows set below.
   cfg.g_threads = 1;
   cfg.g_profile = 0;
   cfg.g_bit_depth = VPX_BITS_8;
@@ -1815,6 +1815,71 @@ TEST(EncodeAPI, DISABLED_Buganizer442105459) {
   // Set control parameters
   vpx_codec_control_(&ctx, VP8E_SET_CPUUSED, -5);
   vpx_codec_control_(&ctx, VP9E_SET_TILE_ROWS, 1);
+  vpx_codec_control_(&ctx, VP9E_SET_TILE_COLUMNS, 1);
+  vpx_codec_control_(&ctx, VP9E_SET_ROW_MT, 1);
+  // Image format selection
+  vpx_img_fmt_t img_fmt = VPX_IMG_FMT_I420;
+  // Allocate image with varied alignment
+  vpx_image_t *img = vpx_img_alloc(nullptr, img_fmt, cfg.g_w, cfg.g_h, 1);
+  // Encode with dynamic configuration changes
+  int num_frames = 2;
+  // Per-frame constants captured from the original run (indices consumed per
+  // frame)
+  const unsigned long frame_pts_mul[] = { 33333UL, 33333UL };
+  const unsigned long frame_durations[] = { 33333UL, 33333UL };
+  const vpx_enc_deadline_t frame_deadlines[] = { VPX_DL_REALTIME,
+                                                 VPX_DL_REALTIME };
+  for (int frame = 0; frame < num_frames; frame++) {
+    // Encode frame
+    vpx_codec_pts_t pts = frame * frame_pts_mul[frame];
+    unsigned long duration = frame_durations[frame];
+    vpx_enc_deadline_t deadline = frame_deadlines[frame];
+    ASSERT_EQ(vpx_codec_encode(&ctx, img, pts, duration, /*flags*/ 0, deadline),
+              VPX_CODEC_OK);
+  }
+  // Flush encoder.
+  ASSERT_EQ(vpx_codec_encode(&ctx, NULL, 0, 0, 0, VPX_DL_REALTIME), 0);
+  // Get remaining data
+  vpx_codec_iter_t iter = NULL;
+  while (vpx_codec_get_cx_data(&ctx, &iter) != NULL) {
+    // Process remaining packets
+  }
+  vpx_img_free(img);
+  vpx_codec_destroy(&ctx);
+}
+
+// Encode a few frames, with realtime mode and tile_rows set to 1,
+// with row-mt enabled. This triggers an assertion in vp9_bitstream.c (in
+// function write_modes()), as in the issue:42105459. In this test it happens on
+// very first encoded frame since lag_in_frames = 0. Issue is due to enabling
+// TILE_ROWS, with number of tile_rows more than the number of superblocks.
+// This tests sets 4 tile_rows with height corresponding to 3 superblocks.
+TEST(EncodeAPI, Buganizer442105459_4RowTiles) {
+  // Initialize VP9 encoder interface
+  vpx_codec_iface_t *iface = vpx_codec_vp9_cx();
+  // Get default encoder configuration
+  vpx_codec_enc_cfg_t cfg;
+  ASSERT_EQ(vpx_codec_enc_config_default(iface, &cfg, 0), VPX_CODEC_OK);
+  // Configure encoder
+  cfg.g_w = 946u;
+  cfg.g_h = 192u;  // 3 sb rows, 4 tiles_rows set below.
+  cfg.g_threads = 1;
+  cfg.g_profile = 0;
+  cfg.g_bit_depth = VPX_BITS_8;
+  // Rate control targeting deeper encoding paths
+  cfg.rc_target_bitrate = 100;
+  cfg.rc_min_quantizer = 0;
+  cfg.rc_max_quantizer = 0;
+  cfg.rc_end_usage = VPX_VBR;
+  cfg.ss_number_layers = 1;
+  cfg.g_lag_in_frames = 0;
+  // Initialize encoder context
+  vpx_codec_ctx_t ctx;
+  ASSERT_EQ(vpx_codec_enc_init(&ctx, iface, &cfg, 0), VPX_CODEC_OK);
+  // Set control parameters
+  vpx_codec_control_(&ctx, VP8E_SET_CPUUSED, -5);
+  vpx_codec_control_(&ctx, VP9E_SET_TILE_ROWS, 2);
+  vpx_codec_control_(&ctx, VP9E_SET_TILE_COLUMNS, 1);
   vpx_codec_control_(&ctx, VP9E_SET_ROW_MT, 1);
   // Image format selection
   vpx_img_fmt_t img_fmt = VPX_IMG_FMT_I420;

webmproject/libvpx

test/encode_api_test.cc
@@ -1788,9 +1788,9 @@ TEST(EncodeAPI, Buganizer441668134) {
 // with row-mt enabled. This triggers an assertion in vp9_bitstream.c (in
 // function write_modes()), as in the issue:42105459. In this test it happens on
 // very first encoded frame since lag_in_frames = 0. Issue is due to enabling
-// TILE_ROWS: passes if tile_rows is disabled (set to 0), or if height is above
-// 64 (so both row-tiles are non-empty).
-TEST(EncodeAPI, DISABLED_Buganizer442105459) {
+// TILE_ROWS, with number of tile_rows more than the number of superblocks.
+// This test sets 2 tile_rows with height corresponding to 1 superblock (sb).
+TEST(EncodeAPI, Buganizer442105459_2RowTiles) {
   // Initialize VP9 encoder interface
   vpx_codec_iface_t *iface = vpx_codec_vp9_cx();
   // Get default encoder configuration
@@ -1798,7 +1798,7 @@ TEST(EncodeAPI, DISABLED_Buganizer442105459) {
   ASSERT_EQ(vpx_codec_enc_config_default(iface, &cfg, 0), VPX_CODEC_OK);
   // Configure encoder
   cfg.g_w = 946u;
-  cfg.g_h = 64u;
+  cfg.g_h = 64u;  // 1 sb row, 2 tile_rows set below.
   cfg.g_threads = 1;
   cfg.g_profile = 0;
   cfg.g_bit_depth = VPX_BITS_8;
@@ -1815,6 +1815,71 @@ TEST(EncodeAPI, DISABLED_Buganizer442105459) {
   // Set control parameters
   vpx_codec_control_(&ctx, VP8E_SET_CPUUSED, -5);
   vpx_codec_control_(&ctx, VP9E_SET_TILE_ROWS, 1);
+  vpx_codec_control_(&ctx, VP9E_SET_TILE_COLUMNS, 1);
+  vpx_codec_control_(&ctx, VP9E_SET_ROW_MT, 1);
+  // Image format selection
+  vpx_img_fmt_t img_fmt = VPX_IMG_FMT_I420;
+  // Allocate image with varied alignment
+  vpx_image_t *img = vpx_img_alloc(nullptr, img_fmt, cfg.g_w, cfg.g_h, 1);
+  // Encode with dynamic configuration changes
+  int num_frames = 2;
+  // Per-frame constants captured from the original run (indices consumed per
+  // frame)
+  const unsigned long frame_pts_mul[] = { 33333UL, 33333UL };
+  const unsigned long frame_durations[] = { 33333UL, 33333UL };
+  const vpx_enc_deadline_t frame_deadlines[] = { VPX_DL_REALTIME,
+                                                 VPX_DL_REALTIME };
+  for (int frame = 0; frame < num_frames; frame++) {
+    // Encode frame
+    vpx_codec_pts_t pts = frame * frame_pts_mul[frame];
+    unsigned long duration = frame_durations[frame];
+    vpx_enc_deadline_t deadline = frame_deadlines[frame];
+    ASSERT_EQ(vpx_codec_encode(&ctx, img, pts, duration, /*flags*/ 0, deadline),
+              VPX_CODEC_OK);
+  }
+  // Flush encoder.
+  ASSERT_EQ(vpx_codec_encode(&ctx, NULL, 0, 0, 0, VPX_DL_REALTIME), 0);
+  // Get remaining data
+  vpx_codec_iter_t iter = NULL;
+  while (vpx_codec_get_cx_data(&ctx, &iter) != NULL) {
+    // Process remaining packets
+  }
+  vpx_img_free(img);
+  vpx_codec_destroy(&ctx);
+}
+
+// Encode a few frames, with realtime mode and tile_rows set to 1,
+// with row-mt enabled. This triggers an assertion in vp9_bitstream.c (in
+// function write_modes()), as in the issue:42105459. In this test it happens on
+// very first encoded frame since lag_in_frames = 0. Issue is due to enabling
+// TILE_ROWS, with number of tile_rows more than the number of superblocks.
+// This test sets 4 tile_rows with height corresponding to 3 superblocks.
+TEST(EncodeAPI, Buganizer442105459_4RowTiles) {
+  // Initialize VP9 encoder interface
+  vpx_codec_iface_t *iface = vpx_codec_vp9_cx();
+  // Get default encoder configuration
+  vpx_codec_enc_cfg_t cfg;
+  ASSERT_EQ(vpx_codec_enc_config_default(iface, &cfg, 0), VPX_CODEC_OK);
+  // Configure encoder
+  cfg.g_w = 946u;
+  cfg.g_h = 192u;  // 3 sb rows, 4 tile_rows set below.
+  cfg.g_threads = 1;
+  cfg.g_profile = 0;
+  cfg.g_bit_depth = VPX_BITS_8;
+  // Rate control targeting deeper encoding paths
+  cfg.rc_target_bitrate = 100;
+  cfg.rc_min_quantizer = 0;
+  cfg.rc_max_quantizer = 0;
+  cfg.rc_end_usage = VPX_VBR;
+  cfg.ss_number_layers = 1;
+  cfg.g_lag_in_frames = 0;
+  // Initialize encoder context
+  vpx_codec_ctx_t ctx;
+  ASSERT_EQ(vpx_codec_enc_init(&ctx, iface, &cfg, 0), VPX_CODEC_OK);
+  // Set control parameters
+  vpx_codec_control_(&ctx, VP8E_SET_CPUUSED, -5);
+  vpx_codec_control_(&ctx, VP9E_SET_TILE_ROWS, 2);
+  vpx_codec_control_(&ctx, VP9E_SET_TILE_COLUMNS, 1);
   vpx_codec_control_(&ctx, VP9E_SET_ROW_MT, 1);
   // Image format selection
   vpx_img_fmt_t img_fmt = VPX_IMG_FMT_I420;