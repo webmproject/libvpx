fe1e2724911825f4dc2ab6bc23e5c9389f870bcf
Marco Paniconi <marpan@google.com>
2026-04-24 10:25:50 -0700

    vp9-svc: follow-up to disallow svc for 2pass
    Move the vp9_set_svc(ctx->cpi, data) in the
    control ctrl_set_svc() after the check on
    invalid parameter.
    
    Bug: 505830937
    Change-Id: Ie4560332c4833f53246a2647441d8b53528fd585

--- vp9/vp9_cx_iface.c
+++ vp9/vp9_cx_iface.c
@@ -1862,12 +1862,12 @@ static vpx_codec_err_t ctrl_set_scale_mode(vpx_codec_alg_priv_t *ctx,
 static vpx_codec_err_t ctrl_set_svc(vpx_codec_alg_priv_t *ctx, va_list args) {
   int data = va_arg(args, int);
   const vpx_codec_enc_cfg_t *cfg = &ctx->cfg;
-  vp9_set_svc(ctx->cpi, data);
   // SVC is only supported for one-pass.
   if (data == 1 &&
       (cfg->g_pass == VPX_RC_FIRST_PASS || cfg->g_pass == VPX_RC_LAST_PASS)) {
     return VPX_CODEC_INVALID_PARAM;
   }
+  vp9_set_svc(ctx->cpi, data);
   vp9_set_row_mt(ctx->cpi);
   return VPX_CODEC_OK;
 }
