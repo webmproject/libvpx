auraecosystem/webm

Revert "Remove spinning before calling pthread_mutex_lock"
This reverts commit 556606296369c53396d40c88304b7003b738a44d.

This causes regressions in Android performance tests.

Bug: 493631602
Change-Id: I82669cda9af56d0c819e77e3f0c4d778d8cc012b

vpx_util/vpx_pthread.h
@@ -92,6 +92,10 @@ static INLINE int pthread_mutex_init(pthread_mutex_t *const mutex,
   return 0;
 }
 
+static INLINE int pthread_mutex_trylock(pthread_mutex_t *const mutex) {
+  return TryEnterCriticalSection(mutex) ? 0 : EBUSY;
+}
+
 static INLINE int pthread_mutex_lock(pthread_mutex_t *const mutex) {
   AcquireSRWLockExclusive(mutex);
   return 0;

webmproject/libvpx

Bug: 493631602, 502837770

vpx_util/vpx_pthread.h
@@ -92,6 +92,10 @@ static INLINE int pthread_mutex_init(pthread_mutex_t *const mutex,
   return 0;
 }
 
+static INLINE int pthread_mutex_trylock(pthread_mutex_t *const mutex) {
+  return TryAcquireSRWLockExclusive(mutex) ? 0 : EBUSY;
+}
+
 static INLINE int pthread_mutex_lock(pthread_mutex_t *const mutex) {
   AcquireSRWLockExclusive(mutex);
   return 0;