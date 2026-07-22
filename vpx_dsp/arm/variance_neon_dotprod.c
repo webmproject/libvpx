b71c17f646ef41488255593ebc0368c8bd98aed8
Jonathan Wright <jonathan.wright@arm.com>
2026-07-22 14:35:49 +0100

    Unroll Arm Neon DotProd variance kernels for w >= 32
    Unroll the Arm Neon DotProd variance kernels and split the
    accumulators for blocks having width >= 32. This change reduces the
    time required to execute these kernels by up to 30% on a Neoverse V3
    when compiled with Clang 22.
    
    Change-Id: I96e51a2427b893b06fc43f00266d60d695e7fdc4

--- vpx_dsp/arm/variance_neon_dotprod.c
+++ vpx_dsp/arm/variance_neon_dotprod.c
@@ -110,56 +110,105 @@ static INLINE void variance_16xh_neon_dotprod(const uint8_t *src_ptr,
   *sse = horizontal_add_uint32x4(sse_u32);
 }
 
-// Process a block of any size where the width is divisible by 16.
-static INLINE void variance_large_neon_dotprod(const uint8_t *src_ptr,
-                                               int src_stride,
-                                               const uint8_t *ref_ptr,
-                                               int ref_stride, int w, int h,
-                                               uint32_t *sse, int *sum) {
-  uint32x4_t src_sum = vdupq_n_u32(0);
-  uint32x4_t ref_sum = vdupq_n_u32(0);
-  uint32x4_t sse_u32 = vdupq_n_u32(0);
+// Process a block of width 32 one row at a time.
+static INLINE void variance_32xh_neon_dotprod(const uint8_t *src_ptr,
+                                              int src_stride,
+                                              const uint8_t *ref_ptr,
+                                              int ref_stride, int h,
+                                              uint32_t *sse, int *sum) {
+  uint32x4_t src_sum[2] = { vdupq_n_u32(0), vdupq_n_u32(0) };
+  uint32x4_t ref_sum[2] = { vdupq_n_u32(0), vdupq_n_u32(0) };
+  uint32x4_t sse_u32[2] = { vdupq_n_u32(0), vdupq_n_u32(0) };
 
   do {
-    int i = 0;
-    do {
-      const uint8x16_t s = vld1q_u8(src_ptr + i);
-      const uint8x16_t r = vld1q_u8(ref_ptr + i);
+    const uint8x16_t s0 = vld1q_u8(src_ptr);
+    const uint8x16_t r0 = vld1q_u8(ref_ptr);
 
-      const uint8x16_t abs_diff = vabdq_u8(s, r);
-      sse_u32 = vdotq_u32(sse_u32, abs_diff, abs_diff);
+    const uint8x16_t abs_diff0 = vabdq_u8(s0, r0);
+    sse_u32[0] = vdotq_u32(sse_u32[0], abs_diff0, abs_diff0);
 
-      src_sum = vdotq_u32(src_sum, s, vdupq_n_u8(1));
-      ref_sum = vdotq_u32(ref_sum, r, vdupq_n_u8(1));
+    src_sum[0] = vdotq_u32(src_sum[0], s0, vdupq_n_u8(1));
+    ref_sum[0] = vdotq_u32(ref_sum[0], r0, vdupq_n_u8(1));
 
-      i += 16;
-    } while (i < w);
+    const uint8x16_t s1 = vld1q_u8(src_ptr + 16);
+    const uint8x16_t r1 = vld1q_u8(ref_ptr + 16);
+
+    const uint8x16_t abs_diff1 = vabdq_u8(s1, r1);
+    sse_u32[1] = vdotq_u32(sse_u32[1], abs_diff1, abs_diff1);
+
+    src_sum[1] = vdotq_u32(src_sum[1], s1, vdupq_n_u8(1));
+    ref_sum[1] = vdotq_u32(ref_sum[1], r1, vdupq_n_u8(1));
 
     src_ptr += src_stride;
     ref_ptr += ref_stride;
   } while (--h != 0);
 
+  src_sum[0] = vaddq_u32(src_sum[0], src_sum[1]);
+  ref_sum[0] = vaddq_u32(ref_sum[0], ref_sum[1]);
   *sum = horizontal_add_int32x4(
-      vreinterpretq_s32_u32(vsubq_u32(src_sum, ref_sum)));
-  *sse = horizontal_add_uint32x4(sse_u32);
+      vreinterpretq_s32_u32(vsubq_u32(src_sum[0], ref_sum[0])));
+  *sse = horizontal_add_uint32x4(vaddq_u32(sse_u32[0], sse_u32[1]));
 }
 
-static INLINE void variance_32xh_neon_dotprod(const uint8_t *src,
+// Process a block of width 64 one row at a time.
+static INLINE void variance_64xh_neon_dotprod(const uint8_t *src_ptr,
                                               int src_stride,
-                                              const uint8_t *ref,
+                                              const uint8_t *ref_ptr,
                                               int ref_stride, int h,
                                               uint32_t *sse, int *sum) {
-  variance_large_neon_dotprod(src, src_stride, ref, ref_stride, 32, h, sse,
-                              sum);
-}
+  uint32x4_t src_sum[4] = { vdupq_n_u32(0), vdupq_n_u32(0), vdupq_n_u32(0),
+                            vdupq_n_u32(0) };
+  uint32x4_t ref_sum[4] = { vdupq_n_u32(0), vdupq_n_u32(0), vdupq_n_u32(0),
+                            vdupq_n_u32(0) };
+  uint32x4_t sse_u32[4] = { vdupq_n_u32(0), vdupq_n_u32(0), vdupq_n_u32(0),
+                            vdupq_n_u32(0) };
 
-static INLINE void variance_64xh_neon_dotprod(const uint8_t *src,
-                                              int src_stride,
-                                              const uint8_t *ref,
-                                              int ref_stride, int h,
-                                              uint32_t *sse, int *sum) {
-  variance_large_neon_dotprod(src, src_stride, ref, ref_stride, 64, h, sse,
-                              sum);
+  do {
+    const uint8x16_t s0 = vld1q_u8(src_ptr);
+    const uint8x16_t r0 = vld1q_u8(ref_ptr);
+
+    const uint8x16_t abs_diff0 = vabdq_u8(s0, r0);
+    sse_u32[0] = vdotq_u32(sse_u32[0], abs_diff0, abs_diff0);
+
+    src_sum[0] = vdotq_u32(src_sum[0], s0, vdupq_n_u8(1));
+    ref_sum[0] = vdotq_u32(ref_sum[0], r0, vdupq_n_u8(1));
+
+    const uint8x16_t s1 = vld1q_u8(src_ptr + 16);
+    const uint8x16_t r1 = vld1q_u8(ref_ptr + 16);
+
+    const uint8x16_t abs_diff1 = vabdq_u8(s1, r1);
+    sse_u32[1] = vdotq_u32(sse_u32[1], abs_diff1, abs_diff1);
+
+    src_sum[1] = vdotq_u32(src_sum[1], s1, vdupq_n_u8(1));
+    ref_sum[1] = vdotq_u32(ref_sum[1], r1, vdupq_n_u8(1));
+
+    const uint8x16_t s2 = vld1q_u8(src_ptr + 32);
+    const uint8x16_t r2 = vld1q_u8(ref_ptr + 32);
+
+    const uint8x16_t abs_diff2 = vabdq_u8(s2, r2);
+    sse_u32[2] = vdotq_u32(sse_u32[2], abs_diff2, abs_diff2);
+
+    src_sum[2] = vdotq_u32(src_sum[2], s2, vdupq_n_u8(1));
+    ref_sum[2] = vdotq_u32(ref_sum[2], r2, vdupq_n_u8(1));
+
+    const uint8x16_t s3 = vld1q_u8(src_ptr + 48);
+    const uint8x16_t r3 = vld1q_u8(ref_ptr + 48);
+
+    const uint8x16_t abs_diff3 = vabdq_u8(s3, r3);
+    sse_u32[3] = vdotq_u32(sse_u32[3], abs_diff3, abs_diff3);
+
+    src_sum[3] = vdotq_u32(src_sum[3], s3, vdupq_n_u8(1));
+    ref_sum[3] = vdotq_u32(ref_sum[3], r3, vdupq_n_u8(1));
+
+    src_ptr += src_stride;
+    ref_ptr += ref_stride;
+  } while (--h != 0);
+
+  src_sum[0] = horizontal_add_4d_uint32x4(src_sum);
+  ref_sum[0] = horizontal_add_4d_uint32x4(ref_sum);
+  *sum = horizontal_add_int32x4(
+      vreinterpretq_s32_u32(vsubq_u32(src_sum[0], ref_sum[0])));
+  *sse = horizontal_add_uint32x4(horizontal_add_4d_uint32x4(sse_u32));
 }
 
 void vpx_get8x8var_neon_dotprod(const uint8_t *src_ptr, int src_stride,
