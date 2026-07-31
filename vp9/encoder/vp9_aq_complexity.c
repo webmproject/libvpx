4c81591cffcc012655334ef27f41f2e8d0b7f4c2
Jingning Han <jingning@google.com>
2026-07-31 09:51:04 -0700

    Apply a clamp to the cumulative bit-rate
    Bug: webm:497896136
    
    Change-Id: I480d9876e80fb7b07b18a554635974f547d3c2cb

--- vp9/encoder/vp9_aq_complexity.c
+++ vp9/encoder/vp9_aq_complexity.c
@@ -125,7 +125,8 @@ void vp9_caq_select_segment(VP9_COMP *cpi, MACROBLOCK *mb, BLOCK_SIZE bs,
   } else {
     // Rate depends on fraction of a SB64 in frame (xmis * ymis / bw * bh).
     // It is converted to bits * 256 units.
-    assert(cpi->rc.sb64_target_rate < INT_MAX / 256);
+    cpi->rc.sb64_target_rate = VPXMIN(INT_MAX / 256, cpi->rc.sb64_target_rate);
+
     const int target_rate =
         (int)(((int64_t)cpi->rc.sb64_target_rate * xmis * ymis * 256) /
               (bw * bh));
